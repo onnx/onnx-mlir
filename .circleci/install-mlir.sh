c4b990aed86327a95b84cde57d5c64e2ab69fa1d
Tian Jin <tjingrant@gmail.com>
2019-12-31 01:45:32 -0500

    1. Simplify CircleCI build scripts. 2. Refactor mlir installation script to a standalone shell script.

---
+++ .circleci/install-mlir.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/llvm/llvm-project.git
+mkdir llvm-project/build
+cd llvm-project/build
+cmake -G Ninja ../llvm \
+   -DLLVM_ENABLE_PROJECTS=mlir \
+   -DLLVM_BUILD_EXAMPLES=ON \
+   -DLLVM_TARGETS_TO_BUILD="host" \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DLLVM_ENABLE_ASSERTIONS=ON \
+   -DLLVM_ENABLE_RTTI=ON
+# TODO(tjingrant): why is RTTI necessary?
+cmake --build . --target check-mlir -- -j ${PARALLEL_JOBS_LIMIT:-4}
