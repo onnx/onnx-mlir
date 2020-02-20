f1d20e368f778c45d165cf88f7e86cdf3ccc2179
Tung D. Le <tung@jp.ibm.com>
2020-02-21 00:55:24 +0900

    Add support of GemmNoBias (#91)
    * Add support of GemmNoBias
    
    * Fix a wrong indentation

--- test/backend/test.py
+++ test/backend/test.py
@@ -98,7 +98,7 @@ test_to_enable = [
     "test_gemm_alpha_cpu",
     "test_gemm_beta_cpu",
     "test_gemm_default_matrix_bias_cpu",
-    # "test_gemm_default_no_bias_cpu", <- error, need support for optional operands
+    "test_gemm_default_no_bias_cpu",
     "test_gemm_default_scalar_bias_cpu",
     "test_gemm_default_single_elem_vector_bias_cpu",
     "test_gemm_default_vector_bias_cpu",
