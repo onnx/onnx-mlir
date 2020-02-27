fc793134c57993fece0cde7fc9b4625eaabef613
Tung D. Le <tung@jp.ibm.com>
2020-02-27 10:41:45 +0900

    Merge the first two krnl loops into one krnl loop; remove attribute checks

--- src/conversion/onnx_to_krnl/nn/pooling.cpp
+++ src/conversion/onnx_to_krnl/nn/pooling.cpp
@@ -14,12 +14,12 @@ using namespace mlir;
 
 // Identity values
 template <>
-float getIdentityValue<float, ONNXMaxPoolSingleOutOp>(){
+float getIdentityValue<float, ONNXMaxPoolSingleOutOp>() {
   return (float)-std::numeric_limits<float>::infinity();
 }
 
 template <>
-int getIdentityValue<int, ONNXMaxPoolSingleOutOp>(){
+int getIdentityValue<int, ONNXMaxPoolSingleOutOp>() {
   return std::numeric_limits<int>::min();
 }
 
@@ -50,19 +50,14 @@ struct ONNXMaxPoolSingleOutOpLowering : public ConversionPattern {
     // Read kernel_shape attribute
     SmallVector<int, 4> kernelShape;
     auto kernelShapeAttribute = poolOp.kernel_shapeAttr();
-    if (kernelShapeAttribute)
-      for (auto dim : kernelShapeAttribute.getValue())
-        kernelShape.emplace_back(dim.cast<IntegerAttr>().getInt());
-    else
-      emitError(loc, "kernel_shape is a mandatory attribute for which there is "
-                     "no default.");
+    for (auto dim : kernelShapeAttribute.getValue())
+      kernelShape.emplace_back(dim.cast<IntegerAttr>().getInt());
 
     // Read strides attribute
     SmallVector<int, 4> strides;
     auto stridesAttribute = poolOp.stridesAttr();
-    if (stridesAttribute)
-      for (auto stride : stridesAttribute.getValue())
-        strides.emplace_back(stride.cast<IntegerAttr>().getInt());
+    for (auto stride : stridesAttribute.getValue())
+      strides.emplace_back(stride.cast<IntegerAttr>().getInt());
 
     // Read ceil_mode attribute
     auto ceilMode = poolOp.ceil_mode().getSExtValue();
@@ -98,127 +93,101 @@ struct ONNXMaxPoolSingleOutOpLowering : public ConversionPattern {
     //     for r1 = 0 .. RH:
     //       for r2 = 0 .. RW:
     //         R[n][c][r1][r2] = negative_infinity;
-    //           for k1 = 0 .. KH:
-    //             for k2 = 0 .. KW:
-    //               t = D[n][c][s1 * r1 + k1][s2 * r2 + k2];
-    //               R[n][c][r1][r2] = max(R[n][c][r1][r2], t);
+    //         for k1 = 0 .. KH:
+    //           for k2 = 0 .. KW:
+    //             t = D[n][c][s1 * r1 + k1][s2 * r2 + k2];
+    //             R[n][c][r1][r2] = max(R[n][c][r1][r2], t);
     //
     // Naming:
-    //   n, c: outer loop nest indices
-    //   r1, r2: spatial loop nest indices
+    //   n, c, r1, r2: outer loop nest indices
     //   k1, k2: inner loop nest indices
     //
     // TODO: handle padding.
     //
 
-    // 1. Define outer loops and emit empty optimization block:
-    int batchRank = 2;  // N and C dimensions
-    BuildKrnlLoop outerLoops(rewriter, loc, batchRank);
-    outerLoops.createDefineAndOptimizeOp();
-    int nIndex = outerLoops.pushBounds(0, inputOperand, 0);
-    int cIndex = outerLoops.pushBounds(0, inputOperand, 1);
-    // Outer loop iteration
-    outerLoops.createIterateOp();
+    // 1. Define outer loops and emit empty optimization block.
+    auto nOuterLoops = resultShape.size();
+    BuildKrnlLoop outerLoops(rewriter, loc, nOuterLoops);
+    outerLoops.createDefineOptimizeAndIterateOp(alloc);
+
     rewriter.setInsertionPointToStart(outerLoops.getIterateBlock());
     {
       // 2. Emit the body of the outer loop nest.
+      SmallVector<Value, 4> resultIndices;
+      for (int i = 0; i < nOuterLoops; ++i)
+        resultIndices.emplace_back(outerLoops.getInductionVar(i));
 
-      // 2.1 Define spatial loops
-      int nSpatialLoops = resultShape.size() - batchRank;
-      BuildKrnlLoop spatialLoops(rewriter, loc, nSpatialLoops);
-      spatialLoops.createDefineAndOptimizeOp();
-      for (int i = batchRank; i < resultShape.size(); ++i)
-        spatialLoops.pushBounds(0, alloc, i);
+      // 2.1 Emit: R[n][c][r1][r2] = negative_infinity;
+      Value identity;
+      if (resultElementType.isa<FloatType>()) {
+        identity = rewriter.create<ConstantOp>(
+            loc, FloatAttr::get(resultElementType,
+                     getIdentityValue<float, ONNXMaxPoolSingleOutOp>()));
+      } else if (resultElementType.isa<IntegerType>()) {
+        identity = rewriter.create<ConstantOp>(
+            loc, IntegerAttr::get(resultElementType,
+                     getIdentityValue<int, ONNXMaxPoolSingleOutOp>()));
+      } else {
+        emitError(loc, "unsupported element type");
+      }
+      rewriter.create<StoreOp>(loc, identity, alloc, resultIndices);
 
-      // 2.2 Emit loop nest over output spatial dimensions.
-      spatialLoops.createIterateOp();
-      rewriter.setInsertionPointToStart(spatialLoops.getIterateBlock());
+      // 2.2 Define inner loops.
+      int nInnerLoops = kernelShape.size();
+      BuildKrnlLoop innerLoops(rewriter, loc, nInnerLoops);
+      innerLoops.createDefineAndOptimizeOp();
+      //   for Kx = 0 .. KX
+      for (int i = 0; i < nInnerLoops; ++i)
+        innerLoops.pushBounds(0, kernelShape[i]);
+
+      // 2.3 Emit inner loop nest.
+      innerLoops.createIterateOp();
+      rewriter.setInsertionPointToStart(innerLoops.getIterateBlock());
       {
-        // 3. Emit the body of the spatial loop nest.
-        // 3.1 Emit: R[n][c][r1][r2] = negative_infinity;
-        SmallVector<Value, 4> resultIndices;
-        // n
-        resultIndices.emplace_back(outerLoops.getInductionVar(nIndex));
-        // c
-        resultIndices.emplace_back(outerLoops.getInductionVar(cIndex));
-        // rX
-        for (auto arg : spatialLoops.getIterateBlock()->getArguments())
-          resultIndices.emplace_back(arg);
-        // Store initializer value into output location.
-        Value identity;
-        if (resultElementType.isa<FloatType>()) {
-          identity = rewriter.create<ConstantOp>(
-              loc, FloatAttr::get(resultElementType,
-                       getIdentityValue<float, ONNXMaxPoolSingleOutOp>()));
-        } else if (resultElementType.isa<IntegerType>()) {
-          identity = rewriter.create<ConstantOp>(
-              loc, IntegerAttr::get(resultElementType,
-                       getIdentityValue<int, ONNXMaxPoolSingleOutOp>()));
-        } else {
-          emitError(loc, "unsupported element type");
-        }
-        rewriter.create<StoreOp>(loc, identity, alloc, resultIndices);
+        // 3. Emit inner loop body
+        // t = D[n][c][s1 * r1 + k1][s2 * r2 + k2];
+        // R[n][c][r1][r2] = max(R[n][c][r1][r2], t);
 
-        // 3.2 Define inner loops.
-        int nInnerLoops = kernelShape.size();
-        BuildKrnlLoop innerLoops(rewriter, loc, nInnerLoops);
-        innerLoops.createDefineAndOptimizeOp();
-        //   for Kx = 0 .. KX
-        for (int i = 0; i < nInnerLoops; ++i)
-          innerLoops.pushBounds(0, kernelShape[i]);
-
-        // 3.3 Emit inner loop nest.
-        innerLoops.createIterateOp();
-        rewriter.setInsertionPointToStart(innerLoops.getIterateBlock());
-
-        {
-          // 4. Emit inner loop body
-          // t = D[n][c][s1 * r1 + k1][s2 * r2 + k2];
-          // R[n][c][r1][r2] = max(R[n][c][r1][r2], t);
-
-          // 4.1 Prepare indices for accesing the data tensor.
-          SmallVector<Value, 4> dataIndices;
-          // n
-          dataIndices.emplace_back(outerLoops.getInductionVar(nIndex));
-          // c
-          dataIndices.emplace_back(outerLoops.getInductionVar(cIndex));
-          // sX * rX + kX
-          for (int i = 0; i < kernelShape.size(); ++i) {
-            Value spatialIndex = spatialLoops.getInductionVar(i);
-            // If strides are present then emit the correct access index.
-            if (stridesAttribute && strides[i] > 1) {
-              spatialIndex = rewriter.create<MulIOp>(loc,
-                  rewriter.create<ConstantIndexOp>(loc, strides[i]),
-                  spatialLoops.getInductionVar(i));
-            }
-            spatialIndex = rewriter.create<AddIOp>(
-                loc, spatialIndex, innerLoops.getInductionVar(i));
-            // If ceil mode is enabled, then the calculated access index may
-            // exceed its dimension. In such a case, we will use the maximum
-            // index, which causes multiple visits to the element of the
-            // maximum index.
-            // TODO: Avoid multiple visits.
-            if (ceilMode) {
-              auto inputIndex = rewriter.create<ConstantIndexOp>(
-                  loc, inputShape[batchRank + i] - 1);
-              auto greaterCondition = rewriter.create<CmpIOp>(
-                  loc, CmpIPredicate::sgt, spatialIndex, inputIndex);
-              spatialIndex = rewriter.create<SelectOp>(
-                  loc, greaterCondition, inputIndex, spatialIndex);
-            }
-            dataIndices.emplace_back(spatialIndex);
+        // 3.1 Prepare indices for accesing the data tensor.
+        SmallVector<Value, 4> dataIndices;
+        // Batch indices: n, c
+        int batchRank = 2;
+        for (int i = 0; i < batchRank; ++i)
+          dataIndices.emplace_back(outerLoops.getInductionVar(i));
+        // Spatial indices: sX * rX + kX
+        for (int i = batchRank; i < nOuterLoops; ++i) {
+          Value spatialIndex = outerLoops.getInductionVar(i);
+          // If strides are present then emit the correct access index.
+          if (stridesAttribute && strides[i - batchRank] > 1) {
+            spatialIndex = rewriter.create<MulIOp>(loc,
+                rewriter.create<ConstantIndexOp>(loc, strides[i - batchRank]),
+                outerLoops.getInductionVar(i));
           }
-
-          // 4.2 Do pooling.
-          auto loadData =
-              rewriter.create<LoadOp>(loc, inputOperand, dataIndices);
-          auto loadPartialResult =
-              rewriter.create<LoadOp>(loc, alloc, resultIndices);
-          Value result = mapToLowerScalarOp<ONNXMaxPoolSingleOutOp>(
-              op, resultElementType, {loadPartialResult, loadData}, rewriter);
-          // 4.3 Store computed value into output location.
-          rewriter.create<StoreOp>(loc, result, alloc, resultIndices);
+          spatialIndex = rewriter.create<AddIOp>(
+              loc, spatialIndex, innerLoops.getInductionVar(i - batchRank));
+          // If ceil mode is enabled, then the calculated access index may
+          // exceed its dimension. In such a case, we will use the maximum
+          // index, which causes multiple visits to the element of the
+          // maximum index.
+          // TODO: Avoid multiple visits.
+          if (ceilMode) {
+            auto inputIndex =
+                rewriter.create<ConstantIndexOp>(loc, inputShape[i] - 1);
+            auto greaterCondition = rewriter.create<CmpIOp>(
+                loc, CmpIPredicate::sgt, spatialIndex, inputIndex);
+            spatialIndex = rewriter.create<SelectOp>(
+                loc, greaterCondition, inputIndex, spatialIndex);
+          }
+          dataIndices.emplace_back(spatialIndex);
         }
+
+        // 3.2 Do pooling.
+        auto loadData = rewriter.create<LoadOp>(loc, inputOperand, dataIndices);
+        auto loadPartialResult =
+            rewriter.create<LoadOp>(loc, alloc, resultIndices);
+        Value result = mapToLowerScalarOp<ONNXMaxPoolSingleOutOp>(
+            op, resultElementType, {loadPartialResult, loadData}, rewriter);
+        rewriter.create<StoreOp>(loc, result, alloc, resultIndices);
       }
     }
     rewriter.replaceOp(op, alloc);
