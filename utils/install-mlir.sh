f856f84c556e1df5625c56ac7d1d8784c183a59b
Tian Jin <tjingrant@gmail.com>
2020-03-02 14:28:36 +0800

    Use llvm-project we know that works.

--- utils/install-mlir.sh
+++ utils/install-mlir.sh
@@ -1,4 +1,6 @@
 git clone https://github.com/llvm/llvm-project.git
+# Check out a specific branch that is known to work with ONNF.
+cd llvm-project && git checkout 391cc4dd41db934081c37ee523cb2149bf0e3a41 && cd ..
 mkdir llvm-project/build
 cd llvm-project/build
 cmake -G Ninja ../llvm \
