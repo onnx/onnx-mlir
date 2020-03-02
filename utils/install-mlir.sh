47831749ce6520416bdf69a0691e45c594e187a4
Tian Jin <tjingrant@gmail.com>
2020-03-03 00:18:59 +0800

    Use the exact commit id specified in clang-ykt/llvm-project.

--- utils/install-mlir.sh
+++ utils/install-mlir.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/llvm/llvm-project.git
 # Check out a specific branch that is known to work with ONNF.
-cd llvm-project && git checkout 476ca094c846a0b6d4d9f37710aba21a6b0b265a && cd ..
+cd llvm-project && git checkout 076475713c236081a3247a53e9dbab9043c3eac2 && cd ..
 mkdir llvm-project/build
 cd llvm-project/build
 cmake -G Ninja ../llvm \
