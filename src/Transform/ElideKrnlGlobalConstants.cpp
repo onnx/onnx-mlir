594eff46e9ffa6f312d239ee2abf0e16c853c36c
Tian Jin <tjingrant@gmail.com>
2020-04-27 16:41:12 +0800

    FunctionPass -> PassWrapper.

--- src/Transform/ElideKrnlGlobalConstants.cpp
+++ src/Transform/ElideKrnlGlobalConstants.cpp
@@ -36,13 +36,13 @@ class KrnlConstGlobalValueElision : public OpRewritePattern<KrnlGlobalOp> {
 public:
   using OpRewritePattern<KrnlGlobalOp>::OpRewritePattern;
 
-  LogicalResult matchAndRewrite(KrnlGlobalOp op,
-                                PatternRewriter &rewriter) const override {
+  LogicalResult matchAndRewrite(
+      KrnlGlobalOp op, PatternRewriter &rewriter) const override {
     auto loc = op.getLoc();
-    
+
     if (op.value().hasValue()) {
       auto newGlobalOp = rewriter.create<KrnlGlobalOp>(
-        loc, op.getResult().getType(), op.shape(), op.name(), nullptr);
+          loc, op.getResult().getType(), op.shape(), op.name(), nullptr);
       rewriter.replaceOp(op, newGlobalOp.getResult());
     }
 
@@ -54,7 +54,7 @@ public:
  *  Function pass that performs constant value elision of Krnl globals.
  */
 class ElideConstGlobalValuePass
-    : public mlir::FunctionPass<ElideConstGlobalValuePass> {
+    : public PassWrapper<ElideConstGlobalValuePass, FunctionPass> {
 public:
   void runOnFunction() override {
     auto function = getFunction();
@@ -63,7 +63,7 @@ public:
     OwningRewritePatternList patterns;
     patterns.insert<KrnlConstGlobalValueElision>(&getContext());
 
-    applyPatternsGreedily(function, patterns);
+    applyPatternsAndFoldGreedily(function, patterns);
   }
 };
 } // namespace
