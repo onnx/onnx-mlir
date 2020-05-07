32260d7c3023ca32bd8f2a800af8a16ef49847b4
Kevin O'Brien <caomhin@us.ibm.com>
2020-05-07 15:56:26 -0400

    Update build_onnx_mlir.sh

--- utils/build_onnx_mlir.sh
+++ utils/build_onnx_mlir.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Export environment variables pointing to LLVM-Projects.
-export LLVM_PROJ_SRC=/llvm-project/
-export LLVM_PROJ_BUILD=/llvm-project/build
+export LLVM_PROJ_SRC=$(pwd)/llvm-project/
+export LLVM_PROJ_BUILD=$(pwd)/llvm-project/build
 HOME=/
 PYENV_ROOT=$HOME/.pyenv
 PATH=$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH
 eval "$(pyenv init -)"
 pyenv shell 3.7.0
-cd build
-mkdir dlcpp-build && cd dlcpp-build
-cmake ../DLCpp
-make -j64
+mkdir onnx-mlir/build && cd onnx-mlir/build
+cmake ..
+cmake --build . --target onnx-mlir
 
-# Test
-make check-dlcpp
+# Run FileCheck tests:
+export LIT_OPTS=-v
+cmake --build . --target check-onnx-lit
