160bdd261312a19509b7ad5fb5fba3a1b9f03e12
Kevin O'Brien <caomhin@us.ibm.com>
2020-05-07 15:39:32 -0400

    Update install_deps.sh

--- utils/install_deps.sh
+++ utils/install_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # first install MLIR in llvm-project
-git clone https://DLCadmin:af52950e231dac7157c3d78d263d7468375fb102@github.com/llvm/llvm-project.git
+git clone https://git@github.com/llvm/llvm-project.git
 # Check out a specific branch that is known to work with ONNX MLIR.
 cd llvm-project && git checkout 076475713c236081a3247a53e9dbab9043c3eac2 && cd ..
 mkdir llvm-project/build
