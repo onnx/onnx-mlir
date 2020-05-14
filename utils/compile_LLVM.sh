eb9fa928ebf0f946bcbe2016d09a316dcbe92be9
Kevin O'Brien <caomhin@us.ibm.com>
2020-05-13 20:33:39 -0400

    Update compile_LLVM.sh

--- utils/compile_LLVM.sh
+++ utils/compile_LLVM.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# first install MLIR in llvm-project
-git clone https://DLCadmin:af52950e231dac7157c3d78d263d7468375fb102@github.com/llvm/llvm-project.git
-# Check out a specific branch that is known to work with ONNX MLIR.
-cd llvm-project && git checkout 076475713c236081a3247a53e9dbab9043c3eac2 && cd ..
-mkdir llvm-project/build
-cd llvm-project/build
+cmake -G Ninja ../llvm \
+   -DLLVM_ENABLE_PROJECTS=mlir \
+   -DLLVM_BUILD_EXAMPLES=ON \
+   -DLLVM_TARGETS_TO_BUILD="host" \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DLLVM_ENABLE_ASSERTIONS=ON \
+   -DLLVM_ENABLE_RTTI=ON
+
+cmake --build . --target -- ${MAKEFLAGS}
