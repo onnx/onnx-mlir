46b58a53cef9bc9cdcc49cab86e972c29c46047f
Kevin O'Brien <caomhin@us.ibm.com>
2020-05-20 16:07:58 -0400

    Update compile-onnx-mlir.sh

--- docker/compile-onnx-mlir.sh
+++ docker/compile-onnx-mlir.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# Export environment variables pointing to LLVM-Projects
+# Export environment variables pointing to LLVM-Projects.
 export LLVM_PROJ_SRC=$(pwd)/llvm-project/
 export LLVM_PROJ_BUILD=$(pwd)/llvm-project/build
-HOME=/
-PYENV_ROOT=$HOME/.pyenv
-PATH=$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH
-eval "$(pyenv init -)"
-# use python 3
-pyenv shell 3.7.0
+
 mkdir onnx-mlir/build && cd onnx-mlir/build
 cmake ..
 cmake --build . --target onnx-mlir
+
+# Run FileCheck tests:
+export LIT_OPTS=-v
+cmake --build . --target check-onnx-lit
