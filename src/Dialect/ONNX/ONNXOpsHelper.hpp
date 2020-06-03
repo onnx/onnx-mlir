ce1d742ace4199abe115303046cbb0f9905f0848
Tung D. Le <tung@jp.ibm.com>
2020-06-03 18:40:55 +0900

    Share type translation between op builder and onnx importer

--- src/Dialect/ONNX/ONNXOpsHelper.hpp
+++ src/Dialect/ONNX/ONNXOpsHelper.hpp
@@ -11,6 +11,10 @@
 #include "mlir/IR/AffineExpr.h"
 #include "mlir/IR/AffineMap.h"
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/Types.h"
+#include "mlir/IR/StandardTypes.h"
+
+#include "onnx/onnx_pb.h"
 
 using namespace mlir;
 
@@ -31,3 +35,6 @@ AffineMap getIdentityDimMap(Builder &builder);
 // - s2: stride
 // - s3: dilation
 AffineMap getConvDimMap(Builder &builder, bool ceilMode);
+
+mlir::Type convertONNXTypeToMLIRType(
+    mlir::OpBuilder &builder_, onnx::TensorProto_DataType onnxType);
