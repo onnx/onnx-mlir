fc14f6031f9dd56d05b4bf93e75186dc7c56273a
Alexandre Eichenberger <alexe@us.ibm.com>
2020-06-04 14:34:07 -0400

    working

--- src/Transform/ONNX/ONNXConstProp.cpp
+++ src/Transform/ONNX/ONNXConstProp.cpp
@@ -24,9 +24,165 @@
 using namespace mlir;
 
 namespace {
-/// Include the patterns defined in the Declarative Rewrite framework.
+
+////////////////////////////////////////////////////////////////////////////////
+// Code to perform constant propagation in presence of broadcast.
+////////////////////////////////////////////////////////////////////////////////
+
+// Template to generate binary operation results. It takes as inupt
+// the element type as well as the two element attributes for the
+// operation, and return the result of the operation, also as an
+// attribute.
+
+template <typename OP>
+Attribute ComputeConstProppElementwiseBinary(PatternRewriter &rewriter,
+    Type elementType, Attribute &lhsAttr, Attribute &secondAttr) {
+  llvm_unreachable("unkonwn operation");
+}
+
+template <>
+Attribute ComputeConstProppElementwiseBinary<ONNXAddOp>(
+    PatternRewriter &rewriter, Type elementType, Attribute &lhsAttr,
+    Attribute &secondAttr) {
+  if (elementType.isa<FloatType>()) {
+    double lhsVal = lhsAttr.cast<FloatAttr>().getValueAsDouble();
+    double rhsVal = secondAttr.cast<FloatAttr>().getValueAsDouble();
+    double res = lhsVal + rhsVal;
+    //printf("  %f + %f -> %f\n", lhsVal, rhsVal, res);
+    // Could use the APFloat interface to emulate the results, are ok to simply
+    // perform them in the highest possible precision.
+    return rewriter.getFloatAttr(elementType, res);
+  }
+  if (elementType.isa<IntegerType>()) {
+    uint64_t lhsVal = lhsAttr.cast<IntegerAttr>().getInt();
+    uint64_t rhsVal = secondAttr.cast<IntegerAttr>().getInt();
+    uint64_t res = lhsVal + rhsVal;
+    //printf("  %llu + %llu -> %llu\n", (unsigned long long) lhsVal, (unsigned long long) rhsVal, (unsigned long long) res);
+    return rewriter.getIntegerAttr(elementType, lhsVal + rhsVal);
+  }
+  llvm_unreachable("constant propagation for AddOp: unkonwn data type");
+}
+
+// Recursively process one dimension in the rank of the two references. There
+// can be one of 3 cases.
+// 1) We have fully defined accesses for both operands, launch the computations.
+// 2) One of the two has a higher number of unprocessed ranks, which is hte case
+// when we have to broadcast the whole lower-dim reference with respect to the
+// other. Iterate over each value of the higher ranked reference, keeping the
+// reference of the lower ranked reference constant.
+// 3) Both references have the same rank, we still do broadcast if one of the
+// dimension size is equal to 1.
+
+template <typename ElementwiseBinaryOp>
+void RecurseConstProppElementwiseBinary(PatternRewriter &rewriter,
+    std::vector<Attribute> &resVector, DenseElementsAttr &lhsAttr,
+    DenseElementsAttr &rhsAttr, SmallVector<uint64_t, 4> &lhsIndices,
+    SmallVector<uint64_t, 4> &rhsIndices, int lhsFreeRank, int rhsFreeRank) {
+  //printf("recurse with free %d/%d\n", lhsFreeRank, rhsFreeRank);
+  if (lhsFreeRank == 0) {
+    // Fully defined ranks.
+    assert(
+        rhsFreeRank == 0 && "expect both to recurse to zero at the same time");
+    auto lhsElementAttr = lhsAttr.getValue(ArrayRef<uint64_t>(lhsIndices));
+    auto rhsElementAttr = rhsAttr.getValue(ArrayRef<uint64_t>(rhsIndices));
+    auto elementaryType = lhsAttr.getType().getElementType();
+    auto res = ComputeConstProppElementwiseBinary<ElementwiseBinaryOp>(
+        rewriter, elementaryType, lhsElementAttr, rhsElementAttr);
+    resVector.emplace_back(res);
+  } else if (lhsFreeRank > rhsFreeRank) {
+    // Initial broadcast from lhs.
+    auto lhsShape = lhsAttr.getType().getShape();
+    int lhsRank = lhsShape.size();
+    int lhsIndex = lhsRank - lhsFreeRank;
+    int lhsSize = lhsAttr.getType().getShape()[lhsIndex];
+    for (int i = 0; i < lhsSize; ++i) {
+      lhsIndices[lhsIndex] = i;
+      RecurseConstProppElementwiseBinary<ElementwiseBinaryOp>(rewriter,
+          resVector, lhsAttr, rhsAttr, lhsIndices, rhsIndices, lhsFreeRank - 1,
+          rhsFreeRank);
+    }
+  } else if (lhsFreeRank < rhsFreeRank) {
+    // Initial broadcast from rhs.
+    auto rhsShape = rhsAttr.getType().getShape();
+    int rhsRank = rhsShape.size();
+    int rhsIndex = rhsRank - rhsFreeRank;
+    int rhsSize = rhsAttr.getType().getShape()[rhsIndex];
+    for (int i = 0; i < rhsSize; ++i) {
+      rhsIndices[rhsIndex] = i;
+      RecurseConstProppElementwiseBinary<ElementwiseBinaryOp>(rewriter,
+          resVector, lhsAttr, rhsAttr, lhsIndices, rhsIndices, lhsFreeRank,
+          rhsFreeRank - 1);
+    }
+  } else {
+    // No initial broadcast, but if one element has size 1 and the other is
+    // greater than one, then we also have broadcast.
+    auto lhsShape = lhsAttr.getType().getShape();
+    int lhsRank = lhsShape.size();
+    int lhsIndex = lhsRank - lhsFreeRank;
+    int lhsSize = lhsAttr.getType().getShape()[lhsIndex];
+    auto rhsShape = rhsAttr.getType().getShape();
+    int rhsRank = rhsShape.size();
+    int rhsIndex = rhsRank - rhsFreeRank;
+    int rhsSize = rhsAttr.getType().getShape()[rhsIndex];
+    assert((lhsSize == 1 || rhsSize == 1 || lhsSize == rhsSize) &&
+           "incompatible sizes");
+    int size = std::max(lhsSize, rhsSize);
+    lhsIndices[lhsIndex] = rhsIndices[rhsIndex] = 0;
+    for (int i = 0; i < size; ++i) {
+      if (lhsSize > 1)
+        lhsIndices[lhsIndex] = i;
+      if (rhsSize > 1)
+        rhsIndices[rhsIndex] = i;
+      RecurseConstProppElementwiseBinary<ElementwiseBinaryOp>(rewriter,
+          resVector, lhsAttr, rhsAttr, lhsIndices, rhsIndices, lhsFreeRank - 1,
+          rhsFreeRank - 1);
+    }
+  }
+}
+
+// Process the constant operands, perform the operation with broadcast, and
+// generate the new constant operation.
+template <typename ElementwiseBinaryOp>
+DenseElementsAttr ConstPropElementwiseBinary(PatternRewriter &rewriter,
+    Value &resOperand, Attribute &lhsAttr, Attribute &rhsAttr) {
+  DenseElementsAttr lhsDenseAttr =
+      lhsAttr.dyn_cast_or_null<mlir::DenseElementsAttr>();
+  DenseElementsAttr rhsDenseAttr =
+      rhsAttr.dyn_cast_or_null<mlir::DenseElementsAttr>();
+  assert((lhsDenseAttr && lhsDenseAttr) && "expected dense attributes");
+  assert(
+      resOperand.getType().isa<RankedTensorType>() && "expected ranked tensor");
+  ShapedType resType = resOperand.getType().cast<RankedTensorType>();
+  auto lhsRank = lhsDenseAttr.getType().getShape().size();
+  auto rhsRank = rhsDenseAttr.getType().getShape().size();
+  SmallVector<uint64_t, 4> lhsIndices(lhsRank, 0);
+  SmallVector<uint64_t, 4> rhsIndices(rhsRank, 0);
+  std::vector<Attribute> resVector;
+  RecurseConstProppElementwiseBinary<ElementwiseBinaryOp>(rewriter, resVector,
+      lhsDenseAttr, rhsDenseAttr, lhsIndices, rhsIndices, lhsRank, rhsRank);
+  ArrayRef<Attribute> resRef(resVector);
+  return DenseElementsAttr::get(resType, resRef);
+}
+
+  // Function called by the rules to generate the proper added constants
+DenseElementsAttr ConstPropForAddOfTwoConst(PatternRewriter &rewriter,
+    Value resOperand, Attribute &v1, Attribute &v2) {
+  return ConstPropElementwiseBinary<mlir::ONNXAddOp>(
+      rewriter, resOperand, v1, v2);
+}
+
+  
+////////////////////////////////////////////////////////////////////////////////
+// Pattern definition.
+////////////////////////////////////////////////////////////////////////////////
+
 #include "src/Transform/ONNX/ONNXConstProp.inc"
 
+  
+////////////////////////////////////////////////////////////////////////////////
+// Code to manage the pass.
+////////////////////////////////////////////////////////////////////////////////
+  
 struct ConstPropONNXToONNXPass
     : public PassWrapper<ConstPropONNXToONNXPass, FunctionPass> {
   void runOnFunction() final;
@@ -55,3 +211,4 @@ std::unique_ptr<mlir::Pass> mlir::createConstPropONNXToONNXPass() {
 
 static PassRegistration<ConstPropONNXToONNXPass> pass("constprop-onnx",
     "ConstProp ONNX operations into composition of other ONNX operations.");
+
--- test/mlir/onnx/onnx_constprop.mlir
+++ test/mlir/onnx/onnx_constprop.mlir
@@ -21,16 +21,70 @@ func @test_add_constant_2(%arg0 : tensor<3xf32>) -> tensor<3xf32> {
   // CHECK-NEXT: [[ADD:%.+]] =  "onnx.Add"(%arg0, [[CONST]]) : (tensor<3xf32>, tensor<3xf32>) -> tensor<3xf32>
 }
 
-/// change (x+c1)+c2 to x+(c1+c2)
-// CHECK-LABEL: @test_add_constant_3(%arg0: tensor<3xf32>) -> tensor<3xf32> 
-func @test_add_constant_3(%arg0 : tensor<3xf32>) -> tensor<3xf32> {
-  %0 = "onnx.Constant"() {value = dense<[0.0, 1.0, 2.0]> : tensor<3xf32>} : () -> tensor<3xf32>
-  %1 = "onnx.Constant"() {value = dense<[10.0, 11.0, 12.0]> : tensor<3xf32>} : () -> tensor<3xf32>
-  %2 = "onnx.Add"(%0, %arg0) : (tensor<3xf32> , tensor<3xf32>) -> tensor<3xf32>
-  %3 = "onnx.Add"(%1, %2) : (tensor<3xf32> , tensor<3xf32>) -> tensor<3xf32>
-  "std.return"(%3) : (tensor<3xf32>) -> ()
-  // CHECK-NEXT: [[CONST1:%.+]] = "onnx.Constant"() {value = dense<[0.000000e+00, 1.000000e+00, 2.000000e+00]> : tensor<3xf32>} : () -> tensor<3xf32>
-  // CHECK-NEXT: [[CONST2:%.+]] = "onnx.Constant"() {value = dense<[1.000000e+01, 1.100000e+01, 1.200000e+01]> : tensor<3xf32>} : () -> tensor<3xf32>
-  // CHECK-NEXT: [[ADD1:%.+]] = "onnx.Add"([[CONST1]], [[CONST2]]) : (tensor<3xf32>, tensor<3xf32>) -> tensor<3xf32>
-  // CHECK-NEXT: [[ADD2:%.+]] = "onnx.Add"(%arg0, [[ADD1]]) : (tensor<3xf32>, tensor<3xf32>) -> tensor<3xf32>
+/// Change (x+c1)+c2 to x+(c1+c2)
+// CHECK-LABEL: @test_add_constant_3(%arg0: tensor<3xi32>) -> tensor<3xi32> 
+func @test_add_constant_3(%arg0 : tensor<3xi32>) -> tensor<3xi32> {
+  %0 = "onnx.Constant"() {value = dense<[0, 1, 2]> : tensor<3xi32>} : () -> tensor<3xi32>
+  %1 = "onnx.Constant"() {value = dense<[10, 11, 12]> : tensor<3xi32>} : () -> tensor<3xi32>
+  %2 = "onnx.Add"(%0, %arg0) : (tensor<3xi32> , tensor<3xi32>) -> tensor<3xi32>
+  %3 = "onnx.Add"(%1, %2) : (tensor<3xi32> , tensor<3xi32>) -> tensor<3xi32>
+  "std.return"(%3) : (tensor<3xi32>) -> ()
+  // CHECK-NEXT: [[CONST1:%.+]] = "onnx.Constant"() {value = dense<[10, 12, 14]> : tensor<3xi32>} : () -> tensor<3xi32>
+  // CHECK-NEXT: [[ADD1:%.+]] = "onnx.Add"(%arg0, [[CONST1]]) : (tensor<3xi32>, tensor<3xi32>) -> tensor<3xi32>
+}
+
+/// Same test as above, but with a use of an intermediary result
+/// change (x+c1)+c2 + (x+c1) to x+(c1+c2) + (x+c1)
+// CHECK-LABEL: @test_add_constant_4(%arg0: tensor<3xi32>) -> tensor<3xi32> 
+func @test_add_constant_4(%arg0 : tensor<3xi32>) -> tensor<3xi32> {
+  %0 = "onnx.Constant"() {value = dense<[0, 1, 2]> : tensor<3xi32>} : () -> tensor<3xi32>
+  %1 = "onnx.Constant"() {value = dense<[10, 11, 12]> : tensor<3xi32>} : () -> tensor<3xi32>
+  %2 = "onnx.Add"(%0, %arg0) : (tensor<3xi32> , tensor<3xi32>) -> tensor<3xi32>
+  %3 = "onnx.Add"(%1, %2) : (tensor<3xi32> , tensor<3xi32>) -> tensor<3xi32>
+  %4 = "onnx.Add"(%2, %3) : (tensor<3xi32> , tensor<3xi32>) -> tensor<3xi32>
+  "std.return"(%4) : (tensor<3xi32>) -> ()
+  // CHECK-NEXT: [[CONST1:%.+]] = "onnx.Constant"() {value = dense<[0, 1, 2]> : tensor<3xi32>} : () -> tensor<3xi32>
+  // CHECK-NEXT: [[ADD1:%.+]] = "onnx.Add"(%arg0, [[CONST1]]) : (tensor<3xi32>, tensor<3xi32>) -> tensor<3xi32>
+  // CHECK-NEXT: [[CONST2:%.+]] = "onnx.Constant"() {value = dense<[10, 12, 14]> : tensor<3xi32>} : () -> tensor<3xi32>
+  // CHECK-NEXT: [[ADD2:%.+]] = "onnx.Add"(%arg0, [[CONST2]]) : (tensor<3xi32>, tensor<3xi32>) -> tensor<3xi32>
+  // CHECK-NEXT: [[ADD3:%.+]] = "onnx.Add"([[ADD1]], [[ADD2]]) : (tensor<3xi32>, tensor<3xi32>) -> tensor<3xi32>
+}
+
+/// Test broadcast 1 -> 2d
+
+// CHECK-LABEL: @test_broadcast_1(%arg0: tensor<3x2xi32>) -> tensor<3x2xi32>
+func @test_broadcast_1(%arg0: tensor<3x2xi32>) -> tensor<3x2xi32> {
+  %0 = "onnx.Constant"() {value = dense<[1]> : tensor<1xi32>} : () -> tensor<1xi32>
+  %1 = "onnx.Constant"() {value = dense<[[2, 3], [4, 5], [6, 7]]> : tensor<3x2xi32>} : () -> tensor<3x2xi32>
+  %2 = "onnx.Add"(%0, %1) : (tensor<1xi32> , tensor<3x2xi32>) -> tensor<3x2xi32>
+  %3 = "onnx.Add"(%2, %arg0) : (tensor<3x2xi32> , tensor<3x2xi32>) -> tensor<3x2xi32>
+  "std.return"(%3) : (tensor<3x2xi32>) -> ()
+  // CHECK-NEXT: [[CONST1:%.+]] = "onnx.Constant"() {value = dense<{{.}}[3, 4], [5, 6], [7, 8]]> : tensor<3x2xi32>} : () -> tensor<3x2xi32>
+  // CHECK-NEXT: [[ADD1:%.+]] = "onnx.Add"(%arg0, [[CONST1]]) : (tensor<3x2xi32>, tensor<3x2xi32>) -> tensor<3x2xi32>
+}
+
+/// Test broadcast 2d (size one) -> 2d
+
+// CHECK-LABEL: @test_broadcast_2(%arg0: tensor<3x2xi32>) -> tensor<3x2xi32>
+func @test_broadcast_2(%arg0: tensor<3x2xi32>) -> tensor<3x2xi32> {
+  %0 = "onnx.Constant"() {value = dense<[[1]]> : tensor<1x1xi32>} : () -> tensor<1x1xi32>
+  %1 = "onnx.Constant"() {value = dense<[[2, 3], [4, 5], [6, 7]]> : tensor<3x2xi32>} : () -> tensor<3x2xi32>
+  %2 = "onnx.Add"(%0, %1) : (tensor<1x1xi32> , tensor<3x2xi32>) -> tensor<3x2xi32>
+  %3 = "onnx.Add"(%2, %arg0) : (tensor<3x2xi32> , tensor<3x2xi32>) -> tensor<3x2xi32>
+  "std.return"(%3) : (tensor<3x2xi32>) -> ()
+  // CHECK-NEXT: [[CONST1:%.+]] = "onnx.Constant"() {value = dense<{{.}}[3, 4], [5, 6], [7, 8]]> : tensor<3x2xi32>} : () -> tensor<3x2xi32>
+  // CHECK-NEXT: [[ADD1:%.+]] = "onnx.Add"(%arg0, [[CONST1]]) : (tensor<3x2xi32>, tensor<3x2xi32>) -> tensor<3x2xi32>
+}
+
+/// check 1d -> 2d
+
+  // CHECK-LABEL: @test_broadcast_3(%arg0: tensor<3x2xi32>) -> tensor<3x2xi32>
+func @test_broadcast_3(%arg0 : tensor<3x2xi32>) -> tensor<3x2xi32> {
+  %0 = "onnx.Constant"() {value = dense<[[1], [2], [3]]> : tensor<3x1xi32>} : () -> tensor<3x1xi32>
+  %1 = "onnx.Constant"() {value = dense<[[10, 11], [21, 22], [31, 32]]> : tensor<3x2xi32>} : () -> tensor<3x2xi32>
+  %2 = "onnx.Add"(%0, %1) : (tensor<3x1xi32> , tensor<3x2xi32>) -> tensor<3x2xi32>
+  %3 = "onnx.Add"(%2, %arg0) : (tensor<3x2xi32> , tensor<3x2xi32>) -> tensor<3x2xi32>
+  "std.return"(%3) : (tensor<3x2xi32>) -> ()
+  // CHECK-NEXT: [[CONST1:%.+]] = "onnx.Constant"() {value = dense<{{.}}[11, 12], [23, 24], [34, 35]]> : tensor<3x2xi32>} : () -> tensor<3x2xi32>
+  // CHECK-NEXT: [[ADD1:%.+]] = "onnx.Add"(%arg0, [[CONST1]]) : (tensor<3x2xi32>, tensor<3x2xi32>) -> tensor<3x2xi32>
 }
