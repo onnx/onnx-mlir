ac497f274cb72ea65611e4a167737f80434a35f7
Tung D. Le <tung@jp.ibm.com>
2020-06-09 11:12:47 +0900

    Code style

--- src/Dialect/ONNX/ONNXOps.cpp
+++ src/Dialect/ONNX/ONNXOps.cpp
@@ -1573,29 +1573,29 @@ LogicalResult ONNXSqueezeOp::inferShapes() {
   int64_t inRank = operandTy.getRank();
 
   ArrayAttr axisAttrs = axesAttr();
-  SmallVector<int64_t, 4> axes;
-  if (axisAttrs) {
-    bool hasNegativeAxis = false;
-    for (auto axisAttr : axisAttrs.getValue()) {
-      int64_t axis = axisAttr.cast<IntegerAttr>().getInt();
-      if (axis < -inRank || axis >= inRank)
-        return emitError("Invalid axis value");
-      if (axis < 0) {
-        axis = inRank + axis;
-        hasNegativeAxis = true;
-      }
-      if (std::find(axes.begin(), axes.end(), axis) != axes.end())
-        return emitError("Duplicated axes");
-      axes.emplace_back(axis);
-    }
-    if (hasNegativeAxis) {
-      // Update axes attribute so that it contains only positive values.
-      auto builder = mlir::Builder(getContext());
-      ArrayRef<int64_t> defaultRefs(axes);
-      axesAttr(builder.getI64ArrayAttr(defaultRefs));
-    }
-  } else
+  if (!axisAttrs)
     return emitError("Axes attribute is required");
+
+  SmallVector<int64_t, 4> axes;
+  bool hasNegativeAxis = false;
+  for (auto axisAttr : axisAttrs.getValue()) {
+    int64_t axis = axisAttr.cast<IntegerAttr>().getInt();
+    if (axis < -inRank || axis >= inRank)
+      return emitError("Invalid axis value");
+    if (axis < 0) {
+      axis = inRank + axis;
+      hasNegativeAxis = true;
+    }
+    if (std::find(axes.begin(), axes.end(), axis) != axes.end())
+      return emitError("Duplicated axes");
+    axes.emplace_back(axis);
+  }
+  if (hasNegativeAxis) {
+    // Update axes attribute so that it contains only positive values.
+    auto builder = mlir::Builder(getContext());
+    ArrayRef<int64_t> defaultRefs(axes);
+    axesAttr(builder.getI64ArrayAttr(defaultRefs));
+  }
 
   SmallVector<int64_t, 4> dims;
   for (int i = 0; i < inRank; ++i) {
