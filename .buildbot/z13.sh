f4831064482bab23b050974efc957b110a9654d7
Tian Jin <tjingrant@gmail.com>
2020-06-19 08:13:45 +0800

    Update z13.sh

--- .buildbot/z13.sh
+++ .buildbot/z13.sh
@@ -70,5 +70,5 @@ LLVM_PROJ_BUILD=${LLVM_PROJECT_ROOT}/build      \
 cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} .. \
 
 make -j$(nproc) onnx-mlir
-VERBOSE=1 LIT_OPTS=-v make -j$(nproc) check-onnx-lit
+LIT_OPTS=-v make -j$(nproc) check-onnx-lit
 RUNTIME_DIR=$(pwd)/lib make -j$(nproc) check-onnx-backend
