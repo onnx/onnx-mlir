7d913891b568b3bd3cf24ebd7408aab037e3e0d3
Chun-Wei Chen <jacky82226@gmail.com>
2020-07-29 07:53:26 -0700

    remove spaces

--- utils/install-onnx-mlir.cmd
+++ utils/install-onnx-mlir.cmd
@@ -8,7 +8,6 @@ md onnx-mlir\build
 cd onnx-mlir\build
 call cmake -G "Visual Studio 16 2019" -A x64 -T host=x64 -DLLVM_EXTERNAL_LIT="%root_dir%\llvm-project\build\Release\bin\llvm-lit.py" -DCMAKE_BUILD_TYPE=Release ..
 call cmake --build . --config Release --target onnx-mlir -- /m
-
 if [ $? -ne 0 ]; then
   echo "build onnx-mlir failed."
   exit 1
@@ -17,7 +16,6 @@ fi
 REM Run FileCheck tests
 set LIT_OPTS=-v
 call cmake --build . --config Release --target check-onnx-lit
-
 if [ $? -ne 0 ]; then
   echo "FileCheck tests for onnx-mlir failed."
   exit 1
