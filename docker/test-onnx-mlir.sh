238fd3a812f38e8645e586715c18a1ef5b2de622
Nathaniel McVicar <namcvica@microsoft.com>
2020-08-13 12:22:27 -0700

    build dependencies for tests

--- docker/test-onnx-mlir.sh
+++ docker/test-onnx-mlir.sh
@@ -2,10 +2,15 @@
 
 pwd
 ls -al
+
+# Install prereqs
+pip install -e onnx-mlir/third_party/onnx
+
 cd onnx-mlir/build
 
 # Run end-to-end tests:
 cmake --build . --target check-onnx-backend
 
 # Run unit tests:
+cmake --build .
 cmake --build . --target test
