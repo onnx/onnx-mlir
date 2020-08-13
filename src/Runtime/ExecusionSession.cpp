9f41a3606f6cbdf152689920f6e2d238dc16b898
Chun-Wei Chen <jacky82226@gmail.com>
2020-08-13 08:18:07 -0700

    use libs from windows.h instead of dlfcn.h

--- src/Runtime/ExecusionSession.cpp
+++ src/Runtime/ExecusionSession.cpp
@@ -21,14 +21,35 @@ namespace onnx_mlir {
 ExecutionSession::ExecutionSession(
     std::string sharedLibPath, std::string entryPointName) {
   // Adapted from https://www.tldp.org/HOWTO/html_single/C++-dlopen/.
+  #if _WIN32
+  _sharedLibraryHandle = LoadLibrary(sharedLibPath.c_str());
+  #else  
   _sharedLibraryHandle = dlopen(sharedLibPath.c_str(), RTLD_LAZY);
+  #endif
   if (!_sharedLibraryHandle) {
     std::stringstream errStr;
+    #if _WIN32
+    errStr << GetLastError() << std::endl;
+    #else
     errStr << "Cannot open library: " << dlerror() << std::endl;
+    #endif
     throw std::runtime_error(errStr.str());
   }
 
   // Reset errors.
+  #if _WIN32
+  GetLastError();
+  _entryPointFunc =
+      (entryPointFuncType)GetProcAddress((HMODULE)_sharedLibraryHandle, entryPointName.c_str());
+  auto dlsymError = GetLastError();
+  if (dlsymError) {
+    std::stringstream errStr;
+    errStr << "Cannot load symbol '" << entryPointName << "': " << dlsymError
+           << std::endl;
+    FreeLibrary((HMODULE)_sharedLibraryHandle);
+    throw std::runtime_error(errStr.str());
+  }  
+  #else
   dlerror();
   _entryPointFunc =
       (entryPointFuncType)dlsym(_sharedLibraryHandle, entryPointName.c_str());
@@ -39,7 +60,8 @@ ExecutionSession::ExecutionSession(
            << std::endl;
     dlclose(_sharedLibraryHandle);
     throw std::runtime_error(errStr.str());
-  }
+  }  
+  #endif
 }
 
 std::vector<std::unique_ptr<RtMemRef>> ExecutionSession::run(
@@ -59,5 +81,11 @@ std::vector<std::unique_ptr<RtMemRef>> ExecutionSession::run(
   return std::move(outs);
 }
 
-ExecutionSession::~ExecutionSession() { dlclose(_sharedLibraryHandle); }
+ExecutionSession::~ExecutionSession() {
+  #if _WIN32
+  FreeLibrary((HMODULE)_sharedLibraryHandle);
+  #else
+  dlclose(_sharedLibraryHandle);
+  #endif
+  }
 } // namespace onnx_mlir
