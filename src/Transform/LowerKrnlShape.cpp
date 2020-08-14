c945faf64a1c272fa2adf90d696f7e899ca0164d
Doru Bercea <gheorghe-teod.bercea@ibm.com>
2020-08-14 18:50:40 -0400

    Add lowering function.

--- src/Transform/LowerKrnlShape.cpp
+++ src/Transform/LowerKrnlShape.cpp
@@ -25,13 +25,14 @@ namespace {
 /*!
  *  RewritePattern that replaces:
  *    %0 = alloc(%d) : memref<?x10x<type>, #map>
- *    %1 = krnl.dim(%0, 0) : memref<?x10x<type>>
- *    %2 = krnl.dim(%0, 1) : memref<?x10x<type>>
- *    %3 = add %1, %2
+ *    %1 = krnl.shape(%0) : memref<?x10x<type>> -> !shape.shape
  *  with:
  *    %0 = alloc(%d) : memref<?x10x<type>, #map>
- *    %2 = constant 10 : index
- *    %3 = add %d, %2
+ *    %c0 = constant 0 : index
+ *    %1 = krnl.dim(%0, %c0) : memref<?x10x<type>, #map>, index
+ *    %c1 = constant 1 : index
+ *    %2 = krnl.dim(%0, %c1) : memref<?x10x<type>, #map>, index
+ *    %shape = shape.from_extents %1, %2
  */
 
 class LowerKrnlShape : public OpRewritePattern<KrnlShapeOp> {
@@ -41,53 +42,31 @@ public:
   LogicalResult matchAndRewrite(
       KrnlShapeOp krnlShapeOp, PatternRewriter &rewriter) const override {
     auto loc = krnlShapeOp.getLoc();
+    auto rank =
+        convertToMemRefType(krnlShapeOp.alloc().getType()).getShape().size();
 
-    printf("Bla \n");
+    SmallVector<mlir::Value, 4> fromExtentsOpOperands;
+    for (int idx = 0; idx < rank; idx++) {
+      auto index = rewriter.create<ConstantOp>(loc,
+          rewriter.getIntegerAttr(rewriter.getIndexType(), idx));
+      auto operand =
+          rewriter.create<KrnlDimOp>(
+              loc, rewriter.getIndexType(), krnlShapeOp.alloc(), index);
+      fromExtentsOpOperands.emplace_back(operand);
+    }
 
-    // // If index is not constant, return failure.
-    // // TODO: support dynamic index case.
-    // ConstantOp indexOp =
-    //     dyn_cast<ConstantOp>(krnlDimOp.index().getDefiningOp());
-    // if (!indexOp)
-    //   return failure();
-
-    // // Get the integer value of the index.
-    // int64_t index = indexOp.getAttrOfType<IntegerAttr>("value").getInt();
-
-    // printf("Index is %d\n", index);
-
-    // // Get defining operation for the MemRef argument.
-    // AllocOp allocOp =
-    //     dyn_cast<AllocOp>(krnlDimOp.alloc().getDefiningOp());
-    // auto memRefShape =
-    //     convertToMemRefType(allocOp.getResult().getType()).getShape();
-    // auto rank =  memRefShape.size();
-    // assert(index >= 0 && index < rank && "Index must be in bounds");
-
-    // Value result;
-    // if (memRefShape[index] > -1) {
-    //   // If dimension is static, then we can just emit the constant value.
-    //   result = rewriter.create<ConstantOp>(loc,
-    //       rewriter.getIntegerAttr(rewriter.getIndexType(),
-    //           memRefShape[index]));
-    // } else {
-    //   // If dimension is dynamic we need to return the input alloc Value which
-    //   // corresponds to it.
-    //   int64_t dynDimIdx = getAllocArgIndex(allocOp, index);
-    //   assert(dynDimIdx >= 0 && dynDimIdx < allocOp.getOperands().size() &&
-    //       "Dynamic index outside range of alloc argument list.");
-
-    //   result = allocOp.getOperands()[dynDimIdx];
-    // }
-
-    // rewriter.replaceOp(krnlDimOp, result);
+    auto fromExtentsOp =
+        rewriter.create<mlir::shape::FromExtentsOp>(
+            loc, rewriter.getType<mlir::shape::ShapeType>(),
+            fromExtentsOpOperands);
+    rewriter.replaceOp(krnlShapeOp, fromExtentsOp.getResult());
 
     return success();
   }
 };
 
 /*!
- *  Function pass that disconnects krnl.dim emission from its MemRef alloc.
+ *  Function pass that emits the shape of a MemRef.
  */
 class LowerKrnlShapePass
     : public PassWrapper<LowerKrnlShapePass, FunctionPass> {
