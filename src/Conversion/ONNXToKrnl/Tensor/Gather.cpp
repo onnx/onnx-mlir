7a74ef053f3d337c1cd647529a805bb345092fb9
Alexandre Eichenberger <alexe@us.ibm.com>
2020-09-04 17:57:39 -0400

    added tests

--- src/Conversion/ONNXToKrnl/Tensor/Gather.cpp
+++ src/Conversion/ONNXToKrnl/Tensor/Gather.cpp
@@ -21,8 +21,7 @@ struct ONNXGatherOpLowering : public ConversionPattern {
     ONNXGatherOpAdaptor operandAdaptor(operands);
     ONNXGatherOp gatherOp = llvm::dyn_cast<ONNXGatherOp>(op);
     auto loc = op->getLoc();
-    printf("hi alex: start\n");
-    // get input operands, shapes, and rank
+        // get input operands, shapes, and rank
     Value data = operandAdaptor.data();
     auto dataShape = data.getType().cast<MemRefType>().getShape();
     int64_t dataRank = dataShape.size();
@@ -45,12 +44,8 @@ struct ONNXGatherOpLowering : public ConversionPattern {
             out[ii + jj + kk] = data[ii + (indices[jj],) + kk]
     */
     // Define loops and iteration trip counts (equivalent to size of output)
-    printf("hi alex, got info axis %d and with sized data %d, indices %d, and "
-           "output %d\n",
-        (int)axisIndex, (int)dataRank, (int)indicesRank, (int)outputRank);
     std::vector<Value> originalLoops;
     defineLoops(rewriter, loc, originalLoops, outputRank);
-    printf("hi alex, created loop vars\n");
     KrnlIterateOperandPack pack(rewriter, originalLoops);
     int iIndexStart = 0;
     for (int ii = 0; ii < axisIndex; ++ii)
@@ -63,7 +58,6 @@ struct ONNXGatherOpLowering : public ConversionPattern {
     int kIndexStart = jIndexStart + indicesRank - (axisIndex+1);
     for (int kk = axisIndex + 1; kk < dataRank; ++kk)
       addDimensionToPack(rewriter, loc, pack, data, kk);
-    printf("hi alex, created %d loops\n", (int)outputRank);
     // Insert an allocation and deallocation for the result of this operation.
     Value alloc;
     bool insertDealloc = checkInsertDealloc(op);
@@ -72,12 +66,10 @@ struct ONNXGatherOpLowering : public ConversionPattern {
           insertAllocAndDealloc(outputMemRefType, loc, rewriter, insertDealloc);
     else
       return emitError(loc, "unsupported dynamic dimensions");
-    printf("hi alex, created alloc\n");
 
     // Create the loops
     auto iterateOp = rewriter.create<KrnlIterateOp>(loc, pack);
     Block &iterationBlock = iterateOp.bodyRegion().front();
-    printf("hi alex, created iterate\n");
 
     // Now perform the insertions into the body of the just generated loops.
     // Insert instructions inside the KernelIterateOp body.
@@ -91,9 +83,7 @@ struct ONNXGatherOpLowering : public ConversionPattern {
           iterationBlock.getArguments()[jIndexStart + j]);
     auto indexValInteger =
         rewriter.create<AffineLoadOp>(loc, indices, indicesMemRefVal);
-    printf("hi alex, created load indices\n");
     auto indexVal = rewriter.create<IndexCastOp>(loc, indexValInteger, rewriter.getIndexType());
-    printf("hi alex, created casted load indices\n");
 
     // Then read input data into DataVal: first add ii's.
     SmallVector<Value, 4> dataMemRefVal;
@@ -109,15 +99,12 @@ struct ONNXGatherOpLowering : public ConversionPattern {
           iterationBlock.getArguments()[kIndexStart + k]);
     }
     auto dataVal = rewriter.create<AffineLoadOp>(loc, data, dataMemRefVal);
-    printf("hi alex, created load data\n");
 
     // Then store the value in the output.
     SmallVector<Value, 4> outputMemRefVal;
     for (int n = 0; n < iterationBlock.getArguments().size(); ++n)
       outputMemRefVal.emplace_back(iterationBlock.getArguments()[n]);
-
     rewriter.create<AffineStoreOp>(loc, dataVal, alloc, outputMemRefVal);
-    printf("hi alex, created store output\n");
 
     rewriter.replaceOp(op, alloc);
 
