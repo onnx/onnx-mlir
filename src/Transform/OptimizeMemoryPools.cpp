ed19ce3e4f5a3934c09d0cbbcc84314afd1bf1a3
Doru Bercea <gheorghe-teod.bercea@ibm.com>
2020-10-14 16:06:41 -0400

    Compact memory pool.

--- src/Transform/OptimizeMemoryPools.cpp
+++ src/Transform/OptimizeMemoryPools.cpp
@@ -24,17 +24,6 @@
 using namespace mlir;
 namespace {
 
-// /// Retrieve function which contains the current operation.
-// FuncOp getContainingFunction(KrnlGetRefOp op) {
-//   Operation *parentFuncOp = op.getParentOp();
-
-//   // While parent is not a FuncOp and its cast to a FuncOp is null.
-//   while (!llvm::dyn_cast_or_null<FuncOp>(parentFuncOp))
-//     parentFuncOp = parentFuncOp->getParentOp();
-
-//   return cast<FuncOp>(parentFuncOp);
-// }
-
 /// Get the AllocOp of the current GetRef.
 AllocOp getAllocOfGetRef(KrnlGetRefOp *getRef) {
   auto parentBlock = getRef->getOperation()->getBlock();
@@ -61,6 +50,21 @@ int64_t getAllocGetRefNum(AllocOp *allocOp) {
   });
 
   return numGetRefs;
+}
+
+/// Get the total size in bytes used by the getref operations associated
+/// with a given memory pool.
+int64_t getAllocGetRefTotalSize(AllocOp *allocOp) {
+  auto parentBlock = allocOp->getOperation()->getBlock();
+
+  int64_t totalSize = 0;
+  parentBlock->walk([&totalSize, allocOp](KrnlGetRefOp op) {
+    auto result = allocOp->getResult();
+    if (op.getOperands()[0] == result)
+      totalSize += getMemRefSizeInBytes(op.getResult());
+  });
+
+  return totalSize;
 }
 
 // Check if this value is an argument of one of the blocks nested
@@ -90,39 +94,40 @@ std::vector<Operation *> getGetRefStores(KrnlGetRefOp *getRef) {
   std::vector<Operation *> stores;
 
   parentBlock->walk([&stores, getRef](StoreOp op) {
-    bool opUsesGetRef = false;
     for (const auto &operand : op.getOperands())
       if (operand == getRef->getResult())
-        opUsesGetRef = true;
-
-    if (opUsesGetRef)
-      stores.emplace_back(op);
+        stores.emplace_back(op);
   });
 
   parentBlock->walk([&stores, getRef](AffineStoreOp op) {
-    bool opUsesGetRef = false;
     for (const auto &operand : op.getOperands())
       if (operand == getRef->getResult())
-        opUsesGetRef = true;
-
-    if (opUsesGetRef)
-      stores.emplace_back(op);
+        stores.emplace_back(op);
   });
 
   // The list contains at least one use.
   return stores;
 }
 
+/// Returns a list of krnl.getref operations in the current block
+/// that use the memory pool.
+std::vector<KrnlGetRefOp> getAllGetRefsForAlloc(AllocOp *allocOp) {
+  auto parentBlock = allocOp->getOperation()->getBlock();
+  std::vector<KrnlGetRefOp> getRefs;
+
+  parentBlock->walk([&getRefs, allocOp](KrnlGetRefOp op) {
+    if (op.getOperands()[0] == allocOp->getResult())
+      getRefs.emplace_back(op);
+  });
+
+  // The list contains at least one use.
+  return getRefs;
+}
+
 bool getRefUsesAreDisjoint(
     KrnlGetRefOp firstGetRef, KrnlGetRefOp secondGetRef) {
   // Return variable.
   bool refsUseIsDisjoint = true;
-
-  // printf("\n\n==============START LOOP=============\n");
-  // printf("First:\n");
-  // firstGetRef.dump();
-  // printf("Second:\n");
-  // secondGetRef.dump();
 
   // Compute all the stores into the second getref.
   std::vector<Operation *> allStores = getGetRefStores(&secondGetRef);
@@ -135,9 +140,6 @@ bool getRefUsesAreDisjoint(
     std::vector<Value> operandList;
     operandList.emplace_back(store->getOperands()[0]);
 
-    // printf("Current STORE: \n");
-    // store->dump();
-
     // Construct the list of Values on which the current AllocOp depends on.
     llvm::SetVector<Operation *> dependentOps;
     while (operandList.size() > 0) {
@@ -148,22 +150,17 @@ bool getRefUsesAreDisjoint(
       if (dependentOps.count(definingOperation) == 0) {
         // Add value to dependent values list.
         dependentOps.insert(definingOperation);
-        // TODO: remove debug print.
-        // definingOperation->dump();
 
         if (llvm::dyn_cast<AffineLoadOp>(definingOperation) ||
             llvm::dyn_cast<LoadOp>(definingOperation)) {
           // Check that the MemRef operand of this store operation is
           // not the firstGetRef.
           Value memRefOperand = definingOperation->getOperands()[0];
-          // printf("Is block argument = %d\n", isBlockArgument(firstGetRef, memRefOperand));
           if (!isBlockArgument(firstGetRef, memRefOperand)) {
             Operation *operandDefinition = memRefOperand.getDefiningOp();
             KrnlGetRefOp storeGetRefOperand =
                 llvm::dyn_cast<KrnlGetRefOp>(operandDefinition);
 
-            // printf("Defining operation for the 2nd operand of the store:\n");
-            // operandDefinition->dump();
             if (storeGetRefOperand && firstGetRef == storeGetRefOperand) {
               refsUseIsDisjoint = false;
             }
@@ -180,18 +177,15 @@ bool getRefUsesAreDisjoint(
       operandList.erase(operandList.begin());
     }
 
-    // printf("===========================\n");
-
     // Exit if use is not disjoint.
     if (!refsUseIsDisjoint)
       break;
   }
 
-  // printf("==============DONE LOOP=============\n");
   return refsUseIsDisjoint;
 }
 
-bool getRefUsesAreMutuallyDisjoin(
+bool getRefUsesAreMutuallyDisjoint(
     KrnlGetRefOp firstGetRef, KrnlGetRefOp secondGetRef) {
   return getRefUsesAreDisjoint(firstGetRef, secondGetRef) &&
     getRefUsesAreDisjoint(secondGetRef, firstGetRef);
@@ -207,8 +201,6 @@ public:
 
   LogicalResult matchAndRewrite(
       KrnlGetRefOp firstGetRef, PatternRewriter &rewriter) const override {
-    auto loc = firstGetRef.getLoc();
-
     auto memRefType = convertToMemRefType(firstGetRef.getResult().getType());
     auto memRefShape = memRefType.getShape();
 
@@ -269,20 +261,93 @@ public:
     // first getref. This means that for any store to the secondGetRef, the
     // value stored does not involve a load from the firstGetRef.
     bool refsUseIsDisjoint =
-        getRefUsesAreMutuallyDisjoin(firstGetRef, secondGetRef);
-    // printf("============== DONE ANALYSIS =============\n");
+        getRefUsesAreMutuallyDisjoint(firstGetRef, secondGetRef);
 
     if (!refsUseIsDisjoint)
       return failure();
 
-    printf("==============Found a replacement!=============\n");
-    firstGetRef.dump();
-    secondGetRef.dump();
-    printf("==============END ANALYSIS=============\n");
-
     // A suitable replacement has been found, perform replacement, replace
     // second getref with first getref.
     rewriter.replaceOp(secondGetRef, firstGetRef.getResult());
+
+    return success();
+  }
+};
+
+class KrnlCompactStaticMemoryPools : public OpRewritePattern<AllocOp> {
+public:
+  using OpRewritePattern<AllocOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(
+      AllocOp allocOp, PatternRewriter &rewriter) const override {
+    auto loc = allocOp.getLoc();
+
+    auto memPoolType = convertToMemRefType(allocOp.getResult().getType());
+    auto memPoolShape = memPoolType.getShape();
+
+    // Only handle alloc ops that return a constant shaped MemRef.
+    if (!hasAllConstantDimensions(memPoolType))
+      return failure();
+
+    // Static memory pool type must be byte.
+    if (getMemRefEltSizeInBytes(memPoolType) != 1)
+      return failure();
+
+    // Rank of the static memory pool must be 1.
+    if (memPoolShape.size() != 1)
+      return failure();
+
+    // This is a memory pool if it is used by at least one getref.
+    if (getAllocGetRefNum(&allocOp) < 1)
+      return failure();
+
+    // Compute size of all krnl.getref operations that use this memory pool.
+    int64_t usedMemory = getAllocGetRefTotalSize(&allocOp);
+
+    assert(usedMemory <= memPoolShape[0] &&
+        "Used memory exceeds allocated memory.");
+
+    // Check if changes to the memory pool are required.
+    if (memPoolShape[0] == usedMemory)
+      return failure();
+
+    // Changes are required, memory pool needs to be compacted.
+    std::vector<KrnlGetRefOp> allGetRefs = getAllGetRefsForAlloc(&allocOp);
+
+    // Compute the shape of the new static memory pool.
+    SmallVector<int64_t, 1> newStaticMemPoolShape;
+    newStaticMemPoolShape.emplace_back(usedMemory);
+    auto newStaticMemPoolType =
+        MemRefType::get(newStaticMemPoolShape, rewriter.getIntegerType(8));
+
+    // We need to emit a new alloc of smaller size.
+    AllocOp newStaticMemPool = rewriter.create<AllocOp>(
+        loc, newStaticMemPoolType);
+    newStaticMemPool.getOperation()->moveBefore(allocOp);
+
+    // Each krnl.getref using the alloc needs to be re-emitted with the new
+    // static memory pool and the new offset.
+    int64_t currentOffset = 0;
+    for (auto getRefOp : allGetRefs) {
+      // Emit the current offset inside the static memory pool.
+      auto newOffset = rewriter.create<ConstantOp>(
+          loc, rewriter.getIntegerAttr(
+                 rewriter.getIntegerType(64), currentOffset));
+
+      // Create a new krnl.getref using the new memory pool and new offset.
+      auto newGetRefOp = rewriter.create<KrnlGetRefOp>(loc,
+          getRefOp.getResult().getType(), newStaticMemPool,
+          newOffset);
+      newGetRefOp.getOperation()->moveBefore(getRefOp);
+
+      // Update offset.
+      currentOffset += getMemRefSizeInBytes(getRefOp.getResult());
+
+      // Replace old krnl.getref with the new one.
+      rewriter.replaceOp(getRefOp, newGetRefOp.getResult());
+    }
+
+    rewriter.replaceOp(allocOp, newStaticMemPool.getResult());
 
     return success();
   }
@@ -297,22 +362,12 @@ public:
   void runOnFunction() override {
     auto function = getFunction();
 
-    // dynamicPoolMap.insert(
-    //     std::pair<FuncOp, std::unique_ptr<BlockToDynamicPool>>(
-    //         function, std::make_unique<BlockToDynamicPool>()));
-
-    // staticPoolMap.insert(std::pair<FuncOp, std::unique_ptr<BlockToStaticPool>>(
-    //     function, std::make_unique<BlockToStaticPool>()));
-
     ConversionTarget target(getContext());
     OwningRewritePatternList patterns;
-    patterns.insert<KrnlOptimizeStaticMemoryPools>(
+    patterns.insert<KrnlOptimizeStaticMemoryPools, KrnlCompactStaticMemoryPools>(
         &getContext());
 
     applyPatternsAndFoldGreedily(function, patterns);
-
-    // dynamicPoolMap.erase(function);
-    // staticPoolMap.erase(function);
   }
 };
 } // namespace
