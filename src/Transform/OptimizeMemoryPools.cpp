797b747d78d282f9ae032ea1cbafb47de2ef5bf3
Doru Bercea <gheorghe-teod.bercea@ibm.com>
2020-10-15 16:35:22 -0400

    Improve condition.

--- src/Transform/OptimizeMemoryPools.cpp
+++ src/Transform/OptimizeMemoryPools.cpp
@@ -184,8 +184,9 @@ bool getRefUsesAreDisjoint(
         // Add value to dependent values list.
         dependentOps.insert(definingOperation);
 
-        if (llvm::dyn_cast<AffineLoadOp>(definingOperation) ||
-            llvm::dyn_cast<LoadOp>(definingOperation)) {
+        if (definingOperation &&
+            (llvm::dyn_cast<AffineLoadOp>(definingOperation) ||
+             llvm::dyn_cast<LoadOp>(definingOperation))) {
           // Check that the MemRef operand of this load operation is
           // not in the firstGetRefList.
           Value loadOperand = definingOperation->getOperands()[0];
