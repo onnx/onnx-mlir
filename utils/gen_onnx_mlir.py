7a867bf1402afb9e36769abdc34da7f05007307a
Lester Szeto <lester.szeto@gmail.com>
2020-10-27 01:12:26 +0000

    Adding missing memref to ops where type is parsed from description
    Signed-off-by: Lester Szeto <lester.szeto@gmail.com>

--- utils/gen_onnx_mlir.py
+++ utils/gen_onnx_mlir.py
@@ -858,7 +858,7 @@ def get_onnx_mlir_types(schema, type_str_dict, input):
          if not input.typeStr in type_str_dict :
              # some arguments use type description directly
              # instead of constraint
-             return [parse_type_str(input.typeStr)]
+             return [parse_type_str(input.typeStr), "AnyMemRef"]
          else :
              return type_str_dict[input.typeStr]
     else :
@@ -1176,4 +1176,3 @@ if __name__ == '__main__':
         sys.stdout.write(Args.op_def.getvalue())
     if args.dry_run_op_build_table:
         sys.stdout.write(Args.op_importer.getvalue())
-
