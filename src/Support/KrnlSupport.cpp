31993369d933a85e18790f749c67667854d26088
Gheorghe-Teodor Bercea <gt.bercea@gmail.com>
2020-10-29 17:53:06 -0400

    Optimize static memory pools (#346)
    * Reorganize main function.
    
    * Follow review comments.
    
    * Emit constants are globals in Krnl and LLVM dialects.
    
    * Reuse MemRef locations in the memory pool.
    
    * Only print replacements.
    
    * Compact memory pool.
    
    * Format code.
    
    * Format code.
    
    * Allow MemRefs with different shapes but same footprint to share the same slot.
    
    * Format.
    
    * Allow multiple getrefs to use the same slot.
    
    * Format.
    
    * Improve condition.
    
    * Improve condition.
    
    * Exclude inner blocks from optimization.
    
    * Add live range analysis. Contains debug code.
    
    * Remove commented code.
    
    * Format.
    
    * Move check.
    
    * Some clean-up. Minor tweaks.
    
    * Add check for krnl.memcpy dependency. Refactor.
    
    * Format.
    
    * Support krnl.memcpy when computing live ranges.
    
    * Format.
    
    * Reinstate functions.
    
    * Add test.
    
    * Remove debug code.
    
    * Format.
    
    * Format.
    
    * Use SmallVectorImpl.
    
    * Add pattern application examples.
    
    * Format.
    
    * Add Support folder.
    
    * Move functions to KrnlSupport.
    
    * Format.
    
    * Eliminate dependency on ONNX Dialect when working on Krnl transformations.
    
    * Fix comment.
    
    Co-authored-by: Alexandre Eichenberger <alexe@us.ibm.com>

---
+++ src/Support/KrnlSupport.cpp
@@ -0,0 +1,377 @@
+//====---------- KrnlSupport.cpp - Krnl-level support functions -----------===//
+//
+// Copyright 2020 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains support code used at the level of the KRNL dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/Support/KrnlSupport.hpp"
+
+//===----------------------------------------------------------------------===//
+// Return various operations.
+//===----------------------------------------------------------------------===//
+
+/// Get the AllocOp of the current GetRef.
+AllocOp getAllocOfGetRef(KrnlGetRefOp *getRef) {
+  auto parentBlock = getRef->getOperation()->getBlock();
+
+  AllocOp alloc = nullptr;
+  parentBlock->walk([&alloc, getRef](AllocOp op) {
+    auto getRefAlloc = getRef->getOperands()[0];
+    if (op.getResult() == getRefAlloc)
+      alloc = op;
+  });
+
+  return alloc;
+}
+
+/// Return the top block.
+Block *getTopBlock(Operation *op) {
+  // Get current block as the first top block candidate.
+  Block *topBlock = op->getBlock();
+  Operation *parentBlockOp = topBlock->getParentOp();
+
+  while (!llvm::dyn_cast_or_null<FuncOp>(parentBlockOp)) {
+    topBlock = parentBlockOp->getBlock();
+    parentBlockOp = topBlock->getParentOp();
+  }
+
+  return topBlock;
+}
+
+/// Retrieve function which contains the current operation.
+FuncOp getContainingFunction(Operation *op) {
+  Operation *parentFuncOp = op->getParentOp();
+
+  // While parent is not a FuncOp and its cast to a FuncOp is null.
+  while (!llvm::dyn_cast_or_null<FuncOp>(parentFuncOp))
+    parentFuncOp = parentFuncOp->getParentOp();
+
+  return cast<FuncOp>(parentFuncOp);
+}
+
+/// Emit constant operation.
+Value emitConstantOp(
+    PatternRewriter &rewriter, Location loc, Type type, double value) {
+  Attribute constantAttr;
+
+  TypeSwitch<Type>(type)
+      .Case<Float16Type>(
+          [&](Type) { constantAttr = rewriter.getF16FloatAttr((float)value); })
+      .Case<Float32Type>(
+          [&](Type) { constantAttr = rewriter.getF32FloatAttr((float)value); })
+      .Case<Float64Type>(
+          [&](Type) { constantAttr = rewriter.getF64FloatAttr((float)value); })
+      .Case<IntegerType>([&](Type) {
+        auto width = type.cast<IntegerType>().getWidth();
+        if (width == 1) {
+          constantAttr = rewriter.getBoolAttr(false);
+        } else {
+          constantAttr =
+              rewriter.getIntegerAttr(type, APInt(width, (int64_t)value));
+        }
+      })
+      .Case<IndexType>([&](Type) {
+        constantAttr = rewriter.getIntegerAttr(type, (int64_t)value);
+      })
+      .Default([](Type) { llvm_unreachable("unsupported element type"); });
+  return rewriter.create<ConstantOp>(loc, constantAttr);
+}
+
+//===----------------------------------------------------------------------===//
+// Perform checks or get statistics about Krnl-level operations.
+//===----------------------------------------------------------------------===//
+
+/// Operation is a LoadOp or AffineLoadOp.
+bool isLoad(Operation *op) {
+  return llvm::dyn_cast_or_null<LoadOp>(op) ||
+         llvm::dyn_cast_or_null<AffineLoadOp>(op);
+}
+
+/// Operation is a StoreOp or AffineStoreOp.
+bool isStore(Operation *op) {
+  return llvm::dyn_cast_or_null<StoreOp>(op) ||
+         llvm::dyn_cast_or_null<AffineStoreOp>(op);
+}
+
+/// Operation is a KrnlMemcpyOp.
+bool isKrnlMemcpy(Operation *op) {
+  return llvm::dyn_cast_or_null<KrnlMemcpyOp>(op);
+}
+
+/// Checks if this operation loads/stores from the result of a specific getRef.
+/// A krnl.memcpy acts as both load and store.
+bool isLoadStoreForGetRef(KrnlGetRefOp getRef, Operation *op) {
+  auto result = getRef.getResult();
+  return (isLoad(op) && result == op->getOperands()[0]) ||
+         (isStore(op) && result == op->getOperands()[1]) ||
+         (isKrnlMemcpy(op) && (result == op->getOperands()[0] ||
+                                  result == op->getOperands()[1]));
+}
+
+/// Check if this value is an argument of one of the blocks nested around it.
+bool isBlockArgument(Operation *op, Value operand) {
+  // Parent operation of the current block.
+  Operation *parentBlockOp;
+  Block *currentBlock = op->getBlock();
+
+  do {
+    // Check the arguments of the current block.
+    for (auto arg : currentBlock->getArguments())
+      if (operand == arg)
+        return true;
+
+    parentBlockOp = currentBlock->getParentOp();
+    currentBlock = parentBlockOp->getBlock();
+
+  } while (!llvm::dyn_cast_or_null<FuncOp>(parentBlockOp));
+
+  return false;
+}
+
+/// Check if two GetRefs participate in the same krnl.memcpy.
+bool usedBySameKrnlMemcpy(
+    KrnlGetRefOp *firstGetRef, KrnlGetRefOp *secondGetRef) {
+  Block *topBlock = getTopBlock(firstGetRef->getOperation());
+
+  bool sameKrnlMemcpy = false;
+  topBlock->walk(
+      [&sameKrnlMemcpy, firstGetRef, secondGetRef](KrnlMemcpyOp memcpyOp) {
+        if ((memcpyOp.dest() == firstGetRef->getResult() &&
+                memcpyOp.src() == secondGetRef->getResult()) ||
+            (memcpyOp.dest() == secondGetRef->getResult() &&
+                memcpyOp.src() == firstGetRef->getResult()))
+          sameKrnlMemcpy = true;
+      });
+
+  return sameKrnlMemcpy;
+}
+
+/// Get the number of GetRef ops associated with this AllocOp.
+int64_t getAllocGetRefNum(AllocOp *allocOp) {
+  auto parentBlock = allocOp->getOperation()->getBlock();
+
+  int64_t numGetRefs = 0;
+  parentBlock->walk([&numGetRefs, allocOp](KrnlGetRefOp op) {
+    auto result = allocOp->getResult();
+    if (op.getOperands()[0] == result)
+      numGetRefs++;
+  });
+
+  return numGetRefs;
+}
+
+/// Check if an operation is in the top-level block of the function.
+bool opInTopLevelBlock(Operation *op) {
+  Block *currentBlock = op->getBlock();
+
+  // If the parent operation of the current block is a FuncOp then
+  // this operation is in the top-level block.
+  return llvm::dyn_cast_or_null<FuncOp>(currentBlock->getParentOp());
+}
+
+/// This function returns true if `beforeOp` is visited before `op` in a
+/// traversal of the provided block.
+bool opBeforeOp(Block *block, Operation *beforeOp, Operation *afterOp) {
+  bool beforeOpIsBefore = true;
+  bool beforeOpFound = false;
+  block->walk(
+      [&beforeOpIsBefore, &beforeOpFound, beforeOp, afterOp](Operation *op) {
+        if (op == beforeOp)
+          beforeOpFound = true;
+        else if (op == afterOp && !beforeOpFound)
+          beforeOpIsBefore = false;
+      });
+  return beforeOpIsBefore;
+}
+
+/// Check Alloc operation result is used by a krnl.getref.
+bool checkOpResultIsUsedByGetRef(AllocOp *allocOp) {
+  FuncOp function = getContainingFunction(allocOp->getOperation());
+
+  bool opIsUsedInGetRef = false;
+  function.walk([&opIsUsedInGetRef, allocOp](KrnlGetRefOp op) {
+    auto result = allocOp->getResult();
+    for (const auto &operand : op.getOperands())
+      if (operand == result)
+        opIsUsedInGetRef = true;
+  });
+
+  return opIsUsedInGetRef;
+}
+
+/// Check if all dimensions are known at compile time.
+bool hasAllConstantDimensions(MemRefType memRefType) {
+  auto memRefShape = memRefType.getShape();
+  for (int i = 0; i < memRefShape.size(); ++i)
+    if (memRefShape[i] < 0)
+      return false;
+  return true;
+}
+
+/// Get the MemRef element size in bytes.
+unsigned getMemRefEltSizeInBytes(MemRefType memRefType) {
+  auto elementType = memRefType.getElementType();
+
+  unsigned sizeInBits;
+  if (elementType.isIntOrFloat()) {
+    sizeInBits = elementType.getIntOrFloatBitWidth();
+  } else {
+    auto vectorType = elementType.cast<VectorType>();
+    sizeInBits =
+        vectorType.getElementTypeBitWidth() * vectorType.getNumElements();
+  }
+  return llvm::divideCeil(sizeInBits, 8);
+}
+
+/// Get the size of a static MemRef in bytes.
+int64_t getMemRefSizeInBytes(Value value) {
+  MemRefType memRefType = value.getType().dyn_cast<MemRefType>();
+  auto memRefShape = memRefType.getShape();
+  int64_t size = 1;
+  for (int i = 0; i < memRefShape.size(); i++)
+    size *= memRefShape[i];
+  size *= getMemRefEltSizeInBytes(memRefType);
+  return size;
+}
+
+/// Get the size of a dynamic MemRef in bytes.
+Value getDynamicMemRefSizeInBytes(
+    MemRefType type, Location loc, PatternRewriter &rewriter, AllocOp allocOp) {
+  // Initialize the size variable with the size in bytes of the type.
+  int64_t typeSize = getMemRefEltSizeInBytes(type);
+  Value result =
+      emitConstantOp(rewriter, loc, rewriter.getIndexType(), typeSize);
+
+  // Multiply all dimensions (constant and dynamic).
+  auto memRefShape = type.getShape();
+  auto rank = memRefShape.size();
+  int dynDimIdx = 0;
+  for (int idx = 0; idx < rank; ++idx) {
+    if (memRefShape[idx] < 0) {
+      // Dyanmic size.
+      auto dynamicDim = allocOp.getOperands()[dynDimIdx];
+      dynDimIdx++;
+      result = rewriter.create<MulIOp>(loc, result, dynamicDim);
+    } else {
+      // Static size.
+      auto staticDim = emitConstantOp(
+          rewriter, loc, rewriter.getIndexType(), memRefShape[idx]);
+      result = rewriter.create<MulIOp>(loc, result, staticDim);
+    }
+  }
+
+  return result;
+}
+
+/// Get the order number of the dynamic index passed as input.
+/// Example for the following shape:
+///   <1x2x?x3x?x4xf32>
+///
+/// getAllocArgIndex(<1x2x?x3x?x4xf32>, 2) will return 0.
+/// getAllocArgIndex(<1x2x?x3x?x4xf32>, 4) will return 1.
+///
+int64_t getAllocArgIndex(AllocOp allocOp, int64_t index) {
+  auto memRefShape =
+      allocOp.getResult().getType().dyn_cast<MemRefType>().getShape();
+  auto rank = memRefShape.size();
+
+  int dynDimIdx = 0;
+  for (int idx = 0; idx < rank; ++idx) {
+    if (memRefShape[idx] < 0) {
+      if (idx == index)
+        return dynDimIdx;
+      dynDimIdx++;
+    }
+  }
+
+  return -1;
+}
+
+//===----------------------------------------------------------------------===//
+// Live range analysis support.
+//===----------------------------------------------------------------------===//
+
+/// Returns the first operation in the live range of a getRef.
+Operation *getLiveRangeFirstOp(KrnlGetRefOp getRef) {
+  Block *topBlock = getTopBlock(getRef.getOperation());
+
+  Operation *firstLoadStore = nullptr;
+  topBlock->walk([&firstLoadStore, getRef](Operation *op) {
+    // If op is a Laod/Store, of any kind then assign it to lastLoadStore.
+    if (!firstLoadStore && isLoadStoreForGetRef(getRef, op))
+      firstLoadStore = op;
+  });
+
+  return firstLoadStore;
+}
+
+/// Returns the last operation in the live range of a getRef.
+Operation *getLiveRangeLastOp(KrnlGetRefOp getRef) {
+  Block *topBlock = getTopBlock(getRef.getOperation());
+
+  Operation *lastLoadStore = nullptr;
+  topBlock->walk([&lastLoadStore, getRef](Operation *op) {
+    // If op is a Laod/Store, of any kind then assign it to lastLoadStore.
+    if (isLoadStoreForGetRef(getRef, op))
+      lastLoadStore = op;
+  });
+
+  return lastLoadStore;
+}
+
+/// Check if an operation is in an existing live range.
+bool operationInLiveRange(
+    Operation *operation, std::vector<Operation *> liveRangeOpList) {
+  for (auto &op : liveRangeOpList) {
+    if (op == operation)
+      return true;
+  }
+  return false;
+}
+
+/// Function that returns the live range of a GetRef operation. The live
+/// range consists of all the operations in the in-order traversal of the
+/// source code between the first load/store instruction from that GetRef
+/// and the last load/store instruction from that GetRef.
+std::vector<Operation *> getLiveRange(KrnlGetRefOp getRef) {
+  std::vector<Operation *> operations;
+
+  auto topBlock = getTopBlock(getRef.getOperation());
+
+  // Determine last load/store from getRef.
+  Operation *lastLoadStore = getLiveRangeLastOp(getRef);
+
+  bool operationInLiveRange = false;
+  topBlock->walk([&operations, &operationInLiveRange, lastLoadStore, getRef](
+                     Operation *op) {
+    // If op is a Laod/Store, of any kind, then assign it to lastLoadStore.
+    if (isLoadStoreForGetRef(getRef, op) && !operationInLiveRange)
+      operationInLiveRange = true;
+
+    if (operationInLiveRange)
+      operations.emplace_back(op);
+
+    if (op == lastLoadStore)
+      operationInLiveRange = false;
+  });
+
+  return operations;
+}
+
+/// The live range is contained between firstOp and lastOp.
+bool liveRangeIsContained(Operation *firstOp, Operation *lastOp,
+    std::vector<Operation *> liveRangeOpList) {
+  Operation *liveRangeFirstOp = liveRangeOpList[0];
+  assert(liveRangeOpList.size() > 0 &&
+         "Live range empty but must have at least one element.");
+  Operation *liveRangeLastOp = liveRangeOpList[liveRangeOpList.size() - 1];
+
+  Block *topLevelBlock = getTopBlock(firstOp);
+
+  return opBeforeOp(topLevelBlock, firstOp, liveRangeFirstOp) &&
+         opBeforeOp(topLevelBlock, liveRangeLastOp, lastOp);
+}
