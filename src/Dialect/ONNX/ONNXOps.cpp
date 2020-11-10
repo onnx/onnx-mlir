2fb07e54da11cde6913d7e4d5f0f7696aa0451c1
Tian Jin <tjingrant@gmail.com>
2020-11-09 21:53:34 -0500

    Format code.

--- src/Dialect/ONNX/ONNXOps.cpp
+++ src/Dialect/ONNX/ONNXOps.cpp
@@ -3650,7 +3650,8 @@ LogicalResult ONNXLoopOp::inferShapes(
   auto builder = mlir::Builder(getContext());
   auto func = getLoopBodyFunc();
   auto &loopBody = func.getBody();
-  assert(loopBody.getNumArguments() >= 2 && "Loop body must take at least 2 inputs.");
+  assert(loopBody.getNumArguments() >= 2 &&
+         "Loop body must take at least 2 inputs.");
 
   // We proceed to set types for loop body function inputs.
   // Set type for iteration number (trip count):
@@ -3696,9 +3697,9 @@ LogicalResult ONNXLoopOp::inferShapes(
     std::get<0>(vFinalValToTy).setType(std::get<1>(vFinalValToTy));
   }
 
-  // For scan outputs, we set their shape to be the shape of the return values of
-  // the loop body function corresponding to scan outputs, but with an extra leading
-  // dimension.
+  // For scan outputs, we set their shape to be the shape of the return values
+  // of the loop body function corresponding to scan outputs, but with an extra
+  // leading dimension.
   for (auto vScanOutputValToTy : llvm::zip(scan_outputs(), bodyResScanTys)) {
     auto rankedScanTy =
         std::get<1>(vScanOutputValToTy).cast<RankedTensorType>();
