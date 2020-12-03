2306f1d16411f8e2387799b43c1ce8dc0a4a74b0
Kevin O'Brien <caomhin@us.ibm.com>
2020-12-03 02:07:08 +0000

    more reformatting
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps.cpp
+++ src/Dialect/ONNX/ONNXOps.cpp
@@ -3512,8 +3512,7 @@ mlir::Operation::result_range ONNXLoopOp::scan_outputs() {
 /// Infer the output shape of the ONNXCustomOp. This method is required by the
 /// shape inference interface.
 LogicalResult ONNXCustomOp::inferShapes(
-  std::function<void(mlir::FuncOp)> shapeInferenceFunc
-) {
+    std::function<void(mlir::FuncOp)> shapeInferenceFunc) {
   // getResult().setType(getOperand().getType());
   return success();
 }
