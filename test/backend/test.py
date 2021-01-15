1e0540580167cd1b76848fe95e7f7658866caa64
chentong <chentong@us.ibm.com>
2021-01-15 14:53:58 -0500

    backend test
    Signed-off-by: chentong <chentong@us.ibm.com>

--- test/backend/test.py
+++ test/backend/test.py
@@ -222,6 +222,25 @@ test_to_enable_static_dynamic = {
     "test_div_example_cpu": (test_static_dynamic,),
 
     # Dropout
+    "test_dropout_default_cpu": (test_static_dynamic,),
+    "test_dropout_default_ratio_cpu": (test_static_dynamic,),
+    #"test_dropout_default_mask_cpu": (test_static_dynamic,),
+    #"test_dropout_default_mask_ratio_cpu": (test_static_dynamic,),
+
+    # Error: input arrays contain a mixture of endianness configuration
+    #"test_training_dropout_default_cpu": (test_static_dynamic,),
+
+    #"test_training_dropout_default_mask_cpu": (test_static_dynamic,),
+
+    # Error: input arrays contain a mixture of endianness configuration
+    #"test_training_dropout_default_cpu": (test_static_dynamic,),
+
+    #"test_training_dropout_mask_cpu": (test_static_dynamic,),
+
+    # Error: input arrays contain a mixture of endianness configuration
+    #"test_training_dropout_zero_ratio_cpu": (test_static_dynamic,),
+
+    #"test_training_dropout_zero_ratio_mask_cpu": (test_static_dynamic,),
 
     # DynamicQuantizeLinear
 
