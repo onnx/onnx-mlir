365862ee4c1bac4d52899982c804e2a13e13f30d
Tian Jin <tjingrant@gmail.com>
2021-01-22 17:04:08 -0500

    Test DenseNet121.

--- test/backend/test.py
+++ test/backend/test.py
@@ -731,6 +731,7 @@ test_to_enable_static_dynamic = {
     "test_shufflenet_cpu": (test_static,),
     "test_resnet50_cpu": (test_static,),
     "test_vgg19_cpu": (test_static,),
+    "test_densenet121_cpu": (test_static,),
 }
 
 # test for static
