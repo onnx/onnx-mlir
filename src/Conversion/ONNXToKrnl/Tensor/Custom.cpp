faeeccad92e8c0c5b715bbe89b94395d5339ed77
Kevin O'Brien <caomhin@us.ibm.com>
2021-02-17 00:07:58 +0000

    lower to kernel, elementwise
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>

--- src/Conversion/ONNXToKrnl/Tensor/Custom.cpp
+++ src/Conversion/ONNXToKrnl/Tensor/Custom.cpp
@@ -12,6 +12,34 @@
 
 using namespace mlir;
 
+template <>
+struct ScalarOp<ONNXCustomOp> {
+  using FOp = KrnlCustomOp;
+  using IOp = KrnlCustomOp; 
+};
+
+
+template <>
+Value emitScalarOpFor<ONNXCustomOp>(ConversionPatternRewriter &rewriter, Location loc,
+    Operation *op, Type elementType, ArrayRef<Value> scalarOperands) {
+    auto funcAttr = op->getAttr("function_name");
+    llvm::StringRef funcName = funcAttr.cast<StringAttr>().getValue();
+    if (elementType.isa<IntegerType>()) {
+    return rewriter.create<ScalarIOp<ONNXCustomOp>>(
+        loc, elementType, 
+        scalarOperands[0], rewriter.getStringAttr(funcName));
+//        rewriter.getStringAttr(funcName),scalarOperands, mlir::None);
+  } else if (elementType.isa<FloatType>()) {
+    return rewriter.create<ScalarFOp<ONNXCustomOp>>(
+        loc, elementType,
+        scalarOperands[0], rewriter.getStringAttr(funcName));
+//        rewriter.getStringAttr(funcName), scalarOperands, mlir::None);
+  } else {
+    llvm_unreachable("unsupported element type");
+  }
+}
+
+
 struct ONNXCustomOpLowering : public ConversionPattern {
   ONNXCustomOpLowering(MLIRContext *ctx)
       : ConversionPattern(mlir::ONNXCustomOp::getOperationName(), 1, ctx) {}
@@ -35,8 +63,51 @@ static FuncOp findOrCreateFunction(PatternRewriter &rewriter,
 }
 
 
+
   LogicalResult matchAndRewrite(Operation *op, ArrayRef<Value> operands,
       ConversionPatternRewriter &rewriter) const final {
+    auto loc = op->getLoc();
+    auto X = operands[0];
+
+    // Insert an allocation and deallocation for the result of this operation.
+    auto memRefType = convertToMemRefType(*op->result_type_begin());
+
+    Value alloc;
+    bool insertDealloc = checkInsertDealloc(op);
+
+    if (hasAllConstantDimensions(memRefType))
+      alloc = insertAllocAndDealloc(memRefType, loc, rewriter, insertDealloc);
+    else
+      alloc =
+          insertAllocAndDealloc(memRefType, loc, rewriter, insertDealloc, X);
+
+    SmallVector<Value, 4> loopIVs;
+    // Only create krnl.iterate if one of the operands is not scalar tensor.
+    if (!hasAllScalarValues(operands)) {
+      // Create iterateOp & get block within iterate op.
+      BuildKrnlLoop loops(rewriter, loc, memRefType.getRank());
+      loops.createDefineAndIterateOp(X);
+      Block *iterationBlock = loops.getIterateBlock();
+
+      // Insert instructions inside the KernelIterateOp body.
+      rewriter.setInsertionPointToStart(iterationBlock);
+
+      // Handle the operation:
+      for (auto arg : iterationBlock->getArguments())
+        loopIVs.push_back(arg);
+    }
+
+    auto loadedVal = rewriter.create<KrnlLoadOp>(loc, X, loopIVs);
+    //auto loweredOpResult = rewriter.create<KrnlCustomOp>(
+    //    loc, memRefType.getElementType(),  {loadedVal}, mlir::None);
+    auto loweredOpResult = emitScalarOpFor<ONNXCustomOp>(
+        rewriter, loc, op, memRefType.getElementType(), {loadedVal});
+    // Store result in the resulting array.
+    rewriter.create<KrnlStoreOp>(loc, loweredOpResult, alloc, loopIVs);
+
+    rewriter.replaceOp(op, alloc);
+    return success();
+    /*
     ONNXCustomOpAdaptor operandAdaptor(operands);
     rewriter.startRootUpdate(op);
     rewriter.setInsertionPointAfter(op);
@@ -50,11 +121,11 @@ static FuncOp findOrCreateFunction(PatternRewriter &rewriter,
     llvm::StringRef fname = sref;
     MLIRContext* context = (op->getContext());
     OpBuilder builder(context);
-    auto it = mlir::IntegerType::get(64,context);
+    auto it = mlir::IntegerType::get(context,64);
     std::vector<int64_t> v = {4};
     llvm::ArrayRef<int64_t> shape(v);
     auto t = mlir::MemRefType::get(shape,it);
-  auto funcType = mlir::FunctionType::get({t}, {t}, context);
+  auto funcType = mlir::FunctionType::get(context,{t}, {t});
     auto function = findOrCreateFunction(rewriter, module, fname, funcType);
     function.dump();
     auto funcCallOp = rewriter.create<CallOp>(loc, function, operandAdaptor.input());
@@ -70,7 +141,7 @@ static FuncOp findOrCreateFunction(PatternRewriter &rewriter,
     //rewriter.replaceOpWithNewOp<CallOp>(
     //    op, op->getResult(0).getType(), operandAdaptor.input());
     rewriter.finalizeRootUpdate(op);
-    return success();
+    return success();*/
   }
 };
 
