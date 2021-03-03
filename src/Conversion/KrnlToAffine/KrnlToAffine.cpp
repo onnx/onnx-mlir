fe099c831aaa86a4a6b098676802c52012c905b6
Alexandre Eichenberger <alexe@us.ibm.com>
2021-03-03 11:55:20 -0500

    remove unrelated changes
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Conversion/KrnlToAffine/KrnlToAffine.cpp
+++ src/Conversion/KrnlToAffine/KrnlToAffine.cpp
@@ -14,23 +14,14 @@
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
-#include "mlir/Dialect/Vector/VectorOps.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/LoopUtils.h"
 
 #include "src/Dialect/Krnl/KrnlOps.hpp"
-#include "src/Dialect/ONNX/IndexExpr.hpp"
 #include "src/Pass/Passes.hpp"
 #include "src/Support/KrnlSupport.hpp"
 
-// EDSC intrinsics (which include all builder methods too).
-#include "mlir/Dialect/Affine/EDSC/Intrinsics.h"
-#include "mlir/Dialect/StandardOps/EDSC/Intrinsics.h"
-#include "mlir/Dialect/Vector/EDSC/Intrinsics.h"
-
-#include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/Types.h"
 using namespace mlir;
 
 namespace {
@@ -216,8 +207,8 @@ LogicalResult interpretOperation(Operation *op, OpBuilder &builder,
   } else if (auto unrollOp = dyn_cast_or_null<KrnlUnrollOp>(op)) {
     // Unroll the affine for loop fully.
     auto loopRef = unrollOp.loop();
-    LogicalResult res = loopUnrollFull(loopRefToOp[loopRef]);
-    assert(res.succeeded() && "full unrolling failed");
+    loopUnrollFull(loopRefToOp[loopRef]);
+
     opsToErase.insert(op);
     return success();
   }
@@ -290,280 +281,15 @@ public:
   }
 };
 
-//===----------------------------------------------------------------------===//
-// Krnl to Affine Rewrite Patterns: Krnl MatMul operation.
-//===----------------------------------------------------------------------===//
-
-using namespace mlir::edsc;
-using namespace mlir::edsc::ops;
-using namespace mlir::edsc::intrinsics;
-
-// KrnlMatmul will be lowered to vector and affine expressions
-class KrnlMatmulLowering : public OpRewritePattern<KrnlMatMulOp> {
-public:
-  using OpRewritePattern<KrnlMatMulOp>::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(
-      KrnlMatMulOp op, PatternRewriter &rewriter) const override {
-    Location loc = op.getLoc();
-    KrnlMatMulOpAdaptor operandAdaptor = KrnlMatMulOpAdaptor(op);
-
-    // Operands and types.
-    Type elementType =
-        operandAdaptor.A().getType().cast<MemRefType>().getElementType();
-    bool simdize = op.simdize();
-    // Init scope and emit constants.
-    ScopedContext scope(rewriter, op.getLoc());
-    IndexExprScope indexScope(&rewriter, op.getLoc());
-
-    // Generate the test for full/partial blocks
-    Value A(operandAdaptor.A()), B(operandAdaptor.B()), C(operandAdaptor.C());
-    MemRefBoundIndexCapture aBounds(A), bBounds(B), cBounds(C);
-    IndexExpr nBlock(cBounds.getDim(0)), mBlock(cBounds.getDim(1)),
-        kBlock(aBounds.getDim(1));
-    assert(nBlock.isLiteral() && "n dim expected to be compile time constant");
-    assert(mBlock.isLiteral() && "m dim expected to be compile time constant");
-    assert(kBlock.isLiteral() && "k dim expected to be compile time constant");
-    SymbolIndexExpr nUB(operandAdaptor.nUpperBound()),
-        mUB(operandAdaptor.mUpperBound()), kUB(operandAdaptor.kUpperBound()),
-        nGI(operandAdaptor.nGlobalIndex()), mGI(operandAdaptor.mGlobalIndex()),
-        kGI(operandAdaptor.kGlobalIndex());
-
-    nBlock.debugPrint("N block");
-    nUB.debugPrint("N UB");
-    nGI.debugPrint("N GI");
-
-    mBlock.debugPrint("M block");
-    mUB.debugPrint("M UB");
-    mGI.debugPrint("M GI");
-
-    kBlock.debugPrint("K block");
-    kUB.debugPrint("K UB");
-    kGI.debugPrint("K GI");
-
-    // Math for tiles:
-
-    // 1) Determine if the current tile is full. It is full if the begining of
-    // the tile (nGI) is smaller or equal to UB - bloc, namely
-    //   PredicateIndexExpr nIsFullTile = (nGI <= (nUB - nBlock));
-
-    // 2) Trip count in general: min(UB - GI, Block).
-    //   IndexExpr nTrip = IndexExpr::min(nUB - nGI, nBlock);
-
-    // 3) Trip counts for full tiles: block by definition.
-
-    // 4) Trip count for partial tiles: leftover = UB - GI in general. If UB is
-    // known at compile time, then without loss of generality, leftover = (UB-
-    // GI) % Block, and since GI is by definition a multiple of Block (GI is
-    // index at begining of tile), then leftover = UB % Block.
-    //   IndexExpr nPartialTrip = nUB.isLiteral() ? nUB % nBlock : nUB - nGI;
-
-    PredicateIndexExpr nIsFullTile = (nGI <= (nUB - nBlock));
-    PredicateIndexExpr mIsFullTile = (mGI <= (mUB - mBlock));
-    PredicateIndexExpr kIsFullTile = (kGI <= (kUB - kBlock));
-    PredicateIndexExpr fullTiles = (nIsFullTile & mIsFullTile) & kIsFullTile;
-
-    nIsFullTile.debugPrint("n full tile");
-    mIsFullTile.debugPrint("m full tile");
-    kIsFullTile.debugPrint("k full tile");
-    fullTiles.debugPrint("full tile");
-
-    using namespace edsc::op;
-
-    if (simdize) {
-      // SIMD code generator.
-      // clang-format off
-      genIfThenElseWithoutParams(rewriter, !fullTiles,
-        /* has some partial tiles */ [&](ValueRange) {
-        // Trip regardless of full/partial for N & K
-        IndexExpr nTrip = IndexExpr::min(nUB - nGI, nBlock);
-        IndexExpr kTrip = IndexExpr::min(kUB - kGI, kBlock);
-        // Test if SIMD dim (M) is full.
-        genIfThenElseWithoutParams(rewriter, mIsFullTile,
-          /* full SIMD */ [&](ValueRange) {
-          genSimd(rewriter, op, elementType, nTrip, mBlock, kTrip, false);
-        }, /* else partial SIMD */ [&](ValueRange) {
-          IndexExpr mPartialTrip = mUB.isLiteral() ? mUB % mBlock : mUB - mGI;
-          if (mPartialTrip.isLiteral() && mPartialTrip.getLiteral() >=2) {
-            // has a known trip count along the simd dimension of at least 2 
-            // elements, use simd again.
-            genSimd(rewriter, op, elementType, nTrip, mPartialTrip, kTrip, false);
-          } else {
-            genScalar(rewriter, op, elementType, nTrip, mPartialTrip, kTrip, false);
-          }
-        });
-      }, /* else full */ [&](ValueRange) {
-        genSimd(rewriter, op, elementType, nBlock, mBlock, kBlock, 
-            /*unroll&jam*/true);
-      });
-      // clang-format on
-    } else {
-      // Scalar code generator.
-      // clang-format off
-      genIfThenElseWithoutParams(rewriter, !fullTiles,
-        /* partial */ [&](ValueRange) {
-        IndexExpr nTrip = IndexExpr::min(nUB - nGI, nBlock);
-        IndexExpr mTrip = IndexExpr::min(mUB - mGI, mBlock);
-        IndexExpr kTrip = IndexExpr::min(kUB - kGI, kBlock);
-        genScalar(rewriter, op, elementType, nTrip, mTrip, kTrip, false);
-      }, /* else  full */ [&](ValueRange) {
-        genScalar(rewriter, op, elementType, nBlock, mBlock, kBlock, true);
-      });
-      // clang-format on
-    }
-
-    rewriter.eraseOp(op);
-    return success();
-  }
-
-private:
-  void genScalar(PatternRewriter &rewriter, KrnlMatMulOp op, Type elementType,
-      IndexExpr N, IndexExpr M, IndexExpr K, bool unrollJam) const {
-    // Get operands.
-    KrnlMatMulOpAdaptor operandAdaptor(op);
-    Value A(operandAdaptor.A()), B(operandAdaptor.B()), C(operandAdaptor.C());
-
-    // Get the EDSC variables, and loop dimensions.
-    AffineIndexedValue AA(A), BB(B), CC(C); // Obj we can load and store into.
-    MemRefType CTmpType = MemRefType::get({}, elementType);
-
-    // For i, j loops.
-    using namespace edsc::op;
-    Value zero = std_constant_index(0);
-    Value i, j;
-    // clang-format off
-    affineLoopNestBuilder({zero, zero}, {N.getValue(), M.getValue()}, 
-        {1, 1}, [&](ValueRange ivs) {
-      // Defines induction variables, and possibly initialize C.
-      i = ivs[0]; 
-      j = ivs[1];
-      // Alloc and init temp c storage.
-      Value TmpC = std_alloca(CTmpType);
-      AffineIndexedValue TTmpC(TmpC);
-      TTmpC() = CC(i, j);
-      // Sum over k.
-      affineLoopBuilder(zero, K.getValue(), 1, [&](Value k) {
-        TTmpC() = AA(i, k) * BB(k, j) + TTmpC();
-        //TTmpC() = std_fmaf(AA(i, k), BB(k, j), TTmpC());
-      });
-      // Store temp result into C(i, j)
-      CC(i, j) = TTmpC();
-    });
-    // clang-format on
-    if (unrollJam && M.isLiteral()) {
-      // Unroll and jam. Seems to support only one operation at this time.
-      auto lj = getForInductionVarOwner(j);
-      LogicalResult res = loopUnrollJamByFactor(lj, M.getLiteral());
-      assert(res.succeeded() && "failed to optimize");
-    }
-  }
-
-  void genSimd(PatternRewriter &rewriter, KrnlMatMulOp op, Type elementType,
-      IndexExpr nTrip, IndexExpr mTrip, IndexExpr kTrip, bool unrollJam) const {
-    // can simdize only if K is compile time
-    assert(mTrip.isLiteral() &&
-           "can only simdize with compile time blocking factor on simd axis");
-    // Get operands.
-    KrnlMatMulOpAdaptor operandAdaptor = KrnlMatMulOpAdaptor(op);
-    Value A = operandAdaptor.A();
-    Value B = operandAdaptor.B();
-    Value C = operandAdaptor.C();
-    MemRefType AType = A.getType().cast<MemRefType>();
-    MemRefType CType = C.getType().cast<MemRefType>();
-    // Find literal value for sizes of the array. These sizes are compile time
-    // constant since its related to the tiling size, decided by the compiler.
-    int64_t NLit = CType.getShape()[0];
-    int64_t MLit = CType.getShape()[1];
-    int64_t KLit = AType.getShape()[1];
-    // Typecast B, C to memrefs of K x vector<M>, N x vector<M>.
-    VectorType vecType = VectorType::get({MLit}, elementType);
-    MemRefType BVecType = MemRefType::get({KLit}, vecType);
-    MemRefType CVecType = MemRefType::get({NLit}, vecType);
-    MemRefType CTmpType = MemRefType::get({}, vecType);
-    Value BVec = vector_type_cast(BVecType, B);
-    Value CVec = vector_type_cast(CVecType, C);
-    // Get the EDSC variables, and loop dimensions.
-    AffineIndexedValue AA(A), BB(B), CC(C), BBVec(BVec), CCvec(CVec);
-    // Iterates over the I indices (j are simd dim).
-    Value ii;
-    using namespace edsc::op;
-    Value zero = std_constant_index(0);
-    // clang-format off
-    affineLoopBuilder(zero, nTrip.getValue(), 1, [&](Value i) {
-      ii = i; // Saved for unroll and jam.
-      // Alloca temp vector TmpC and save C(i)/0.0 into it.
-      Value TmpC = std_alloca(CTmpType);
-      AffineIndexedValue TTmpC(TmpC);
-      TTmpC() = CCvec(i);
-      // Sum over k.
-      affineLoopBuilder(zero, kTrip.getValue(), 1, [&](Value k) {
-        TTmpC() = vector_fma(vector_broadcast(vecType, AA(i, k)), BBVec(k), TTmpC());
-      });
-      // Store temp result into C(i)
-      Value tmpResults = TTmpC();
-      int64_t mTripLit = mTrip.getLiteral();
-      if (MLit != mTripLit) {
-        // create vector constant
-        SmallVector<int64_t, 8> mask;
-        for(int64_t i=0; i<MLit; i++) 
-          mask.emplace_back((i<mTripLit) ? i : MLit+i);
-        // permute
-        Value originalCvec = CCvec(i);
-        tmpResults = rewriter.create<vector::ShuffleOp>(op.getLoc(), 
-          tmpResults, originalCvec, mask);
-      }
-      CCvec(i) = tmpResults;
-    });
-    // clang-format on
-    if (unrollJam && nTrip.isLiteral()) {
-      // Unroll and jam. Seems to support only one operation at this time.
-      auto li = getForInductionVarOwner(ii);
-      LogicalResult res = loopUnrollJamByFactor(li, nTrip.getLiteral());
-      assert(res.succeeded() && "failed to optimize");
-    }
-  }
-
-  void genIfThenElseWithoutParams(PatternRewriter &rewriter,
-      IndexExpr condition, function_ref<void(ValueRange)> thenFn,
-      function_ref<void(ValueRange)> elseFn) const {
-    Block *ifBlock = rewriter.getInsertionBlock();
-    // Handle branches known at compile time.
-    if (condition.isLiteral()) {
-      if (condition.getLiteral() != 0) {
-        // Issue only the then path directly.
-        appendToBlock(ifBlock, [&](ValueRange args) { thenFn(args); });
-      } else {
-        appendToBlock(ifBlock, [&](ValueRange args) { elseFn(args); });
-      }
-      return;
-    }
-    // Split current block in the if-conditional block, and the end block.
-    auto opPosition = rewriter.getInsertionPoint();
-    Block *endBlock = rewriter.splitBlock(ifBlock, opPosition);
-    // Construct the empty Then / Else bock.
-    Block *thenBlock =
-        buildInNewBlock({}, [&](ValueRange args) { std_br(endBlock, {}); });
-    Block *elseBlock =
-        buildInNewBlock({}, [&](ValueRange args) { std_br(endBlock, {}); });
-    // Add the conditional to the If block
-    appendToBlock(ifBlock, [&](ValueRange args) {
-      std_cond_br(condition.getValue(), thenBlock, {}, elseBlock, {});
-    });
-    // Has to add the then/else code after fully creating the blocks
-    appendToBlock(thenBlock, [&](ValueRange args) { thenFn(args); });
-    appendToBlock(elseBlock, [&](ValueRange args) { elseFn(args); });
-  }
-};
-
 void ConvertKrnlToAffinePass::runOnFunction() {
   OpBuilder builder(&getContext());
   mlir::Operation *funcOp = getFunction();
 
   // Interpret krnl dialect operations while looping recursively through
-  // operations within the current function, note that erasing operations
-  // while iterating is tricky because it can invalidate the iterator, so we
-  // collect the operations to be erased in a small ptr set `opsToErase`, and
-  // only erase after iteration completes.
+  // operations within the current function, note that erasing operations while
+  // iterating is tricky because it can invalidate the iterator, so we collect
+  // the operations to be erased in a small ptr set `opsToErase`, and only erase
+  // after iteration completes.
   llvm::SmallDenseMap<Value, AffineForOp, 4> loopRefToOp;
   llvm::SmallPtrSet<Operation *, 4> opsToErase;
   if (failed(interpretOperation(funcOp, builder, loopRefToOp, opsToErase))) {
@@ -595,19 +321,19 @@ void ConvertKrnlToAffinePass::runOnFunction() {
   target.addIllegalOp<KrnlTerminatorOp>();
   // krnl.dim operations must be lowered prior to this pass.
   target.addIllegalOp<KrnlDimOp>();
-  target.addIllegalOp<KrnlMatMulOp>();
-  target.addLegalDialect<mlir::AffineDialect, mlir::StandardOpsDialect,
-      mlir::vector::VectorDialect>();
+  target.addLegalOp<AffineYieldOp>();
+  target.addLegalOp<AffineLoadOp>();
+  target.addLegalOp<AffineStoreOp>();
+  target.addLegalOp<LoadOp>();
+  target.addLegalOp<StoreOp>();
   OwningRewritePatternList patterns;
   patterns.insert<KrnlTerminatorLowering>(&getContext());
   patterns.insert<KrnlLoadLowering>(&getContext());
   patterns.insert<KrnlStoreLowering>(&getContext());
-  patterns.insert<KrnlMatmulLowering>(&getContext());
   DenseSet<Operation *> unconverted;
   if (failed(applyPartialConversion(
-          getFunction(), target, std::move(patterns), &unconverted))) {
+          getFunction(), target, std::move(patterns), &unconverted)))
     signalPassFailure();
-  }
 }
 } // namespace
 
