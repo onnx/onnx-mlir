07f91ca63d7b5e7bb55f40cb700e5722cd11e5a8
Kevin O'Brien <caomhin@us.ibm.com>
2021-04-07 22:04:18 +0000

    functions to get signatures
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>

---
+++ src/Runtime/InputSignature.h
@@ -0,0 +1,22 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===---- InputSignature.h - Get JSON input signature ---===//
+//
+// Copyright 2021 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains runtime API declarations to extract get the input
+// signature of the model as a JSON string 
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <stdint.h>
+
+extern "C" {
+char *inputSignature();
+}
---
+++ src/Runtime/OutputSignature.h
@@ -0,0 +1,22 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===---- OutputSignature.h - Get JSON output signature ---===//
+//
+// Copyright 2021 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains runtime API declarations to extract get the output
+// signature of the model as a JSON string 
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <stdint.h>
+
+extern "C" {
+char *outputSignature();
+}
