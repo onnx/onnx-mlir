1539889ae22d3c5653f22d2e01337ed60fb0697d
Stella Stamenova <stilis@microsoft.com>
2021-04-09 11:39:02 -0700

    Fix the windows build of onnx-mlir
    Signed-off-by: Stella Stamenova <stilis@microsoft.com>

--- src/MainUtils.cpp
+++ src/MainUtils.cpp
@@ -19,11 +19,12 @@
 #include <string>
 #include <vector>
 
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Program.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/IR/SymbolTable.h"
 #include "mlir/Pass/Pass.h"
-#include <llvm/Support/FileSystem.h>
-#include <llvm/Support/Program.h>
-#include <mlir/Dialect/LLVMIR/LLVMDialect.h>
-#include <mlir/IR/SymbolTable.h>
+#include "mlir/Target/LLVMIR.h"
 
 #include "src/ExternalUtil.hpp"
 #include "src/MainUtils.hpp"
