ba0044465b06abe703d0c41b817aa723833a97d5
chentong <chentong@us.ibm.com>
2021-05-03 14:19:04 -0400

    format
    Signed-off-by: chentong <chentong@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps.cpp
+++ src/Dialect/ONNX/ONNXOps.cpp
@@ -3539,11 +3539,11 @@ LogicalResult ONNXZipMapOp::inferShapes(
 }
 
 // New Ops from onnx1.8.1
-#define NOT_IMPLEMENTED_INFERSHAPE(T) \
-  LogicalResult T::inferShapes( \
-      std::function<void(mlir::Region &)> doShapeInference) { \
-    return emitError(NOT_IMPLEMENTED_MESSAGE); \
-  } 
+#define NOT_IMPLEMENTED_INFERSHAPE(T)                                          \
+  LogicalResult T::inferShapes(                                                \
+      std::function<void(mlir::Region &)> doShapeInference) {                  \
+    return emitError(NOT_IMPLEMENTED_MESSAGE);                                 \
+  }
 
 NOT_IMPLEMENTED_INFERSHAPE(ONNXAdagradOp);
 NOT_IMPLEMENTED_INFERSHAPE(ONNXAdamOp);
@@ -3555,7 +3555,6 @@ NOT_IMPLEMENTED_INFERSHAPE(ONNXLessOrEqualOp);
 NOT_IMPLEMENTED_INFERSHAPE(ONNXMomentumOp);
 NOT_IMPLEMENTED_INFERSHAPE(ONNXNegativeLogLikelihoodLossOp);
 NOT_IMPLEMENTED_INFERSHAPE(ONNXSoftmaxCrossEntropyLossOp);
-
 
 //===----------------------------------------------------------------------===//
 // Loop
