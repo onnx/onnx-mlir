8406f69e8fa04750d17b8cad7f12de7efcd10d6a
Rui Zhang <ruzhang@microsoft.com>
2021-05-12 17:29:40 +0000

    Set MNIST return value to interface with ctest properly
    1. Set MNIST host driver's return value so it properly interface with ctest.
    
    2. Run clang-format on previously added MNIST source files after noticing our onnx-mlir pipeline complains about the format of these files.

--- test/ApolloSDK/MNIST/NPU/MNIST_NPU_Program/DeviceCP/MnistClusterCommand.cpp
+++ test/ApolloSDK/MNIST/NPU/MNIST_NPU_Program/DeviceCP/MnistClusterCommand.cpp
@@ -2,8 +2,8 @@
 // Copyright (C) Microsoft Corporation. All rights reserved.
 //
 
-#include "Nepal/ArrayRef.h"
 #include "MnistClusterCommand.h"
+#include "Nepal/ArrayRef.h"
 #include "tcp_driver.tcp.h"
 
 #include <Commands/CommandRuntime.h>
@@ -23,56 +23,60 @@ REGISTER_CLUSTERCP_INTERFACE(Mnist::ClusterCP::Interface)
 
 namespace npl = Apollo::Nepal;
 namespace npli = Apollo::Nepal::Internal;
-namespace twf        = Trainwave::FirmwareSDK;
+namespace twf = Trainwave::FirmwareSDK;
 namespace twfCluster = Trainwave::FirmwareSDK::Cluster;
-namespace mmTileCP   = MaiaCompiler::GeneratedCode;
+namespace mmTileCP = MaiaCompiler::GeneratedCode;
 namespace twrProgram = Trainwave::FirmwareRT::Program;
 
-namespace Mnist::ClusterCP
-{
-  using DMemArray2D = npl::ArrayRef<2, npl::MemoryKind::DeviceMem, npl::ElementDataType::BFloat16, npl::FormatKind::Tile>;
-  
+namespace Mnist::ClusterCP {
+using DMemArray2D = npl::ArrayRef<2, npl::MemoryKind::DeviceMem,
+    npl::ElementDataType::BFloat16, npl::FormatKind::Tile>;
+
 // Relays the PipelinedMatMul task for the TileCP to execute.
 // Waits for the TileCP to finish before returning.
-void MnistClusterCommand::Execute(const MnistClusterCommand::Arguments& args)
-{
-    const auto NativeMatrixDim = Trainwave::FirmwareRT::GetHWInfo().GetNativeMatrixDim();
+void MnistClusterCommand::Execute(const MnistClusterCommand::Arguments &args) {
+  const auto NativeMatrixDim =
+      Trainwave::FirmwareRT::GetHWInfo().GetNativeMatrixDim();
 
-    npl::NotificationList done = npl::BuildNotificationList(npl::NotifConsumerKind::ClusterCP);
+  npl::NotificationList done =
+      npl::BuildNotificationList(npl::NotifConsumerKind::ClusterCP);
 
-    auto input1 = reinterpret_cast<KernelParams::ArrayRef const*>(args.params.data);
-    auto input2 = input1 + 1;
-    auto input3 = input1 + 2;
-    auto input4 = input1 + 3;
-    auto input5 = input1 + 4;
-    auto output = input1 + 5;
+  auto input1 =
+      reinterpret_cast<KernelParams::ArrayRef const *>(args.params.data);
+  auto input2 = input1 + 1;
+  auto input3 = input1 + 2;
+  auto input4 = input1 + 3;
+  auto input5 = input1 + 4;
+  auto output = input1 + 5;
 
-    DMemArray2D arg0({ 256, 256 }, input1->dramAddress);
-    DMemArray2D arg1({ 256, 256 }, input2->dramAddress);
-    DMemArray2D arg2({ 256, 256 }, input3->dramAddress);
-    DMemArray2D arg3({ 256, 256 }, input4->dramAddress);
-    DMemArray2D arg4({ 256, 256 }, input5->dramAddress);
-    DMemArray2D arg5({ 256, 256 }, output->dramAddress); // Output
+  DMemArray2D arg0({256, 256}, input1->dramAddress);
+  DMemArray2D arg1({256, 256}, input2->dramAddress);
+  DMemArray2D arg2({256, 256}, input3->dramAddress);
+  DMemArray2D arg3({256, 256}, input4->dramAddress);
+  DMemArray2D arg4({256, 256}, input5->dramAddress);
+  DMemArray2D arg5({256, 256}, output->dramAddress); // Output
 
-    mmTileCP::main_graph::Arguments cmdArgs
-      = {arg0, arg1, arg2, arg3, arg4, arg5, done};
+  mmTileCP::main_graph::Arguments cmdArgs = {
+      arg0, arg1, arg2, arg3, arg4, arg5, done};
 
-    const Apollo::Primitives::SemId controlSemId
-        = twf::SemManager::MapClusterSemOffset(twf::SemManager::GetNumClusterSem() - 1);
+  const Apollo::Primitives::SemId controlSemId =
+      twf::SemManager::MapClusterSemOffset(
+          twf::SemManager::GetNumClusterSem() - 1);
 
-    DebugPuts("DeviceCP: Calling TileCP...\n"); 
-    twfCluster::template CallTileCPAsync<mmTileCP::main_graph>(cmdArgs, controlSemId);
+  DebugPuts("DeviceCP: Calling TileCP...\n");
+  twfCluster::template CallTileCPAsync<mmTileCP::main_graph>(
+      cmdArgs, controlSemId);
 
-    DebugPuts("DeviceCP: Waiting for TileCP to finish...\n");
-    done[0].Wait();
+  DebugPuts("DeviceCP: Waiting for TileCP to finish...\n");
+  done[0].Wait();
 
-    DebugPuts("DeviceCP: Finishing TileCP...\n");
+  DebugPuts("DeviceCP: Finishing TileCP...\n");
 
-    // Write funcId to OutputCtrlMsgFifo to signal firmware complete
-    const uint32_t funcId = twf::GetCommandID<MnistClusterCommand, Interface>();
-    twrProgram::WriteMessageWord(funcId);
+  // Write funcId to OutputCtrlMsgFifo to signal firmware complete
+  const uint32_t funcId = twf::GetCommandID<MnistClusterCommand, Interface>();
+  twrProgram::WriteMessageWord(funcId);
 }
-} // namespace MsgcMnistDemo::ClusterCP
+} // namespace Mnist::ClusterCP
 
 #pragma clang diagnostic pop
 #pragma warning(pop)
--- test/ApolloSDK/MNIST/NPU/MNIST_NPU_Program/DeviceCP/MnistClusterCommand.h
+++ test/ApolloSDK/MNIST/NPU/MNIST_NPU_Program/DeviceCP/MnistClusterCommand.h
@@ -5,22 +5,19 @@
 #include "Commands/CommandRuntime.h"
 #include <KernelParams.h>
 #include <stdint.h>
-namespace Mnist::ClusterCP
-{
-    // Runs no-op firmware which enqueues commands for each accelerator
-    // which utilizes the Nios DMA to move commands from Nios DMem to each
-    // child processor's command queue
-    struct MnistClusterCommand 
-    {
+namespace Mnist::ClusterCP {
+// Runs no-op firmware which enqueues commands for each accelerator
+// which utilizes the Nios DMA to move commands from Nios DMem to each
+// child processor's command queue
+struct MnistClusterCommand {
 #pragma pack(push, 1)
-        struct Arguments
-        {
-            KernelParams params;
-        };
+  struct Arguments {
+    KernelParams params;
+  };
 #pragma pack(pop)
 
-        static void Execute(const Arguments&);
-    };
+  static void Execute(const Arguments &);
+};
 
-    using Interface = Trainwave::FirmwareSDK::CommandList<MnistClusterCommand>;
-} // namespace MsgcMnistDemo::ClusterCP
+using Interface = Trainwave::FirmwareSDK::CommandList<MnistClusterCommand>;
+} // namespace Mnist::ClusterCP
