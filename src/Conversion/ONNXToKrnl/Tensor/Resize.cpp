5353abb2715073ae2d45d1888cfc5a08017767c2
chentong <chentong@us.ibm.com>
2021-06-19 03:15:43 -0400

    pass backend upsample
    Signed-off-by: chentong <chentong@us.ibm.com>

--- src/Conversion/ONNXToKrnl/Tensor/Resize.cpp
+++ src/Conversion/ONNXToKrnl/Tensor/Resize.cpp
@@ -31,32 +31,49 @@ struct ONNXResizeOpLowering : public ConversionPattern {
     ONNXResizeOp resizeOp = llvm::cast<ONNXResizeOp>(op);
     ONNXResizeOpAdaptor operandAdaptor(operands);
     Value data = operandAdaptor.X();
+    Value scales = operandAdaptor.scales();
+    MemRefType memRefType = convertToMemRefType(*op->result_type_begin());
+    int64_t rank = memRefType.getShape().size();
 
+
+#if 0
     // Check implementation constraints
     if (resizeOp.mode() != "nearest" ||
         resizeOp.coordinate_transformation_mode() != "asymmetric" ||
         resizeOp.nearest_mode() != "floor")
       llvm_unreachable("not implemented yet");
+#endif
 
     // Get the scales
+    SmallVector<Value, 4> scaleValues;
     DenseElementsAttr scalesAttrs =
         getDenseElementAttributeFromONNXValue(resizeOp.scales());
-    if (!scalesAttrs)
-      return emitError(loc, "Not implemented yet");
     SmallVector<float, 4> scalesConstant;
-    for (auto scaleAttr : scalesAttrs.getValues<FloatAttr>()) {
-      scalesConstant.emplace_back(scaleAttr.getValueAsDouble());
+    if (scalesAttrs) {
+      for (auto scaleAttr : scalesAttrs.getValues<FloatAttr>()) {
+        Value scaleConstant = emitConstantOp(
+            rewriter, loc, rewriter.getF32Type(), scaleAttr.getValueAsDouble());
+        scaleValues.emplace_back(scaleConstant);
+      }
+    } else {
+      for (decltype(rank) i = 0; i < rank; i++) {
+        Value indexValue = emitConstantOp(
+            rewriter, loc, rewriter.getIndexType(), i);
+        SmallVector<Value, 1> loadIndex;
+        loadIndex.emplace_back(indexValue);
+        Value scaleVal = rewriter.create<KrnlLoadOp>(loc, scales, loadIndex);
+        scaleValues.emplace_back(scaleVal);
+      }
     }
 
-    MemRefType memRefType = convertToMemRefType(*op->result_type_begin());
-    int64_t rank = memRefType.getShape().size();
+    IndexExprScope outerloopContex(&rewriter, loc);
+    DimsExpr outputDims(rank);
+    MemRefBoundsIndexCapture dataBounds(data);
+    ArrayValueIndexCapture scaleIEs(op, resizeOp.scales(), getDenseElementAttributeFromKrnlValue, loadDenseElementArrayValueAtIndex);
 
     if (hasAllConstantDimensions(memRefType))
       alloc = insertAllocAndDealloc(memRefType, loc, rewriter, insertDealloc);
     else {
-      IndexExprScope outerloopContex(&rewriter, loc);
-      DimsExpr outputDims(rank);
-      MemRefBoundsIndexCapture dataBounds(data);
       for (decltype(rank) i = 0; i < rank; i++) {
         if (memRefType.getShape()[i] != -1) {
 	  outputDims[i] = LiteralIndexExpr(memRefType.getShape()[i]);
@@ -66,10 +83,8 @@ struct ONNXResizeOpLowering : public ConversionPattern {
               loc, inputDim, rewriter.getIntegerType(64));
           Value inputDimFloat = rewriter.create<SIToFPOp>(
               loc, rewriter.getF32Type(), inputDimInteger);
-          Value scaleVal = emitConstantOp(
-              rewriter, loc, rewriter.getF32Type(), scalesConstant[i]);
           Value outputDimFloat = rewriter.create<MulFOp>(
-              loc, inputDimFloat, scaleVal);         
+              loc, inputDimFloat, scaleValues[i]);         
           Value outputDimInteger = rewriter.create<FPToSIOp>(
               loc, rewriter.getIntegerType(64), outputDimFloat);
           Value outDim = rewriter.create<IndexCastOp>(
@@ -95,10 +110,8 @@ struct ONNXResizeOpLowering : public ConversionPattern {
           loc, outIndex, rewriter.getIntegerType(64));
       Value outIndexFloat = rewriter.create<SIToFPOp>(
           loc, rewriter.getF32Type(), outIndexInteger);
-      Value scaleVal = emitConstantOp(
-          rewriter, loc, rewriter.getF32Type(), scalesConstant[i]);
       Value inIndexFloat =
-          rewriter.create<DivFOp>(loc, outIndexFloat, scaleVal);
+          rewriter.create<DivFOp>(loc, outIndexFloat, scaleValues[i]);
       Value inIndexInteger = rewriter.create<FPToSIOp>(
           loc, rewriter.getIntegerType(64), inIndexFloat);
       Value inIndex = rewriter.create<IndexCastOp>(
