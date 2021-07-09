3f73d4e03afaccbd7dee0c52c866b4fd7ac87fd1
Alexandre Eichenberger <alexe@us.ibm.com>
2021-07-09 15:58:36 -0400

    updated to July 9th MLIR
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/llvm/llvm-project.git
 # Check out a specific branch that is known to work with ONNX MLIR.
-cd llvm-project && git checkout 76f734040a54622b847729e5acb4480e667d2c74 && cd ..
+cd llvm-project && git checkout 23dd750279c9e32ea631cc9e92c4413c7a3df60a && cd ..
