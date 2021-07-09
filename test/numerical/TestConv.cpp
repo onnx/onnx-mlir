19b527fd5d5ceac13dcaf5e81a4f4c120bfdf7c5
Tung D. Le <tung@jp.ibm.com>
2021-07-09 20:09:46 +0900

    Test conv with dynamic dimensions
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- test/numerical/TestConv.cpp
+++ test/numerical/TestConv.cpp
@@ -28,16 +28,27 @@ using namespace mlir;
 // parameters/configuration.
 bool isOMConvTheSameAsNaiveImplFor(const int N, const int C, const int H,
     const int W, const int kH, const int kW, const int pHBegin, const int pHEnd,
-    const int pWBegin, const int pWEnd) {
+    const int pWBegin, const int pWEnd, bool isDynamicH = false) {
+  static int testNum = 0;
+  printf("attempt %d with N %d, C %d, H %d, W %d, kH %d, kW %d, pHBegin %d, "
+         "pHEnd %d, pWBegin %d, pWEnd %d, isDynamicH %d \n",
+      ++testNum, N, C, H, W, kH, kW, pHBegin, pHEnd, pWBegin, pWEnd,
+      isDynamicH);
+
   MLIRContext ctx;
   registerDialects(ctx);
+
+  int H1 = H;
+  if (isDynamicH)
+    H1 = -1;
 
   auto module = ModuleOp::create(UnknownLoc::get(&ctx));
   OpBuilder builder(&ctx);
   llvm::SmallVector<int64_t, 4> xShape = {N, C, H, W};
+  llvm::SmallVector<int64_t, 3> xShapeSymbol = {N, C, H1, W};
   llvm::SmallVector<int64_t, 1> bShape = {C};
   llvm::SmallVector<int64_t, 4> wShape = {C, C, kH, kW};
-  auto xType = RankedTensorType::get(xShape, builder.getF32Type());
+  auto xType = RankedTensorType::get(xShapeSymbol, builder.getF32Type());
   auto wType = RankedTensorType::get(wShape, builder.getF32Type());
   auto yType = UnrankedTensorType::get(builder.getF32Type());
 
@@ -162,11 +173,15 @@ int main(int argc, char *argv[]) {
     const auto pWBegin = *rc::gen::inRange(0, kW - 1);
     const auto pWEnd = *rc::gen::inRange(0, kW - 1);
 
+    // Whether test dynamic height.
+    const auto DynH = *rc::gen::element(0, 1);
+    const bool isDynH = (DynH == 0);
+
     // Make sure we have at least 1 output per dimension.
     RC_PRE((H >= kH) && (W > kW));
 
     RC_ASSERT(isOMConvTheSameAsNaiveImplFor(
-        N, C, H, W, kH, kW, pHBegin, pHEnd, pWBegin, pWEnd));
+        N, C, H, W, kH, kW, pHBegin, pHEnd, pWBegin, pWEnd, isDynH));
   });
   if (!success)
     return 1;
