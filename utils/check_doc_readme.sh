77f7dd8b1d32e1acc5afe24fd7b6fc2a616fa655
Tung D. Le <tung@jp.ibm.com>
2021-08-02 15:05:43 +0900

    Test check README
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

---
+++ utils/check_doc_readme.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ROOT_DIR=$1
+DOC_DIR=$1/docs
+
+README="README.md"
+
+echo "Checking docs/README.md is up-to-date"
+
+diff ${ROOT_DIR}/${README} ${DOC_DIR}/${REAMDE} > readme.patch
+
+# not up-to-date.
+if [ -s readme.patch ]; then
+  cat readme.patch
+  echo "docs/README.md is not up-to-date, please copy README.md from the root folder."
+  rm readme.patch
+  exit 1
+fi
+
+# up-to-date.
+echo "Done."
+rm readme.patch
