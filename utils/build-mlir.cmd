c7b88dddf02db5111634ee256eec737248b3c968
Stella Stamenova <stilis@microsoft.com>
2021-08-18 09:44:22 -0700

    Use ninja to build on Windows (#827)
    This updates the Windows CI to use ninja as the cmake generator (though the build tools are still Visual Studio). The scripts and readme are updated accordingly as well
    
    Signed-off-by: Stella Stamenova <stilis@microsoft.com>
    
    Co-authored-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- utils/build-mlir.cmd
+++ utils/build-mlir.cmd
@@ -1,7 +1,7 @@
 set root_dir=%cd%
 md llvm-project\build
 cd llvm-project\build
-call cmake %root_dir%\llvm-project\llvm -G "Visual Studio 16 2019" -A x64 -T host=x64 ^
+call cmake %root_dir%\llvm-project\llvm -G "Ninja" ^
    -DCMAKE_INSTALL_PREFIX="%root_dir%\llvm-project\build\install" ^
    -DLLVM_ENABLE_PROJECTS=mlir ^
    -DLLVM_TARGETS_TO_BUILD="host" ^
@@ -10,6 +10,6 @@ call cmake %root_dir%\llvm-project\llvm -G "Visual Studio 16 2019" -A x64 -T hos
    -DLLVM_ENABLE_RTTI=ON ^
    -DLLVM_ENABLE_ZLIB=OFF
 
-call cmake --build . --config Release -- /m
-call cmake --build . --config Release --target install -- /m
-call cmake --build . --config Release --target check-mlir -- /m
+call cmake --build . --config Release
+call cmake --build . --config Release --target install
+call cmake --build . --config Release --target check-mlir
