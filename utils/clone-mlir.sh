4aac16165554c99f60a132834a807a753dce576f
Haruki Imai <imaihal@jp.ibm.com>
2021-08-18 13:34:58 +0000

    Updated MLIR commit to 0104cc
    Signed-off-by: Haruki Imai <imaihal@jp.ibm.com>

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/llvm/llvm-project.git
 # Check out a specific branch that is known to work with ONNX MLIR.
-cd llvm-project && git checkout c84c5d0520159bde529a39cfa593c5948bec3747 && cd ..
+cd llvm-project && git checkout 0104cc85b14aad7980360f58a1159463f8da5adb && cd ..
