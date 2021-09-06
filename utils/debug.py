5cc3d2110ef135b77a5e7481ef4f37068090a326
Tung D. Le <tung@jp.ibm.com>
2021-09-06 18:07:56 +0900

    Compile and run static and dynamic models
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- utils/debug.py
+++ utils/debug.py
@@ -2,15 +2,12 @@ import os
 import sys
 import argparse
 import onnx
+import time
 import subprocess
 import numpy as np
 import tempfile
 
 from collections import OrderedDict
-
-# Reference backend, use onnxruntime by default
-import onnxruntime
-prepare = onnxruntime.InferenceSession
 
 if (not os.environ.get('ONNX_MLIR_HOME', None)):
     raise RuntimeError(
@@ -25,7 +22,8 @@ ONNX_MLIR_EXENAME = "onnx-mlir"
 if sys.platform == "win32":
     ONNX_MLIR_EXENAME = "onnx-mlir.exe"
 
-ONNX_MLIR = os.path.join(os.environ['ONNX_MLIR_HOME'], "bin", ONNX_MLIR_EXENAME)
+ONNX_MLIR = os.path.join(os.environ['ONNX_MLIR_HOME'], "bin",
+                         ONNX_MLIR_EXENAME)
 
 # Include runtime directory in python paths, so PyRuntime can be imported.
 RUNTIME_DIR = os.path.join(os.environ['ONNX_MLIR_HOME'], "lib")
@@ -59,7 +57,18 @@ def extend_model_output(model, intermediate_outputs):
     return model
 
 
-def main(model_path):
+def main(model_path, verify, shape_info, mcpu, mtriple):
+    # Get shape information if given.
+    # shape_info in the form of 'input_index:d1xd2, input_index:d1xd2'
+    input_shapes = {}
+    if shape_info:
+        for input_shape in shape_info.strip().split(","):
+            input_index_shape = input_shape.split(":")
+            input_index = input_index_shape[0]
+            assert not (input_index in input_shapes), "Duplicate input indices"
+            dims = [int(d) for d in input_index_shape[1].split("x")]
+            input_shapes[int(input_index)] = dims
+
     model = onnx.load(model_path)
     intermediate_outputs = sum(
         [list(node.output) for node in model.graph.node], [])
@@ -68,50 +77,92 @@ def main(model_path):
 
     with tempfile.TemporaryDirectory() as temp_dir:
         print("Temporary directory has been created at {}".format(temp_dir))
-        
+
+        print("Compiling the model ...")
         # Save modified model & invoke onnx-mlir to compile it.
         temp_model_path = os.path.join(temp_dir, "model.onnx")
         onnx.save(model, temp_model_path)
-        execute_commands([ONNX_MLIR, temp_model_path])
+        command_list = [ONNX_MLIR]
+        if mcpu:
+            command_list.append("--mcpu="+args.mcpu)
+        if mtriple:
+            command_list.append("--mtriple="+args.mtriple)
+        command_list.append(temp_model_path)
+        start = time.perf_counter()
+        execute_commands(command_list)
+        end = time.perf_counter()
+        print("  took ", end - start, " seconds.")
 
         # Use the generated shared library to create an execution session.
         temp_shared_lib_path = os.path.join(temp_dir, "model.so")
-        sess = ExecutionSession(temp_shared_lib_path,
-                                "run_main_graph")
+        sess = ExecutionSession(temp_shared_lib_path, "run_main_graph")
 
+        print("Generating random inputs ...")
         # Generate random data as input.
         inputs = []
         input_names = []
         initializers = list(map(lambda x: x.name, model.graph.initializer))
         np.random.seed(42)
-        for input_proto in model.graph.input:
+        for i, input_proto in enumerate(model.graph.input):
             if input_proto.name not in initializers:
                 input_names.append(input_proto.name)
                 shape_proto = input_proto.type.tensor_type.shape
                 explicit_shape = []
-                for dim in shape_proto.dim:
-                    assert dim.dim_value, "Can only debug models with inputs that have explicit shapes."
-                    explicit_shape.append(dim.dim_value)
+                for d, dim in enumerate(shape_proto.dim):
+                    if dim.dim_value:
+                        explicit_shape.append(dim.dim_value)
+                    else:
+                        if i in input_shapes:
+                            explicit_shape.append(input_shapes[i][d])
+                        else:
+                            print(
+                                "No shape information for the {}-th input. Use --shape-info."
+                                .format(i))
+                            print(shape_proto)
+                            exit()
                 inputs.append(
-                    np.random.uniform(-1.0, 1.0, explicit_shape).astype(np.float32))
+                    np.random.uniform(-1.0, 1.0,
+                                      explicit_shape).astype(np.float32))
+        print("  done.")
 
+        print("Running inference ...")
         # Run the compiled inference function on the randomly generated data.
+        start = time.perf_counter()
         outs = sess.run(inputs)
+        end = time.perf_counter()
+        print("  took ", end - start, " seconds.")
 
         # Run the model with reference backend and get results.
-        ref_session = prepare(temp_model_path)
-        output_names = list(map(lambda x: x.name, model.graph.output))
-        input_feed = dict(zip(input_names, inputs))
-        ref_outs = ref_session.run(output_names, input_feed)
+        if (verify and verify.lower() == "onnxruntime"):
+            # Reference backend, use onnxruntime by default
+            import onnxruntime
+            prepare = onnxruntime.InferenceSession
 
-        # For each intermediate output tensor, compare results.
-        for i, name in enumerate(intermediate_outputs):
-            print("Verifying value of {}".format(name))
-            np.testing.assert_array_almost_equal(ref_outs[i], outs[i], decimal=5)
+            ref_session = prepare(temp_model_path)
+            output_names = list(map(lambda x: x.name, model.graph.output))
+            input_feed = dict(zip(input_names, inputs))
+            ref_outs = ref_session.run(output_names, input_feed)
+
+            # For each intermediate output tensor, compare results.
+            for i, name in enumerate(intermediate_outputs):
+                print("Verifying value of {}".format(name))
+                np.testing.assert_array_almost_equal(ref_outs[i],
+                                                     outs[i],
+                                                     decimal=5)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('model_path', type=str, help="Path to the model to debug.")
+    parser.add_argument('model_path',
+                        type=str,
+                        help="Path to the model to debug.")
+    parser.add_argument('--verify', choices=['onnxruntime'])
+    parser.add_argument('--shape-info',
+                        type=str,
+                        help="Shape for each input, e.g. 0:1x10x20,1:7x5x3")
+    parser.add_argument('--mtriple', type=str, default="", 
+                        help='triple to pass to the compiler')
+    parser.add_argument('--mcpu', type=str, default="",
+                        help='target a specific cpu, passed to the compiler')
     args = parser.parse_args()
     main(**vars(args))
