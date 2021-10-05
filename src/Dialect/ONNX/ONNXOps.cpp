3306d450a837d618c0e16a75c0426556c93a086c
Tung D. Le <tung@jp.ibm.com>
2021-10-06 01:53:03 +0900

    Rewrite Pad v2 into the latest Pad version (#890)
    * Rewrite PadV2 to Pad
    
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>
    
    * Don't need special handler for PadV2
    
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>
    
    * No shape inference for old ops
    
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>
    
    Co-authored-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps.cpp
+++ src/Dialect/ONNX/ONNXOps.cpp
@@ -4071,16 +4071,6 @@ LogicalResult ONNXUpsampleOp::inferShapes(
   return emitError(NOT_IMPLEMENTED_MESSAGE);
 }
 
-LogicalResult ONNXUpsampleV9Op::inferShapes(
-    std::function<void(mlir::Region &)> doShapeInference) {
-  return emitError(NOT_IMPLEMENTED_MESSAGE);
-}
-
-LogicalResult ONNXUpsampleV7Op::inferShapes(
-    std::function<void(mlir::Region &)> doShapeInference) {
-  return emitError(NOT_IMPLEMENTED_MESSAGE);
-}
-
 LogicalResult ONNXWhereOp::inferShapes(
     std::function<void(mlir::Region &)> doShapeInference) {
   return emitError(NOT_IMPLEMENTED_MESSAGE);
@@ -4181,6 +4171,9 @@ NOT_IMPLEMENTED_INFERSHAPE(ONNXGradientOp);
 NOT_IMPLEMENTED_INFERSHAPE(ONNXMomentumOp);
 NOT_IMPLEMENTED_INFERSHAPE(ONNXNegativeLogLikelihoodLossOp);
 NOT_IMPLEMENTED_INFERSHAPE(ONNXSoftmaxCrossEntropyLossOp);
+NOT_IMPLEMENTED_INFERSHAPE(ONNXUpsampleV9Op);
+NOT_IMPLEMENTED_INFERSHAPE(ONNXUpsampleV7Op);
+NOT_IMPLEMENTED_INFERSHAPE(ONNXPadV2Op);
 
 //===----------------------------------------------------------------------===//
 // Loop
