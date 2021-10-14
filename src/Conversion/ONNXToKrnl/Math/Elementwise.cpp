2e5f2998fd540a947a05e1fc4404c1def4faee08
Kevin O'Brien <caomhin@us.ibm.com>
2021-10-14 21:04:25 +0000

    add runtimecode
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>

--- src/Conversion/ONNXToKrnl/Math/Elementwise.cpp
+++ src/Conversion/ONNXToKrnl/Math/Elementwise.cpp
@@ -855,6 +855,7 @@ struct ONNXElementwiseOpLoweringPrint : public ConversionPattern {
           Value loadedVal=createKrnl.load(operands[0],loopIVs);
           createKrnl.getBuilder().create<KrnlPrintTensorElementOp>(loc,loadedVal);    
         });
+    rewriter.create<KrnlPrintTensorEndOp>(loc); 
 
     rewriter.eraseOp(op);
 
---
+++ src/Runtime/OMDebugSupport.cpp
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===------------- OMDebugSupport.cpp - OMDebugSupport C++ Implementation -------------===//
+//
+// Copyright 2021 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains implementation of OMDebugSupport API
+//
+//===----------------------------------------------------------------------===//
+
+#include "OMDebugSupport.inc"
