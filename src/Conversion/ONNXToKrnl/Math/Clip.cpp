a6e49dfe771f439dcdcb44ff023692aad065661b
Ettore Tiotto <etiotto@ca.ibm.com>
2021-10-22 15:57:08 -0400

    Refactor ONNXShapeHelper code. (#933)
    Signed-off-by: Ettore Tiotto <etiotto@ca.ibm.com>

--- src/Conversion/ONNXToKrnl/Math/Clip.cpp
+++ src/Conversion/ONNXToKrnl/Math/Clip.cpp
@@ -13,7 +13,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/Conversion/ONNXToKrnl/ONNXToKrnlCommon.hpp"
-#include "src/Dialect/ONNX/ONNXShapeHelper.hpp"
+#include "src/Dialect/ONNX/ShapeInference/ONNXShapeHelper.hpp"
 
 using namespace mlir;
 
