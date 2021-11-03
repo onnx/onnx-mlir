0f93bec4e47bb66f01c9f897a4731e8d8577805c
Alexandre Eichenberger <alexe@us.ibm.com>
2021-11-03 09:09:43 -0400

    Onehot update (#967)
    * initial commit
    
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>
    
    * one hot upgraded, more dyn cases
    
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>
    
    * cleanup
    
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>
    
    * cleanup
    
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>
    
    * initial commit
    
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>
    
    * one hot upgraded, more dyn cases
    
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>
    
    * cleanup
    
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>
    
    * cleanup
    
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps.cpp
+++ src/Dialect/ONNX/ONNXOps.cpp
@@ -3830,6 +3830,48 @@ LogicalResult ONNXNotOp::inferShapes(
   return success();
 }
 
+static LogicalResult verify(ONNXOneHotOp op) {
+  ONNXOneHotOpAdaptor operandAdaptor = ONNXOneHotOpAdaptor(op);
+  // Check indices.
+  Value indices = operandAdaptor.indices();
+  if (hasShapeAndRank(indices)) {
+    // Get rank.
+    int64_t indicesRank = indices.getType().cast<ShapedType>().getRank();
+    // Verify axis.
+    int64_t axisValue = op.axis();
+    // Unusually, with a rank of 3, acceptable values are 0 (before first) to 3
+    // (after last).
+    if (axisValue < 0)
+      axisValue += indicesRank + 1;
+    if (!(axisValue >= 0 && axisValue <= indicesRank))
+      return op->emitError("OneHot axis value is out of range");
+  }
+  // Check that values is a rank 2 with 2 elements
+  Value values = operandAdaptor.values();
+  if (hasShapeAndRank(values)) {
+    ShapedType valuesShape = values.getType().cast<ShapedType>();
+    if (valuesShape.getRank() != 1)
+      return op->emitError("OneHot values must be 1D tensor");
+    int64_t dim = valuesShape.getDimSize(0);
+    if (dim >= 0 && dim != 2)
+      return op->emitError("OneHot values must be 1D tensor with 2 elements");
+  }
+  // Depth is a scalar, check when its a tensor of rank 0 or 1.
+  Value depth = operandAdaptor.depth();
+  if (hasShapeAndRank(depth)) {
+    ShapedType depthShape = depth.getType().cast<ShapedType>();
+    if (depthShape.getRank() == 1) {
+      int64_t dim = depthShape.getDimSize(0);
+      if (dim >= 0 && dim != 1)
+        return op->emitError("OneHot depth can be 1D tensor with 1 elements");
+    } else {
+      if (depthShape.getRank() > 1)
+        return op->emitError("OneHot depth must be 0 or 1D tensor");
+    }
+  }
+  return success();
+}
+
 LogicalResult ONNXOneHotOp::inferShapes(
     std::function<void(mlir::Region &)> doShapeInference) {
   // Cannot infer shape if no shape exists.
@@ -3838,7 +3880,7 @@ LogicalResult ONNXOneHotOp::inferShapes(
 
   ONNXOneHotOpShapeHelper shapeHelper(this);
   ONNXOneHotOpAdaptor operandAdaptor(*this);
-  if (failed(shapeHelper.ComputeShape(operandAdaptor)))
+  if (failed(shapeHelper.computeShape(operandAdaptor)))
     return emitError("Failed to scan OneHot parameters successfully");
 
   SmallVector<int64_t, 4> outputDims;
@@ -3950,9 +3992,9 @@ LogicalResult ONNXRangeOp::inferShapes(
   auto constantDelta = getONNXConstantOp(delta());
   if (constantStart && constantLimit && constantDelta) {
     // Get all inputs:
-    double start = getScalarValue(constantStart, startTensorTy);
-    double limit = getScalarValue(constantLimit, limitTensorTy);
-    double delta = getScalarValue(constantDelta, deltaTensorTy);
+    double start = getScalarValue<double>(constantStart, startTensorTy);
+    double limit = getScalarValue<double>(constantLimit, limitTensorTy);
+    double delta = getScalarValue<double>(constantDelta, deltaTensorTy);
 
     // Compute size:
     number_of_elements = (int64_t)ceil((limit - start) / delta);
