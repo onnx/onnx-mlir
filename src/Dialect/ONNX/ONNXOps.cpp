435ba574f1877f0378df5e462054a59fdda53502
Kevin O'Brien <caomhin@us.ibm.com>
2021-11-11 20:48:16 -0500

    add ONNX dialect call op (#987)
    * add ONNX call
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>
    
    * format
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>
    
    * address review comments
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps.cpp
+++ src/Dialect/ONNX/ONNXOps.cpp
@@ -4690,6 +4690,44 @@ LogicalResult ONNXCustomOp::inferShapes(
   return success();
 }
 
+LogicalResult ONNXCallOp::verifySymbolUses(SymbolTableCollection &symbolTable) {
+  // Check that the callee attribute was specified.
+  auto fnAttr = (*this)->getAttrOfType<FlatSymbolRefAttr>("callee");
+  if (!fnAttr)
+    return emitOpError("requires a 'callee' symbol reference attribute");
+  FuncOp fn = symbolTable.lookupNearestSymbolFrom<FuncOp>(*this, fnAttr);
+  if (!fn)
+    return emitOpError() << "'" << fnAttr.getValue()
+                         << "' does not reference a valid function";
+
+  // Verify that the operand and result types match the callee.
+  auto fnType = fn.getType();
+  if (fnType.getNumInputs() != getNumOperands())
+    return emitOpError("incorrect number of operands for callee");
+
+  for (unsigned i = 0, e = fnType.getNumInputs(); i != e; ++i)
+    if (getOperand(i).getType() != fnType.getInput(i))
+      return emitOpError("operand type mismatch: expected operand type ")
+             << fnType.getInput(i) << ", but provided "
+             << getOperand(i).getType() << " for operand number " << i;
+
+  if (fnType.getNumResults() != getNumResults())
+    return emitOpError("incorrect number of results for callee");
+
+  for (unsigned i = 0, e = fnType.getNumResults(); i != e; ++i)
+    if (getResult(i).getType() != fnType.getResult(i)) {
+      auto diag = emitOpError("result type mismatch at index ") << i;
+      diag.attachNote() << "      op result types: " << getResultTypes();
+      diag.attachNote() << "function result types: " << fnType.getResults();
+      return diag;
+    }
+
+  return success();
+}
+
+FunctionType ONNXCallOp::getCalleeType() {
+  return FunctionType::get(getContext(), getOperandTypes(), getResultTypes());
+}
 //===----------------------------------------------------------------------===//
 // ONNX type related code
 //===----------------------------------------------------------------------===//
