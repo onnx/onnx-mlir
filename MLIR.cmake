741a680f89955abdda38621fde1fa27c60ff7c05
Alexandre Eichenberger <alexe@us.ibm.com>
2021-11-17 14:49:37 -0500

    removed default building of onnx-mlir docs
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- MLIR.cmake
+++ MLIR.cmake
@@ -68,7 +68,7 @@ function(add_onnx_mlir_dialect_doc dialect dialect_tablegen_file)
   add_custom_target(${dialect}DocGen DEPENDS ${GEN_DOC_FILE})
   add_dependencies(onnx-mlir-docs ${dialect}DocGen)
 endfunction()
-add_custom_target(onnx-mlir-docs ALL)
+add_custom_target(onnx-mlir-docs)
 
 function(add_onnx_mlir_dialect dialect)
   set(LLVM_TARGET_DEFINITIONS ${dialect}.td)
