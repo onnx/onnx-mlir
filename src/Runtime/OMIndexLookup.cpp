f259c4c65d415ee2f7e391112a107bdd55f5a67e
Ettore Tiotto <etiotto@ca.ibm.com>
2021-11-23 11:02:28 -0500

    Infrastructure for lowering to LLVM CategoryMapper. (#1018)
    Signed-off-by: Ettore Tiotto <etiotto@ca.ibm.com>

---
+++ src/Runtime/OMIndexLookup.cpp
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===-------- OMIndexLookup.cpp - OMIndexLookup C++ Implementation --------===//
+//
+// Copyright 2021 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file contains implementation of the OMIndexLookup functions.
+//
+//===----------------------------------------------------------------------===//
+
+#include "OMIndexLookup.inc"
