1d70313d1804e1a46be197c9f0816c3022377035
Doxygen Bot (GitHub Action) <onnxmlir@onnx.ai>
2021-12-03 21:38:56 +0000

    Push docs and doxygen updates into gh-pages branch

---
+++ mnist_example/update_env.sh
@@ -0,0 +1,12 @@
+# ONNX_MLIR_ROOT points to the root of the onnx-mlir, 
+# under which the include and the build directory lies.
+export ONNX_MLIR_ROOT=$(pwd)/../..
+# Define the bin directory where onnx-mlir binary resides. Change only if you
+# have a non-standard install.
+export ONNX_MLIR_BIN=$ONNX_MLIR_ROOT/build/Debug/bin
+# Define the include directory where onnx-mlir runtime include files resides.
+# Change only if you have a non-standard install.
+export ONNX_MLIR_INCLUDE=$ONNX_MLIR_ROOT/include
+
+# Include ONNX-MLIR executable directories part of $PATH.
+export PATH=$ONNX_MLIR_ROOT/build/Debug/bin:$PATH
