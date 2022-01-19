987fa771e67550b1580632f495398c553ce6f66d
chentong319 <chentong@us.ibm.com>
2022-01-19 12:50:31 -0500

    Update document (#1077)
    * create
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * delete HowTOAddAnOperation.md
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * modify testing
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * create
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * delete HowTOAddAnOperation.md
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * modify testing
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * fix
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * create
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * add comment
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * delete HowTOAddAnOperation.md
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * modify testing
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * fix
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * create
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    Signed-off-by: Ethan Wang <ywan2928@uwo.ca>

--- utils/gen_onnx_mlir.py
+++ utils/gen_onnx_mlir.py
@@ -21,6 +21,10 @@ from onnx.backend.sample.ops import collect_sample_implementations
 from typing import Any, Text, Sequence, Dict, List, Type, Set, Tuple
 
 import pprint
+import onnx
+
+# change this variable only when upgrading the ONNX support within ONNX-MLIR
+current_onnx_version = "1.9.0"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dry-run-onnx-ops",
@@ -39,7 +43,26 @@ parser.add_argument("--check-operation-version",
 
 args = parser.parse_args()
 
-import onnx
+
+# check the version of onnx package being used
+if current_onnx_version != onnx.__version__ :
+    print("version of expected onnx is {}, ".format(current_onnx_version)+
+          "while onnx package being used is {}".format(onnx.__version__))
+    quit()
+
+current_onnx_version = "1.9.0"
+# check the version of onnx package being used
+if current_onnx_version != onnx.__version__ :
+    print("version of expected onnx is {}, ".format(current_onnx_version)+
+          "while onnx package being used is {}".format(onnx.__version__))
+    quit()
+
+current_onnx_version = "1.9.0"
+# check the version of onnx package being used
+if current_onnx_version != onnx.__version__ :
+    print("version of expected onnx is {}, ".format(current_onnx_version)+
+          "while onnx package being used is {}".format(onnx.__version__))
+    quit()
 
 check_operation_version = args.check_operation_version
 
@@ -234,17 +257,7 @@ version_dict = {
  'Where': [9],
  'Xor': [7],
  'ZipMap': [1]}
-# Manual specification of attribute defaults.
-special_attr_defaults = dict([
-    # ("AveragePool.kernel_shape", ('ints', '{}')),
-    # ("MaxPool.kernel_shape", ('ints', '{}')),
-    # ("Cast.to", ('int', '0')),
-    # ("Concat.axis", ('int', '0')),
-    # ("Conv.group", ('int', '1')),
-    # ("Unsqueeze.axes", ('ints', '{}')),
-    # ("RNN.activation_alpha", ('floats', '{}')),
-    # ("RNN.activation_beta", ('floats', '{}')),
-])
+
 # Manual specification of attribute type.
 special_attr_types = dict([("Cast.to", 'type')])
 
@@ -654,9 +667,6 @@ def get_attrs(schema):
           continue
 
         qualified_attr_name = "{}.{}".format(schema.name, attr.name)
-        if qualified_attr_name in special_attr_defaults:
-            name_to_type[attr.name] = get_attr_type_with_default(
-                *special_attr_defaults[qualified_attr_name])
         if qualified_attr_name in special_attr_types:
             name_to_type[attr.name] = onnx_attr_type_to_mlir_attr_type(
                 special_attr_types[qualified_attr_name])
