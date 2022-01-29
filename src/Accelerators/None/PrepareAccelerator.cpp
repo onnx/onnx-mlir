fe06329dfdde9c1a53a569b2bd5b02a7e052eb9e
Kevin O'Brien <caomhin@us.ibm.com>
2022-01-29 03:30:46 +0000

    skeleton ... forgot these
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>

---
+++ src/Accelerators/None/PrepareAccelerator.cpp
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===-------------------------- PrepareAccelerator.cpp -------------------------===//
+//
+// Copyright 2022 The IBM Research Authors.
+//
+// =============================================================================
+//
+// Dummy file to add accelerator passes when there are no accelerators being targeted.
+//
+//===----------------------------------------------------------------------===//
+
+void prepareNothing() {
+    
+}
