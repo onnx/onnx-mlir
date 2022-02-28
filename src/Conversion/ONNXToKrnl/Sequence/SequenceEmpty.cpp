f46a505da9975416a4c6a167f9a5f76ce06163ca
chentong319 <chentong@us.ibm.com>
2022-02-28 12:22:13 -0500

    move type converter
    Signed-off-by: chentong319 <chentong@us.ibm.com>

--- src/Conversion/ONNXToKrnl/Sequence/SequenceEmpty.cpp
+++ src/Conversion/ONNXToKrnl/Sequence/SequenceEmpty.cpp
@@ -26,17 +26,42 @@ struct ONNXSequenceEmptyOpLowering : public ConversionPattern {
   LogicalResult matchAndRewrite(Operation *op, ArrayRef<Value> operands,
       ConversionPatternRewriter &rewriter) const final {
     Location loc = op->getLoc();
+    ONNXSequenceEmptyOp sequenceEmptyOp = cast<ONNXSequenceEmptyOp>(op);
 
+    // Get element type
+    //auto elementType = convertONNXTypeToMLIRType(sequenceEmptyOp.dtype());
+
+    // Default type, F32, for the Optional attribute
+    Type elementType = rewriter.getF32Type();
+    if (sequenceEmptyOp.dtypeAttr())
+      elementType = convertONNXTypeToMLIRType(rewriter, (onnx::TensorProto_DataType)sequenceEmptyOp.dtypeAttr().getValue().getSExtValue());
+
+    // Get element shape
+    int64_t rank = -1;
+
+    // Will add an attribute for shape in SequenceEmpty
+
+    Type seqElementType;
+    if (rank == -1) {
+      // unranked memref
+      seqElementType = UnrankedMemRefType::get(elementType, 0);
+    } else {
+      // temporarily assume all dim unknown
+      SmallVector<int64_t, 1> dims(-1, rank);
+      llvm::ArrayRef<int64_t> shape(dims.data(), dims.size());
+      seqElementType = MemRefType::get(shape, elementType);
+    }
+      
     SmallVector<int64_t, 1> dims;
     dims.emplace_back(0);
     llvm::ArrayRef<int64_t> shape(dims.data(), dims.size());
  
+    //auto itemType = UnrankedMemRefType::get(rewriter.getF32Type(),0);
     //auto outputTensorType = RankedTensorType::get(shape, rewriter.getIntegerType(64));
     //auto outputMemRefType = convertToMemRefType(outputTensorType);
-    MemRefType outputMemRefType = MemRefType::get(shape, rewriter.getIntegerType(64));
+    MemRefType outputMemRefType = MemRefType::get(shape, seqElementType);
     bool insertDealloc = checkInsertDealloc(op);
     Value alloc = insertAllocAndDealloc(outputMemRefType, loc, rewriter, insertDealloc);
-
     rewriter.replaceOp(op, alloc);
     return success();
   }
