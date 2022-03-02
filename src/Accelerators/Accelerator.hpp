6e060dd056a56bbfbb50fd2fb7de48ea44592661
Kevin O'Brien <caomhin@us.ibm.com>
2022-03-02 15:18:07 -0500

    initial support for (optional) accelerators (#1208)
    * initial support for (optional) accelerators
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>
    
    * format
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>
    
    * remove bad line
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>
    
    * format
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>
    
    * address review comennts
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>
    
    * cleanup
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>
    
    * fix comments
    
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>
    
    Co-authored-by: Ettore Tiotto <etiotto@ca.ibm.com>
    Signed-off-by: Ethan Wang <ywan2928@uwo.ca>

---
+++ src/Accelerators/Accelerator.hpp
@@ -0,0 +1,33 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===-------------------------- Accelerator.hpp -------------------------===//
+//
+// Copyright 2022 The IBM Research Authors.
+//
+// =============================================================================
+//
+// Accelerator base class
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Pass/PassManager.h"
+#include "onnx-mlir/Compiler/OMCompilerTypes.h"
+#include <vector>
+
+namespace mlir {
+class Accelerator {
+public:
+  Accelerator();
+  static std::vector<Accelerator *> *getAcceleratorList();
+  virtual bool isActive() = 0;
+  virtual void prepareAccelerator(mlir::OwningOpRef<ModuleOp> &module,
+      mlir::MLIRContext &context, mlir::PassManager &pm,
+      onnx_mlir::EmissionTargetType emissionTarget) = 0;
+
+private:
+  static std::vector<Accelerator *> *acceleratorTargets;
+};
+} // namespace mlir
