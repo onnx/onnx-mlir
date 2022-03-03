e35dd71ae840bc9ff8ed3805c7e2598aa49da118
Kevin O'Brien <caomhin@us.ibm.com>
2022-03-03 03:24:59 -0500

    add functional code for NNPA backend
    Signed-off-by: Kevin O'Brien <caomhin@us.ibm.com>

---
+++ src/Accelerators/NNPA/NNPAAccelerator.hpp
@@ -0,0 +1,33 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===-------------------------- NNPAAccelerator.hpp
+//------------------------===//
+//
+// Copyright 2022 The IBM Research Authors.
+//
+// =============================================================================
+//
+// Accelerator class for NNPA
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+#include "src/Accelerators/Accelerator.hpp"
+
+namespace mlir {
+class NNPAAccelerator final : public Accelerator {
+private:
+  static bool initialized;
+
+public:
+  NNPAAccelerator();
+
+  void prepareAccelerator(mlir::OwningOpRef<ModuleOp> &module,
+      mlir::MLIRContext &context, mlir::PassManager &pm,
+      onnx_mlir::EmissionTargetType emissionTarget) override;
+  bool isActive() override;
+};
+
+} // namespace mlir
