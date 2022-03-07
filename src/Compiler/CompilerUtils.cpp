f19a369d14e252fc37a75fe84a38fafeec237bad
Whitney Tsang <whitneyt@ca.ibm.com>
2022-03-07 10:44:43 -0500

    Address review comments
    Signed-off-by: Whitney Tsang <whitneyt@ca.ibm.com>

--- src/Compiler/CompilerUtils.cpp
+++ src/Compiler/CompilerUtils.cpp
@@ -45,7 +45,7 @@ using namespace onnx_mlir;
 const string OnnxMlirEnvOptionName = "ONNX_MLIR_FLAGS";
 #if defined(ONNX_MLIR_REPOSITORY) && defined(ONNX_MLIR_REVISION) &&            \
     defined(LLVM_REPOSITORY) && defined(LLVM_REVISION)
-const string OnnxMlirVersion =
+static const string OnnxMlirVersion =
     "onnx-mlir version 1.0.0 (" ONNX_MLIR_REPOSITORY " " ONNX_MLIR_REVISION
     " " LLVM_REPOSITORY " " LLVM_REVISION ")";
 #else
@@ -545,9 +545,8 @@ static void genLLVMBitcode(const mlir::OwningOpRef<ModuleOp> &module,
   // Emit the onnx-mlir version as llvm.ident metadata.
   llvm::NamedMDNode *identMetadata =
       llvmModule->getOrInsertNamedMetadata("llvm.ident");
-  std::string version = OnnxMlirVersion;
   llvm::LLVMContext &ctx = llvmModule->getContext();
-  llvm::Metadata *identNode[] = {llvm::MDString::get(ctx, version)};
+  llvm::Metadata *identNode[] = {llvm::MDString::get(ctx, OnnxMlirVersion)};
   identMetadata->addOperand(llvm::MDNode::get(ctx, identNode));
 
   llvm::WriteBitcodeToFile(*llvmModule, moduleBitcodeStream);
