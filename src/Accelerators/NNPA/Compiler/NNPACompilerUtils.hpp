7e777fd0c1936aa90f7614fad0084c4ca733f7db
Tung D. Le <tung@jp.ibm.com>
2022-03-08 16:18:51 +0900

    Missing files
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

---
+++ src/Accelerators/NNPA/Compiler/NNPACompilerUtils.hpp
@@ -0,0 +1,43 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===-------------------------- NNPACompilerUtils.hpp
+//------------------------===//
+//
+// Copyright 2022 The IBM Research Authors.
+//
+// =============================================================================
+//
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "mlir/Pass/PassManager.h"
+#include "src/Compiler/CompilerUtils.hpp"
+#include "src/Support/OMOptions.hpp"
+
+enum NNPAEmissionTargetType {
+  EmitZNONE,
+  EmitZLowIR,
+  EmitZHighIR,
+};
+
+void addMemoryPooling(mlir::PassManager &pm);
+
+void addONNXToZHighPasses(mlir::PassManager &pm);
+
+void addZHighToZLowPasses(mlir::PassManager &pm);
+
+void addAllToLLVMPasses(mlir::PassManager &pm);
+
+void addPassesNNPA(mlir::OwningOpRef<mlir::ModuleOp> &module,
+    mlir::PassManager &pm, onnx_mlir::EmissionTargetType &emissionTarget,
+    NNPAEmissionTargetType dlcEmissionTarget,
+    mlir::ArrayRef<std::string> execNodesOnCpu);
+
+int compileModuleNNPA(mlir::OwningOpRef<mlir::ModuleOp> &module,
+    mlir::MLIRContext &context, std::string outputBaseName,
+    onnx_mlir::EmissionTargetType emissionTarget,
+    NNPAEmissionTargetType dlcEmissionTarget = EmitZNONE,
+    mlir::ArrayRef<std::string> execNodesOnCpu = mlir::ArrayRef<std::string>());
