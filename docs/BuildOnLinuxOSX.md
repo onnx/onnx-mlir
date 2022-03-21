9210a121f4a25eca5e8f9effcd2a87b9dfd2b271
Ethan Wang <ywan2928@uwo.ca>
2022-03-21 18:11:39 +0000

    build openmp runtime
    Signed-off-by: Ethan Wang <ywan2928@uwo.ca>

--- docs/BuildOnLinuxOSX.md
+++ docs/BuildOnLinuxOSX.md
@@ -23,10 +23,12 @@ mkdir llvm-project/build
 cd llvm-project/build
 cmake -G Ninja ../llvm \
    -DLLVM_ENABLE_PROJECTS=mlir \
+   -DLLVM_ENABLE_PROJECTS="clang;mlir" \
    -DLLVM_TARGETS_TO_BUILD="host" \
    -DCMAKE_BUILD_TYPE=Release \
    -DLLVM_ENABLE_ASSERTIONS=ON \
-   -DLLVM_ENABLE_RTTI=ON
+   -DLLVM_ENABLE_RTTI=ON \
+   -DLLVM_ENABLE_RUNTIMES="openmp"
 
 cmake --build . -- ${MAKEFLAGS}
 cmake --build . --target check-mlir
--- utils/build-mlir.cmd
+++ utils/build-mlir.cmd
@@ -4,10 +4,12 @@ cd llvm-project\build
 call cmake %root_dir%\llvm-project\llvm -G "Ninja" ^
    -DCMAKE_INSTALL_PREFIX="%root_dir%\llvm-project\build\install" ^
    -DLLVM_ENABLE_PROJECTS=mlir ^
+   -DLLVM_ENABLE_PROJECTS="clang;mlir" ^
    -DLLVM_TARGETS_TO_BUILD="host" ^
    -DCMAKE_BUILD_TYPE=Release ^
    -DLLVM_ENABLE_ASSERTIONS=ON ^
    -DLLVM_ENABLE_RTTI=ON ^
+   -DLLVM_ENABLE_RUNTIMES="openmp" ^
    -DLLVM_ENABLE_ZLIB=OFF ^
    -DLLVM_INSTALL_UTILS=ON
 
