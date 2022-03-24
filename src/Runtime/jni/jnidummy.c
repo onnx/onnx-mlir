586ce472dc1c3cb2541e71ecc64db572aa00d48e
gongsu832 <gong_su@hotmail.com>
2022-03-23 20:43:14 -0400

    - add queryEntryPoints Java API (#1255)
    - use GENERATE_NATIVE_HEADERS option of add_jar (require
      cmake 3.11+) to generate JNI header since javah was
      deprecated since Java 8
    
    Signed-off-by: Gong Su <gong_su@hotmail.com>

--- src/Runtime/jni/jnidummy.c
+++ src/Runtime/jni/jnidummy.c
@@ -19,6 +19,7 @@
    into libmodel.so */
 void __dummy_do_not_call__(JNIEnv *env, jclass cls, jobject obj) {
   Java_com_ibm_onnxmlir_OMModel_main_1graph_1jni(NULL, NULL, NULL);
+  Java_com_ibm_onnxmlir_OMModel_query_1entry_1points(NULL, NULL);
   Java_com_ibm_onnxmlir_OMModel_input_1signature_1jni(NULL, NULL, NULL);
   Java_com_ibm_onnxmlir_OMModel_output_1signature_1jni(NULL, NULL, NULL);
 }
