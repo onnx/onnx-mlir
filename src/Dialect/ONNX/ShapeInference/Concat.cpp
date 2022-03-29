eb4569c1b5814f3fccd1b991f95e8812e28b4c1b
Ettore Tiotto <etiotto@ca.ibm.com>
2022-03-29 17:52:59 -0400

    Add copyright and refactor code
    Signed-off-by: Ettore Tiotto <etiotto@ca.ibm.com>

--- src/Dialect/ONNX/ShapeInference/Concat.cpp
+++ src/Dialect/ONNX/ShapeInference/Concat.cpp
@@ -4,6 +4,10 @@
 
 //===---------- Concat.cpp - Shape Inference for Concat Op ----------------===//
 //
+// Copyright 2020-2022 The IBM Research Authors.
+//
+// =============================================================================
+//
 // This file implements shape inference for the ONNX Concat Operator.
 //
 //===----------------------------------------------------------------------===//
@@ -24,37 +28,31 @@ ONNXConcatOpShapeHelper::ONNXConcatOpShapeHelper(ONNXConcatOp *newOp,
 LogicalResult ONNXConcatOpShapeHelper::computeShape(
     ONNXConcatOpAdaptor operandAdaptor) {
 
-  int inputNum = op->getNumOperands();
-  Value firstInput = operandAdaptor.getODSOperands(0)[0];
-  auto commonType = firstInput.getType().cast<ShapedType>();
-  auto commonShape = commonType.getShape();
-  auto commonRank = commonShape.size();
+  unsigned numInputs = op->getNumOperands();
+  Value firstInput = operandAdaptor.inputs().front();
+  ArrayRef<int64_t> commonShape =
+      firstInput.getType().cast<ShapedType>().getShape();
+  size_t commonRank = commonShape.size();
   int64_t axisIndex = op->axis();
 
   // Negative axis means values are counted from the opposite side.
   // TOFIX should be in normalization pass
-  if (axisIndex < 0) {
-    axisIndex = commonRank + axisIndex;
-  }
+  if (axisIndex < 0)
+    axisIndex += commonRank;
 
-  IndexExpr cumulativeAxisSize = LiteralIndexExpr(0);
-  for (int i = 0; i < inputNum; ++i) {
-    Value currentInput = operandAdaptor.getODSOperands(0)[i];
+  IndexExpr cumulativeAxisSize(0);
+  for (unsigned i = 0; i < numInputs; ++i) {
+    Value currentInput = operandAdaptor.inputs()[i];
     MemRefBoundsIndexCapture currInputBounds(currentInput);
     DimIndexExpr currentSize(currInputBounds.getDim(axisIndex));
     cumulativeAxisSize = cumulativeAxisSize + currentSize;
   }
 
-  DimsExpr outputDims;
+  DimsExpr outputDims(commonRank);
   MemRefBoundsIndexCapture firstInputBounds(firstInput);
-  outputDims.resize(commonRank);
-  for (unsigned int i = 0; i < commonRank; i++) {
-    if (i == axisIndex) {
-      outputDims[i] = cumulativeAxisSize;
-    } else {
-      outputDims[i] = firstInputBounds.getDim(i);
-    }
-  }
+  for (unsigned i = 0; i < commonRank; i++)
+    outputDims[i] =
+        (i == axisIndex) ? cumulativeAxisSize : firstInputBounds.getDim(i);
 
   dimsForOutput(0) = outputDims;
   return success();
