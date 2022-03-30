9cf44bebd4e1729f92fd619828b235b0e5f86584
Whitney Tsang <whitneyt@ca.ibm.com>
2022-03-30 15:43:14 -0400

    [Slice] Use builder based interface to generate Krnl loops
    Signed-off-by: Whitney Tsang <whitneyt@ca.ibm.com>

--- src/Conversion/ONNXToKrnl/Tensor/Slice.cpp
+++ src/Conversion/ONNXToKrnl/Tensor/Slice.cpp
@@ -40,32 +40,27 @@ struct ONNXSliceOpLowering : public ConversionPattern {
     Value alloc = insertAllocAndDeallocSimple(
         rewriter, op, outputMemRefType, loc, shapeHelper.dimsForOutput(0));
 
-    BuildKrnlLoop outputLoops(rewriter, loc, outputRank);
-    outputLoops.createDefineOp();
-    outputLoops.pushAllBounds(shapeHelper.dimsForOutput(0));
-    outputLoops.createIterateOp();
-    rewriter.setInsertionPointToStart(outputLoops.getIterateBlock());
-
-    IndexExprScope childScope(&rewriter, shapeHelper.scope);
-    // Scope for krnl ops
     KrnlBuilder createKrnl(rewriter, loc);
-
-    // Compute indices for the load and store op.
-    // Load: "i * step + start" for all dim.
-    // Store: "i" for all dims.
-    SmallVector<IndexExpr, 4> loadIndices;
-    SmallVector<IndexExpr, 4> storeIndices;
-    for (int ii = 0; ii < outputRank; ++ii) {
-      Value inductionVal = outputLoops.getInductionVar(ii);
-      DimIndexExpr inductionIndex(inductionVal);
-      IndexExpr start = SymbolIndexExpr(shapeHelper.starts[ii]);
-      IndexExpr step = SymbolIndexExpr(shapeHelper.steps[ii]);
-      loadIndices.emplace_back((step * inductionIndex) + start);
-      storeIndices.emplace_back(inductionIndex);
-    }
-    // Load data and store in alloc data.
-    Value loadVal = createKrnl.loadIE(operandAdaptor.data(), loadIndices);
-    createKrnl.storeIE(loadVal, alloc, storeIndices);
+    ValueRange loopDef = createKrnl.defineLoops(outputRank);
+    SmallVector<IndexExpr, 4> lbs(outputRank, LiteralIndexExpr(0));
+    createKrnl.iterateIE(loopDef, loopDef, lbs, shapeHelper.dimsForOutput(),
+        [&](KrnlBuilder &createKrnl, ValueRange loopInd) {
+          // Compute indices for the load and store op.
+          // Load: "i * step + start" for all dim.
+          // Store: "i" for all dims.
+          SmallVector<IndexExpr, 4> loadIndices;
+          SmallVector<IndexExpr, 4> storeIndices;
+          for (int ii = 0; ii < outputRank; ++ii) {
+            DimIndexExpr inductionIndex(loopInd[ii]);
+            IndexExpr start = SymbolIndexExpr(shapeHelper.starts[ii]);
+            IndexExpr step = SymbolIndexExpr(shapeHelper.steps[ii]);
+            loadIndices.emplace_back((step * inductionIndex) + start);
+            storeIndices.emplace_back(inductionIndex);
+          }
+          // Load data and store in alloc data.
+          Value loadVal = createKrnl.loadIE(operandAdaptor.data(), loadIndices);
+          createKrnl.storeIE(loadVal, alloc, storeIndices);
+        });
 
     rewriter.replaceOp(op, alloc);
     return success();
