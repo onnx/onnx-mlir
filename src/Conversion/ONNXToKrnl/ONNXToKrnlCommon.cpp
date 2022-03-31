469bdf746a2a9e040c53d788fe388fc54f3f44b7
Alexandre Eichenberger <alexe@us.ibm.com>
2022-03-31 19:45:41 -0400

    Isolated MLIR specific dialect support from Dialect/ONNX (#1298)
    * isolated MLIR specific dialect support from Dialect/ONNX
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Conversion/ONNXToKrnl/ONNXToKrnlCommon.cpp
+++ src/Conversion/ONNXToKrnl/ONNXToKrnlCommon.cpp
@@ -14,7 +14,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/Conversion/ONNXToKrnl/ONNXToKrnlCommon.hpp"
-#include "src/Dialect/ONNX/MLIRDialectBuilder.hpp"
+#include "src/Dialect/Krnl/DialectBuilder.hpp"
+#include "src/Dialect/Mlir/DialectBuilder.hpp"
 
 using namespace onnx_mlir;
 
