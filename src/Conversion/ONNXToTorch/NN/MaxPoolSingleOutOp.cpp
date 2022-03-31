f5935b93d67b30ca66429d3b85907c18d20a5062
jcamacho1234 <jcamacho@xlinx.com>
2022-03-31 16:28:45 -0700

    Applied previous commit's fixes to MaxPool Op

--- src/Conversion/ONNXToTorch/NN/MaxPoolSingleOutOp.cpp
+++ src/Conversion/ONNXToTorch/NN/MaxPoolSingleOutOp.cpp
@@ -162,34 +162,51 @@ public:
     auto by = IntegerType::get(op1.getContext(), 1);
 
     if (pads) {
-      int j = 0;
-      for (unsigned int i = 0; i < pads.size(); i++) {
-        dimArray[j].dim_start =
+      bool is_symmetric = true;
+      for (unsigned int i = 0; i < pads.size(); i += 2) {
+	if (pads[i] != pads[i+1]) {
+	  is_symmetric = false;
+	  break;
+	}
+      }
+      
+      if (is_symmetric) {
+	for (unsigned int i = 0; i < pads.size(); i += 2) {
+	  auto pad_value = (pads[i].dyn_cast<IntegerAttr>()).getValue().getZExtValue();
+	  auto f0 = IntegerAttr::get(ty, pad_value);
+	  Value p0v = rewriter.create<ConstantIntOp>(loc, f0);
+	  translatepadsList.push_back(p0v);	  
+	}
+      } else { 
+	int j = 0;
+	for (unsigned int i = 0; i < pads.size(); i++) {
+	  dimArray[j].dim_start =
             (pads[i].dyn_cast<IntegerAttr>()).getValue().getZExtValue();
-        i++;
-        dimArray[j].dim_end =
+	  i++;
+	  dimArray[j].dim_end =
             (pads[i].dyn_cast<IntegerAttr>()).getValue().getZExtValue();
-        j++;
-      }
+	  j++;
+	}
 
-      // read the onnx pad values from array(dim_start values)
-      int k = 0;
-      for (unsigned int i = 0; i < pads.size(); i = i + 2) {
-        auto f0 = IntegerAttr::get(ty, (dimArray[k].dim_start));
-        Value p0v = rewriter.create<ConstantIntOp>(loc, f0);
-        translatepadsList.push_back(p0v);
-        k++;
-      }
+	// read the onnx pad values from array(dim_start values)
+	int k = 0;
+	for (unsigned int i = 0; i < pads.size(); i = i + 2) {
+	  auto f0 = IntegerAttr::get(ty, (dimArray[k].dim_start));
+	  Value p0v = rewriter.create<ConstantIntOp>(loc, f0);
+	  translatepadsList.push_back(p0v);
+	  k++;
+	}
 
-      // read the onnx pad values from array(dim_end values)
-      k = 0;
-      for (unsigned int i = 0; i < pads.size(); i = i + 2) {
-        auto f1 = IntegerAttr::get(ty, (dimArray[k].dim_end));
-        Value p1v = rewriter.create<ConstantIntOp>(loc, f1);
-        translatepadsList.push_back(p1v);
-        k++;
+	// read the onnx pad values from array(dim_end values)
+	k = 0;
+	for (unsigned int i = 0; i < pads.size(); i = i + 2) {
+	  auto f1 = IntegerAttr::get(ty, (dimArray[k].dim_end));
+	  Value p1v = rewriter.create<ConstantIntOp>(loc, f1);
+	  translatepadsList.push_back(p1v);
+	  k++;
+	}
       }
-    }
+    }	
 
     std::vector<Value> dilationonnxList;
     std::vector<Value> kernalshapeonnxList;
