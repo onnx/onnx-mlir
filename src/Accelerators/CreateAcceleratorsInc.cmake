6567e6292f980633c282e29500cef2f25fe7b6de
Ettore Tiotto <etiotto@ca.ibm.com>
2022-04-05 15:41:14 -0400

    Address code review commens
    Signed-off-by: Ettore Tiotto <etiotto@ca.ibm.com>

--- src/Accelerators/CreateAcceleratorsInc.cmake
+++ src/Accelerators/CreateAcceleratorsInc.cmake
@@ -3,8 +3,13 @@
 # We need to do this in a stand-alone cmake script because there's no
 # cross-platform way to append to a file through add_custom_command.
 
-file(WRITE ${INC_FILE} "#define APPLY_TO_ACCELERATORS(MACRO) \\\n")
+file(WRITE "${INC_FILE}.tmp" "#define APPLY_TO_ACCELERATORS(MACRO) \\\n")
+
 foreach(t ${ACCELERATORS})
-  file (APPEND ${INC_FILE} "  MACRO(${t}) \\\n")
+  file (APPEND "${INC_FILE}.tmp" "  MACRO(${t}) \\\n")
 endforeach(t)
-file (APPEND ${INC_FILE} "\n")
+
+file(APPEND "${INC_FILE}.tmp" "\n")
+
+file(COPY_FILE "${INC_FILE}.tmp" ${INC_FILE} ONLY_IF_DIFFERENT)
+file(REMOVE "${INC_FILE}.tmp")
