a6bd1dae7e2dbc07842523b1c7bbe7df74bfd909
Whitney Tsang <54643204+whitneywhtsang@users.noreply.github.com>
2022-04-07 11:01:24 -0400

    [ArgMax][Resize][RNNBase] Use builder based interface to generate Krnl loops (#1317)
    [LRN][Pooling][Loop] Use builder based interface to generate Krnl loops (Only partially done)
    
    Signed-off-by: Whitney Tsang <whitneyt@ca.ibm.com>

--- src/Conversion/ONNXToKrnl/Tensor/Resize.cpp
+++ src/Conversion/ONNXToKrnl/Tensor/Resize.cpp
@@ -51,6 +51,7 @@ struct ONNXResizeOpLowering : public ConversionPattern {
     IndexExprScope outerloopContex(&rewriter, loc);
     DimsExpr outputDims(rank);
     MemRefBoundsIndexCapture dataBounds(data);
+    KrnlBuilder createKrnl(rewriter, loc);
     if (fromScale) {
       // Get the scales
       // SymbolIndexExpr was tried but got runtime error
@@ -69,14 +70,14 @@ struct ONNXResizeOpLowering : public ConversionPattern {
       } else {
         for (decltype(rank) i = 0; i < rank; i++) {
           Value indexValue = create.math.constantIndex(i);
-          Value scaleVal = create.krnl.load(scales, indexValue);
+          Value scaleVal = createKrnl.load(scales, indexValue);
           scaleValues.emplace_back(scaleVal);
         }
       }
     } else {
       for (decltype(rank) i = 0; i < rank; i++) {
         Value indexValue = create.math.constantIndex(i);
-        Value resizedVal = create.krnl.load(sizes, indexValue);
+        Value resizedVal = createKrnl.load(sizes, indexValue);
         Value resizedFVal = rewriter.create<arith::SIToFPOp>(
             loc, rewriter.getF32Type(), resizedVal);
         Value inputDim = dataBounds.getDim(i).getValue();
@@ -111,7 +112,7 @@ struct ONNXResizeOpLowering : public ConversionPattern {
           outputDims[i] = LiteralIndexExpr(memRefType.getShape()[i]);
         } else {
           Value indexValue = create.math.constantIndex(i);
-          Value resizedVal = create.krnl.load(sizes, indexValue);
+          Value resizedVal = createKrnl.load(sizes, indexValue);
           Value outDim = create.math.castToIndex(resizedVal);
           SymbolIndexExpr outDimIE(outDim);
           outputDims[i] = SymbolIndexExpr(outDimIE);
@@ -134,80 +135,83 @@ struct ONNXResizeOpLowering : public ConversionPattern {
     Value zero = create.math.constant(rewriter.getIntegerType(64), 0);
     Value one = create.math.constantIndex(1);
 
-    // Create loops
-    krnl::BuildKrnlLoop outputLoops(rewriter, loc, rank);
-    outputLoops.createDefineAndIterateOp(alloc);
-    rewriter.setInsertionPointToStart(outputLoops.getIterateBlock());
+    ValueRange loopDef = createKrnl.defineLoops(rank);
+    SmallVector<IndexExpr, 4> lbs(rank, LiteralIndexExpr(0));
+    MemRefBoundsIndexCapture allocBounds(alloc);
+    SmallVector<IndexExpr, 4> ubs;
+    allocBounds.getDimList(ubs);
+    createKrnl.iterateIE(loopDef, loopDef, lbs, ubs,
+        [&](KrnlBuilder &createKrnl, ValueRange loopInd) {
+          SmallVector<Value, 4> readIndices;
+          for (int64_t i = 0; i < rank; ++i) {
+            Value inIndexFloat;
+            Value outIndex = loopInd[i];
+            Value outIndexInteger = rewriter.create<arith::IndexCastOp>(
+                loc, rewriter.getIntegerType(64), outIndex);
+            Value outIndexFloat =
+                create.math.cast(rewriter.getF32Type(), outIndexInteger);
 
-    // Loop body
-    SmallVector<Value, 4> readIndices;
-    SmallVector<Value, 4> writeIndices;
-    for (decltype(rank) i = 0; i < rank; ++i) {
-      Value inIndexFloat;
-      Value outIndex = outputLoops.getInductionVar(i);
-      Value outIndexInteger = rewriter.create<arith::IndexCastOp>(
-          loc, rewriter.getIntegerType(64), outIndex);
-      Value outIndexFloat =
-          create.math.cast(rewriter.getF32Type(), outIndexInteger);
+            // Handle coordinate transformation
+            if (resizeOp.coordinate_transformation_mode() == "asymmetric") {
+              inIndexFloat = create.math.div(outIndexFloat, scaleValues[i]);
+            } else if (resizeOp.coordinate_transformation_mode() ==
+                       "half_pixel") {
+              // If coordinate_transformation_mode is "half_pixel",
+              // x_original = (x_resized + 0.5) / scale - 0.5,
+              Value halfPixelConstant =
+                  create.math.constant(rewriter.getF32Type(), 0.5);
+              Value addValue =
+                  create.math.add(outIndexFloat, halfPixelConstant);
+              Value divValue = create.math.div(addValue, scaleValues[i]);
+              inIndexFloat = create.math.sub(divValue, halfPixelConstant);
+            }
 
-      // Handle coordinate transformation
-      if (resizeOp.coordinate_transformation_mode() == "asymmetric") {
-        inIndexFloat = create.math.div(outIndexFloat, scaleValues[i]);
-      } else if (resizeOp.coordinate_transformation_mode() == "half_pixel") {
-        // If coordinate_transformation_mode is "half_pixel",
-        // x_original = (x_resized + 0.5) / scale - 0.5,
-        Value halfPixelConstant =
-            create.math.constant(rewriter.getF32Type(), 0.5);
-        Value addValue = create.math.add(outIndexFloat, halfPixelConstant);
-        Value divValue = create.math.div(addValue, scaleValues[i]);
-        inIndexFloat = create.math.sub(divValue, halfPixelConstant);
-      }
+            // Handle nearest_mode
+            if (resizeOp.nearest_mode() == "round_prefer_floor") {
+              // round_prefer_floor will round 2.5 to 2, not 3
+              Value deltaConstant =
+                  create.math.constant(rewriter.getF32Type(), 0.499999);
+              inIndexFloat = create.math.add(inIndexFloat, deltaConstant);
+            } else if (resizeOp.nearest_mode() == "round_prefer_ceil") {
+              Value deltaConstant =
+                  create.math.constant(rewriter.getF32Type(), 0.5);
+              inIndexFloat = create.math.add(inIndexFloat, deltaConstant);
+            } else if (resizeOp.nearest_mode() == "floor") {
+              // Not supported by create.math
+              inIndexFloat = rewriter.create<math::FloorOp>(loc, inIndexFloat);
+            } else if (resizeOp.nearest_mode() == "ceil") {
+              // Not supported by create.math
+              inIndexFloat = rewriter.create<math::CeilOp>(loc, inIndexFloat);
+            } else {
+              llvm_unreachable("Unexpected nearest_mode() for ResizeOp");
+            }
 
-      // Handle nearest_mode
-      if (resizeOp.nearest_mode() == "round_prefer_floor") {
-        // round_prefer_floor will round 2.5 to 2, not 3
-        Value deltaConstant =
-            create.math.constant(rewriter.getF32Type(), 0.499999);
-        inIndexFloat = create.math.add(inIndexFloat, deltaConstant);
-      } else if (resizeOp.nearest_mode() == "round_prefer_ceil") {
-        Value deltaConstant = create.math.constant(rewriter.getF32Type(), 0.5);
-        inIndexFloat = create.math.add(inIndexFloat, deltaConstant);
-      } else if (resizeOp.nearest_mode() == "floor") {
-        // Not supported by create.math
-        inIndexFloat = rewriter.create<math::FloorOp>(loc, inIndexFloat);
-      } else if (resizeOp.nearest_mode() == "ceil") {
-        // Not supported by create.math
-        inIndexFloat = rewriter.create<math::CeilOp>(loc, inIndexFloat);
-      } else {
-        llvm_unreachable("Unexpected nearest_mode() for ResizeOp");
-      }
+            // FPToSIOp is round-to-zero, same as floor for positive
+            Value inIndexInteger =
+                create.math.cast(rewriter.getIntegerType(64), inIndexFloat);
 
-      // FPToSIOp is round-to-zero, same as floor for positive
-      Value inIndexInteger =
-          create.math.cast(rewriter.getIntegerType(64), inIndexFloat);
+            // When the index is out of bound, use the boundary index.
+            // This is equivalent to np.pad with mode = "edge"
 
-      // When the index is out of bound, use the boundary index.
-      // This is equivalent to np.pad with mode = "edge"
+            // Compare with integer type because lower bound may be negative
+            Value lessThanZero = create.math.slt(inIndexInteger, zero);
+            Value inIndexLBPadded =
+                create.math.select(lessThanZero, zero, inIndexInteger);
 
-      // Compare with integer type because lower bound may be negative
-      Value lessThanZero = create.math.slt(inIndexInteger, zero);
-      Value inIndexLBPadded =
-          create.math.select(lessThanZero, zero, inIndexInteger);
+            // Upper bound comparison can be done with Index type
+            inIndexLBPadded = create.math.castToIndex(inIndexLBPadded);
+            Value inputDim = dataBounds.getDim(i).getValue();
 
-      // Upper bound comparison can be done with Index type
-      inIndexLBPadded = create.math.castToIndex(inIndexLBPadded);
-      Value inputDim = dataBounds.getDim(i).getValue();
+            Value lessThanDim = create.math.slt(inIndexLBPadded, inputDim);
+            Value inputDimMinus = create.math.sub(inputDim, one);
+            Value inIndexPadded =
+                create.math.select(lessThanDim, inIndexLBPadded, inputDimMinus);
 
-      Value lessThanDim = create.math.slt(inIndexLBPadded, inputDim);
-      Value inputDimMinus = create.math.sub(inputDim, one);
-      Value inIndexPadded =
-          create.math.select(lessThanDim, inIndexLBPadded, inputDimMinus);
-
-      readIndices.emplace_back(inIndexPadded);
-      writeIndices.emplace_back(outIndex);
-    }
-    Value loadVal = create.krnl.load(data, readIndices);
-    create.krnl.store(loadVal, alloc, writeIndices);
+            readIndices.emplace_back(inIndexPadded);
+          }
+          Value loadVal = createKrnl.load(data, readIndices);
+          createKrnl.store(loadVal, alloc, loopInd);
+        });
 
     rewriter.replaceOp(op, alloc);
     return success();
