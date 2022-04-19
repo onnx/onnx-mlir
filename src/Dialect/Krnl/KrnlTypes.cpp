8580d7222d45693a13b33bfa41faa9a0d44bd0d8
Tung D. Le <tung@jp.ibm.com>
2022-04-19 12:02:24 +0900

    Do not use LLVMTypeConverter to support MemRefs with affine_map
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- src/Dialect/Krnl/KrnlTypes.cpp
+++ src/Dialect/Krnl/KrnlTypes.cpp
@@ -20,22 +20,6 @@ using namespace onnx_mlir;
 namespace onnx_mlir {
 namespace krnl {
 
-void customizeTypeConverter(LLVMTypeConverter &typeConverter) {
-  typeConverter.addConversion([&](MemRefType type) -> llvm::Optional<Type> {
-    Type elementType = type.getElementType();
-    if (!elementType.isa<krnl::StringType>())
-      return llvm::None;
-
-    elementType =
-        elementType.cast<krnl::StringType>().getLLVMType(type.getContext());
-    return typeConverter.convertType(
-        MemRefType::get(type.getShape(), elementType));
-  });
-
-  typeConverter.addConversion([&](krnl::StringType type) -> Type {
-    return typeConverter.convertType(type.getLLVMType(type.getContext()));
-  });
-}
 
 } // namespace krnl
 } // namespace onnx_mlir
