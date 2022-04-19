c51907d5c16a3b593943ef605f74b02ae03ddc39
Robbie Perrone <robert.perrone1@marist.edu>
2022-04-19 11:28:17 -0400

    Relu - Constant Propagation
    Signed-off-by: Robbie Perrone <robert.perrone1@marist.edu>

--- src/Transform/ONNX/ConstProp.cpp
+++ src/Transform/ONNX/ConstProp.cpp
@@ -392,6 +392,12 @@ template <typename T>
 struct ElementWiseUnaryOpImpl<ONNXSqrtOp, T> {
   static T impl(T val) { return sqrt(val); }
 };
+
+template <typename T>
+struct ElementWiseUnaryOpImpl<ONNXReluOp, T> {
+  static T impl(T val) { if(val < 0){return 0;} else {return val;} }
+};
+
 template <typename OP, typename T>
 T ComputeConstPropElementwiseUnary(T val) {
   return ElementWiseUnaryOpImpl<OP, T>::impl(val);
