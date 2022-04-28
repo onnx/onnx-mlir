61584cc4719d0ca92e920d2ec0487c616380cb75
Alexandre Eichenberger <alexe@us.ibm.com>
2022-04-28 16:08:24 +0000

    update
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Accelerators/CreateAcceleratorsInc.cmake
+++ src/Accelerators/CreateAcceleratorsInc.cmake
@@ -3,8 +3,8 @@
 # We need to do this in a stand-alone cmake script because there's no
 # cross-platform way to append to a file through add_custom_command.
 
-file(WRITE "${INC_FILE}.tmp" "#define VA_ARGS(...) , ##__VA_ARGS__ \\\n")
-file(WRITE "${INC_FILE}.tmp" "#define APPLY_TO_ACCELERATORS(MACRO, ...) \\\n")
+file(WRITE "${INC_FILE}.tmp" "#define VA_ARGS(...) , ##__VA_ARGS__ \n")
+file(APPEND "${INC_FILE}.tmp" "#define APPLY_TO_ACCELERATORS(MACRO, ...) \\\n")
 
 foreach(t ${ACCELERATORS})
   file (APPEND "${INC_FILE}.tmp" "  MACRO(${t} VA_ARGS(__VA_ARGS__)) \\\n")
