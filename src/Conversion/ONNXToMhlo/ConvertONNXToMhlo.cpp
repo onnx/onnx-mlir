63a9d3acc7a496328df7fda852f04e7a21a27759
Chengji Yao <chengji.yao@bytedance.com>
2022-05-09 13:07:59 +0800

    [Mhlo] add lowering-to-mhlo pass
    Signed-off-by: yan.xu <yan.xu@bytedance.com>
    UA
    Signed-off-by: Chengji Yao <chengji.yao@bytedance.com>
    Signed-off-by: Yan Xu <yan.xu@bytedance.com>

---
+++ src/Conversion/ONNXToMhlo/ConvertONNXToMhlo.cpp
@@ -0,0 +1,107 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//====------ ConvertONNXToMhlo.cpp - ONNX dialects to Mhlo lowering -------===//
+//
+// Copyright 2019-2022 The IBM Research Authors.
+//
+// =============================================================================
+//
+// This file implements the lowering of frontend operations to a combination of
+// Mhlo IR and standard operations.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/Dialect/StandardOps/Transforms/FuncConversions.h"
+
+#include "src/Accelerators/Accelerator.hpp"
+
+#include "mlir-hlo/Dialect/mhlo/IR/hlo_ops.h"
+
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Math/IR/Math.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "mlir/Dialect/StandardOps/Transforms/FuncConversions.h"
+#include "mlir/Dialect/StandardOps/Transforms/Passes.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/Sequence.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+#include "src/Dialect/Mlir/IndexExpr.hpp"
+#include "src/Dialect/ONNX/DialectBuilder.hpp"
+#include "src/Dialect/ONNX/ONNXOps.hpp"
+#include "src/Dialect/ONNX/ONNXOpsHelper.hpp"
+#include "src/Pass/Passes.hpp"
+#include "src/Transform/ONNX/ConstPropHelper.hpp"
+
+using namespace mlir;
+
+namespace onnx_mlir {
+
+void populateONNXToMhloConversionPattern(
+    RewritePatternSet &patterns, MLIRContext *ctx) {
+}
+
+//===----------------------------------------------------------------------===//
+// Frontend to Mhlo Dialect lowering pass
+//===----------------------------------------------------------------------===//
+
+struct FrontendToMhloLoweringPass
+    : public PassWrapper<FrontendToMhloLoweringPass, OperationPass<ModuleOp>> {
+
+  StringRef getArgument() const override { return "convert-onnx-to-mhlo"; }
+
+  StringRef getDescription() const override {
+    return "Lower frontend ops to Mhlo dialect.";
+  }
+
+  // Make sure that we have a valid default constructor and copy
+  // constructor to make sure that the options are initialized properly.
+  FrontendToMhloLoweringPass() = default;
+  FrontendToMhloLoweringPass(const FrontendToMhloLoweringPass &pass)
+      : PassWrapper<FrontendToMhloLoweringPass, OperationPass<ModuleOp>>() {}
+
+  void runOnOperation() final;
+};
+
+void FrontendToMhloLoweringPass::runOnOperation() {
+  ModuleOp module = getOperation();
+
+  // The first thing to define is the conversion target. This will define the
+  // final target for this lowering.
+  ConversionTarget target(getContext());
+
+  // We define the specific operations, or dialects, that are legal targets for
+  // this lowering.
+  target.addLegalDialect<mhlo::MhloDialect, StandardOpsDialect>();
+  // Needed to support unsigned int computations. To be removed if we use a
+  // scheme that does not rely on the UnrealizedConversionCastOp.
+  target.addLegalOp<::mlir::UnrealizedConversionCastOp>();
+
+  // Now that the conversion target has been defined, we just need to provide
+  // the set of patterns that will lower the frontend operations.
+  RewritePatternSet patterns(&getContext());
+
+  // Define patterns.
+  populateONNXToMhloConversionPattern(patterns, &getContext());
+
+  // With the target and rewrite patterns defined, we can now attempt the
+  // conversion. The conversion will signal failure if any of our `illegal`
+  // operations were not converted successfully.
+  if (failed(applyPartialConversion(module, target, std::move(patterns)))) {
+    signalPassFailure();
+  }
+}
+
+std::unique_ptr<Pass> createLowerToMhloPass() {
+  return std::make_unique<FrontendToMhloLoweringPass>();
+}
+
+} // namespace onnx_mlir
--- src/Tools/onnx-mlir-opt/onnx-mlir-opt.cpp
+++ src/Tools/onnx-mlir-opt/onnx-mlir-opt.cpp
@@ -37,6 +37,8 @@
 #include "src/InitOMPasses.hpp"
 #include "src/Pass/Passes.hpp"
 
+#include "mlir-hlo/Dialect/mhlo/IR/hlo_ops.h"
+
 using namespace mlir;
 using namespace onnx_mlir;
 
@@ -115,6 +117,7 @@ int main(int argc, char **argv) {
   registry.insert<mlir::ONNXDialect>();
   registry.insert<mlir::KrnlOpsDialect>();
   registry.insert<mlir::tosa::TosaDialect>();
+  registry.insert<mlir::mhlo::MhloDialect>();
 
   // Initialize accelerators if they exist.
   onnx_mlir::accel::initAccelerators(maccel);
