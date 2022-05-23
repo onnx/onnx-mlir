7591500ce6516d01c53c27a792a9f02138ba0958
Vasantha Ganesh Kanniappan <vasantha@helprack.com>
2022-05-23 16:52:02 +0530

    Docs

--- src/Conversion/ONNXToTorch/NN/SqueezeOp.cpp
+++ src/Conversion/ONNXToTorch/NN/SqueezeOp.cpp
@@ -44,7 +44,6 @@ std::vector<int> getSortedWithNegativeAxes(mlir::ArrayAttr axesRaw) {
   return axesSorted;
 }
 
-
 std::vector<int> getAxes(ONNXSqueezeOp squeezeOp) {
   auto builder = mlir::Builder(squeezeOp.getContext());
   auto axesConstOp = getONNXConstantOp(squeezeOp.axes());
@@ -69,8 +68,8 @@ mlir::Value squeezeResult(std::vector<int> axes, mlir::Value dataTensor,
       // This works because, axes is sorted and normalized to possitive integers
       auto dim_raw = axes[i] - i;
       // Fail when dimension squeeze is not 1?
-      // assert((result.getType().dyn_cast<TensorType>().getShape()[dim_raw] == 1) && "Cannot squeeze for
-      // dim");
+      // assert((result.getType().dyn_cast<TensorType>().getShape()[dim_raw] ==
+      // 1) && "Cannot squeeze for dim");
       Value dim = getIntValue(dim_raw, rewriter, context, loc);
       result = rewriter.create<AtenSqueezeDimOp>(loc, resultType, result, dim);
     }
@@ -80,6 +79,47 @@ mlir::Value squeezeResult(std::vector<int> axes, mlir::Value dataTensor,
 
   return result;
 }
+
+/**
+ *
+ * ONNX SqueezeV13/SqueezeV11
+ *
+ * Remove single-dimensional entries from the shape of a tensor Takes an input
+ * axes with a list of axes to squeeze. If axes is not provided, all the
+ * single dimensions will be removed from the shape. If an axis is selected
+ * with shape entry not equal to one, an error is raised.
+ *
+ *
+ * Operands :
+ *   data       tensor of 8-bit unsigned integer values or tensor of 16-bit
+ * unsigned integer values or tensor of 32-bit unsigned integer values or tensor
+ * of 64-bit unsigned integer values or tensor of 8-bit signless integer values
+ * or tensor of 16-bit signless integer values or tensor of 32-bit signless
+ * integer values or tensor of 64-bit signless integer values or tensor of
+ * bfloat16 type values or tensor of 16-bit float values or tensor of 32-bit
+ * float values or tensor of 64-bit float values or tensor of string type values
+ * or tensor of 1-bit signless integer values or tensor of complex type with
+ * 32-bit float elements values or tensor of complex type with 64-bit float
+ * elements values or memref of any type values
+ *
+ *   axes       (attr/operand) tensor of 64-bit signless integer values or
+ * memref of any type values or none type
+ *
+ *
+ * Output   :
+ *   output   	tensor of 8-bit unsigned integer
+ * values or tensor of 16-bit unsigned integer values or tensor of 32-bit
+ * unsigned integer values or tensor of 64-bit unsigned integer values or tensor
+ * of 8-bit signless integer values or tensor of 16-bit signless integer values
+ * or tensor of 32-bit signless integer values or tensor of 64-bit signless
+ * integer values or tensor of bfloat16 type values or tensor of 16-bit float
+ * values or tensor of 32-bit float values or tensor of 64-bit float values or
+ * tensor of string type values or tensor of 1-bit signless integer values or
+ * tensor of complex type with 32-bit float elements values or tensor of complex
+ * type with 64-bit float elements values or memref of any type values
+ *
+ *
+ */
 
 template <typename SqueezeOp>
 struct ONNXToTorchSqueezeOpLowering : public ConversionPattern {
@@ -113,8 +153,7 @@ struct ONNXToTorchSqueezeOpLowering : public ConversionPattern {
 void populateLoweringONNXToTorchSqueezeOpPattern(RewritePatternSet &patterns,
                                                  TypeConverter &typeConverter,
                                                  MLIRContext *ctx) {
-  patterns
-    .insert<ONNXToTorchSqueezeOpLowering<ONNXSqueezeV11Op>,
-            ONNXToTorchSqueezeOpLowering<ONNXSqueezeOp>>(
-          typeConverter, ctx);
+  patterns.insert<ONNXToTorchSqueezeOpLowering<ONNXSqueezeV11Op>,
+                  ONNXToTorchSqueezeOpLowering<ONNXSqueezeOp>>(typeConverter,
+                                                               ctx);
 }
