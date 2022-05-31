c6ec8abd0eb36fd46fb31b1778dd71fef31a92cb
Gong Su <gong_su@hotmail.com>
2022-05-31 05:01:31 -0400

    Fix docker run the input device is not a TTY
    Signed-off-by: Gong Su <gong_su@hotmail.com>

--- .buildbot/jenkins-check-model-zoo.py
+++ .buildbot/jenkins-check-model-zoo.py
@@ -21,43 +21,49 @@ MEMORY_IN_GB               = (os.sysconf('SC_PAGE_SIZE') *
                               os.sysconf('SC_PHYS_PAGES') / (1024.**3))
 NPROC                      = str(math.ceil(min(max(2, MEMORY_IN_GB/4), os.cpu_count())))
 
-JENKINS_HOME               = os.getenv('JENKINS_HOME')
-JOB_NAME                   = os.getenv('JOB_NAME')
-WORKSPACE                  = os.getenv('WORKSPACE')
-MODELZOO_WORKDIR           = os.getenv('MODELZOO_WORKDIR')
-MODELZOO_HTML              = os.getenv('MODELZOO_HTML')
-MODELZOO_STDOUT            = os.getenv('MODELZOO_STDOUT')
-
 DOCKER_DEV_IMAGE_WORKDIR   = '/workdir'
 RUN_ONNX_MODEL_PY          = 'RunONNXModel.py'
 RUN_ONNX_MODELZOO_PY       = 'RunONNXModelZoo.py'
 
-WORKSPACE_MODELZOO         = os.path.join(JENKINS_HOME, 'workspace',
-                                          JOB_NAME+'@'+MODELZOO_WORKDIR)
-
+docker_daemon_socket       = os.getenv('DOCKER_DAEMON_SOCKET')
+docker_registry_host_name  = os.getenv('DOCKER_REGISTRY_HOST_NAME')
+docker_registry_user_name  = os.getenv('DOCKER_REGISTRY_USER_NAME')
 github_repo_name           = os.getenv('GITHUB_REPO_NAME')
 github_repo_name2          = os.getenv('GITHUB_REPO_NAME').replace('-', '_')
 github_pr_baseref          = os.getenv('GITHUB_PR_BASEREF')
 github_pr_baseref2         = os.getenv('GITHUB_PR_BASEREF').lower()
+github_pr_number           = os.getenv('GITHUB_PR_NUMBER')
+jenkins_home               = os.getenv('JENKINS_HOME')
+job_name                   = os.getenv('JOB_NAME')
+workspace                  = os.getenv('WORKSPACE')
+modelzoo_workdir           = os.getenv('MODELZOO_WORKDIR')
+modelzoo_html              = os.getenv('MODELZOO_HTML')
+modelzoo_stdout            = os.getenv('MODELZOO_STDOUT')
+
 docker_dev_image_name      = (github_repo_name + '-dev' +
                               ('.' + github_pr_baseref2
                                if github_pr_baseref != 'main' else ''))
+docker_dev_image_tag       = github_pr_number.lower()
+workspace_modelzoo         = os.path.join(jenkins_home, 'workspace',
+                                          job_name+'@'+modelzoo_workdir)
 
 def main():
-    cmd = [ 'docker', 'run', '--rm', '-ti',
+    cmd = [ 'docker', 'run', '--rm',
             '-e', ('ONNX_MLIR_HOME=' +
-                   os.path.join(WORKSPACE, 'build', 'Debug')),
-            '-v', (WORKSPACE_MODELZOO + ':' +
-                   os.path.join(DOCKER_DEV_IMAGE_WORKDIR, MODELZOO_WORKDIR)),
-            '-v', (os.path.join(WORKSPACE, 'utils', RUN_ONNX_MODEL_PY) + ':' +
+                   os.path.join(workspace, 'build', 'Debug')),
+            '-v', (workspace_modelzoo + ':' +
+                   os.path.join(DOCKER_DEV_IMAGE_WORKDIR, modelzoo_workdir)),
+            '-v', (os.path.join(workspace, 'utils', RUN_ONNX_MODEL_PY) + ':' +
                    os.path.join(DOCKER_DEV_IMAGE_WORKDIR, RUN_ONNX_MODEL_PY)),
-            '-v', (os.path.join(WORKSPACE, 'utils', RUN_ONNX_MODELZOO_PY) + ':' +
+            '-v', (os.path.join(workspace, 'utils', RUN_ONNX_MODELZOO_PY) + ':' +
                    os.path.join(DOCKER_DEV_IMAGE_WORKDIR, RUN_ONNX_MODELZOO_PY)),
-            docker_dev_image_name,
+            ((docker_registry_host_name + '/' if docker_registry_host_name else '') +
+             (docker_registry_user_name + '/' if docker_registry_user_name else '') +
+             docker_dev_image_name + ':' + docker_dev_image_tag),
             os.path.join(DOCKER_DEV_IMAGE_WORKDIR, RUN_ONNX_MODELZOO_PY),
             '-j', NPROC,
-            '-w', MODELZOO_WORKDIR,
-            '-H', MODELZOO_HTML,
+            '-w', modelzoo_workdir,
+            '-H', modelzoo_html,
             '-l', 'debug',
             '-k' ]
 
@@ -74,7 +80,7 @@ def main():
         sys.exit(proc.returncode)
 
     # write summary line to file for Jenkinsfile to pickup
-    with open(os.path.join(WORKSPACE_MODELZOO, MODELZOO_STDOUT), 'w') as f:
+    with open(os.path.join(workspace_modelzoo, modelzoo_stdout), 'w') as f:
         f.write(proc.stdout.decode('utf-8'))
     sys.exit(0)
 
