0d54459932012d52a10e347b293ebab1fa6b9b64
Vasantha Ganesh Kanniappan <vasantha@helprack.com>
2022-06-02 13:24:15 +0530

    Implement interim types

--- src/Conversion/ONNXToTorch/NN/SqueezeOp.cpp
+++ src/Conversion/ONNXToTorch/NN/SqueezeOp.cpp
@@ -57,13 +57,38 @@ std::vector<int> getAxes(ONNXSqueezeV11Op squeezeOp) {
   return getSortedWithNonNegativeAxes(squeezeOp.axesAttr());
 }
 
+std::vector<int64_t> getResultShape(int dim_index,
+                                    std::vector<int64_t> tensorShape) {
+
+  std::vector<int64_t> intermShape;
+
+  for(uint64_t i =0; i < tensorShape.size(); i++) {
+    if(i != (unsigned long)dim_index)
+      intermShape.push_back(tensorShape[i]);
+  }
+
+  return intermShape;
+}
+
+Torch::ValueTensorType getResultType(std::vector<int64_t> tensorShape,
+                                     mlir::Type elementType,
+                                     MLIRContext *context) {
+  return Torch::ValueTensorType::get(context,
+       		llvm::makeArrayRef(tensorShape),
+      		elementType);
+}
+
 mlir::Value squeezeResult(std::vector<int> axes, mlir::Value dataTensor,
                           Torch::ValueTensorType resultType,
+                          ArrayRef<int64_t> tensorShape,
+                          mlir::Type elementType,
                           ConversionPatternRewriter &rewriter,
                           mlir::MLIRContext *context, Location loc) {
   Value result = dataTensor;
 
   if (axes.size() > 0) {
+    std::vector<int64_t> shape(tensorShape.begin(), tensorShape.end());
+
     for (uint64_t i = 0; i < axes.size(); i++) {
       // With every successive deleting on dimension, the input axis
       // changes to `axis = axis - number_of_dimensions_deleted`
@@ -73,7 +98,9 @@ mlir::Value squeezeResult(std::vector<int> axes, mlir::Value dataTensor,
       // assert((result.getType().dyn_cast<TensorType>().getShape()[dim_raw] ==
       // 1) && "Cannot squeeze for dim");
       Value dim = getIntValue(dim_raw, rewriter, context, loc);
-      result = rewriter.create<AtenSqueezeDimOp>(loc, resultType, result, dim);
+      shape = getResultShape(dim_raw, shape);
+      auto interimType = getResultType(shape, elementType, context);
+      result = rewriter.create<AtenSqueezeDimOp>(loc, interimType, result, dim);
     }
   } else {
     result = rewriter.create<AtenSqueezeOp>(loc, resultType, dataTensor);
@@ -144,7 +171,10 @@ struct ONNXToTorchSqueezeOpLowering : public ConversionPattern {
 
     auto resultType = toTorchType(context, squeezeOp.getResult().getType());
     auto result =
-        squeezeResult(axes, dataTensor, resultType, rewriter, context, loc);
+        squeezeResult(axes, dataTensor, resultType,
+                      squeezeOp.data().getType().template dyn_cast<TensorType>().getShape(),
+                      squeezeOp.data().getType().template dyn_cast<TensorType>().getElementType(),
+                      rewriter, context, loc);
 
     rewriter.replaceOpWithNewOp<TensorStaticInfoCastOp>(op, resultType, result);
 
