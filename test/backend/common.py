1457b65a946578b9b2d912361f279ddcaada5478
Haruki Imai <imaihal@jp.ibm.com>
2022-06-02 07:40:53 +0000

    [Test] Check if check_instruction() works
    Signed-off-by: Haruki Imai <imaihal@jp.ibm.com>

--- test/backend/common.py
+++ test/backend/common.py
@@ -15,7 +15,6 @@ from __future__ import unicode_literals
 
 import sys
 import os
-import platform
 import ctypes
 import onnx
 import subprocess
@@ -68,22 +67,12 @@ def execute_commands(cmds, dynamic_inputs_dims):
 
 
 def check_instruction(exec_name, symbol_name):
-#    if not args.check_instruction:
-#        return True
-    
+#    if symbol_name and args.check_instruction
     if symbol_name:
         lib = ctypes.cdll.LoadLibrary(exec_name)
-        try:
-            symbol = getattr(lib, symbol_name)
-            print("Found symbol:" + symbol_name + " "
-                  + str(ctypes.addressof(symbol)))
-        except AttributeError:
-            print("undefined symbol: " + symbol_name)
-        platform_name = platform.system()
-        if platform_name == 'Windows':
-            _ctypes.FreeLibrary(lib._handle)
-        else:
-            _ctypes.dlclose(lib._handle)
+        # Raise AttributeError if symbol undefined
+        symbol = getattr(lib, symbol_name)
+        _ctypes.dlclose(lib._handle) # For Windows, _ctypes.FreeLibrary(lib._handle)
 
 
 def compile_model(model, emit):
@@ -154,8 +143,9 @@ def compile_model(model, emit):
     # Check if specific instruction are included in the compiled model.
     # target_symbol_name = "zdnn_matmul_op"
     target_symbol_name = 'omTensorGetShap'
-    # print(exec_name + " " + target_symbol_name)
-    if os.path.splitext(exec_name) == '.so':
+    print(exec_name + " " + target_symbol_name)
+    path, ext = os.path.splitext(exec_name)
+    if ext == '.so':
         check_instruction(exec_name, target_symbol_name)
 
     return exec_name
