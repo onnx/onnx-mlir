d188ecd132168d7b3d5548ac4b03a13dda38ab6e
Whitney Tsang <whitneyt@ca.ibm.com>
2022-06-07 21:43:44 -0400

    Add backend test for Is Nan
    Signed-off-by: Whitney Tsang <whitneyt@ca.ibm.com>

--- test/backend/inference_backend.py
+++ test/backend/inference_backend.py
@@ -363,6 +363,7 @@ def get_test_models():
         # Is Inf Neg/Pos
 
         # Is Nan
+        "test_isnan_cpu": {STATIC_SHAPE:{}, DYNAMIC_SHAPE:{-1:{-1}}, CONSTANT_INPUT:{-1}},
 
         # Leaky Relu
         "test_leakyrelu_cpu": {STATIC_SHAPE:{}, DYNAMIC_SHAPE:{-1:{-1}}, CONSTANT_INPUT:{-1}},
