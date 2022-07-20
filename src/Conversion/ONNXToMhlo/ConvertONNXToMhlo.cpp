6075a72adc74dc80029d45a226f0827cda09f991
Yan Xu <yan.xu@bytedance.com>
2022-07-20 16:45:26 -0400

    move function definition
    Signed-off-by: Yan Xu <yan.xu@bytedance.com>

--- src/Conversion/ONNXToMhlo/ConvertONNXToMhlo.cpp
+++ src/Conversion/ONNXToMhlo/ConvertONNXToMhlo.cpp
@@ -36,23 +36,6 @@ void populateONNXToMhloConversionPattern(
   populateLoweringONNXReshapeOpToMhloPattern(patterns, ctx);
 }
 
-Value getShapedZero(Location loc, ConversionPatternRewriter &rewriter,
-    const ShapedType &inpType, Value &inp, const Type &resultType) {
-  Value broadcastedZero;
-  if (inpType.hasStaticShape())
-    broadcastedZero =
-        rewriter.create<mhlo::ConstOp>(loc, rewriter.getZeroAttr(inpType));
-  else {
-    Type elemType = inpType.getElementType();
-    Value zero =
-        rewriter.create<mhlo::ConstOp>(loc, rewriter.getZeroAttr(elemType));
-    Value shape = rewriter.create<shape::ShapeOfOp>(loc, inp);
-    broadcastedZero = rewriter.create<mhlo::DynamicBroadcastInDimOp>(
-        loc, resultType, zero, shape, rewriter.getI64TensorAttr({}));
-  }
-  return broadcastedZero;
-}
-
 //===----------------------------------------------------------------------===//
 // Frontend to Mhlo Dialect lowering pass
 //===----------------------------------------------------------------------===//
