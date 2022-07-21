8e65a51870a161f3e6f2e477bc1d09ac2547e353
Yan Xu <yan.xu@bytedance.com>
2022-07-20 21:44:52 -0400

    [Mhlo] fix duplicate symbol
    Signed-off-by: Yan Xu <yan.xu@bytedance.com>

--- src/Transform/ONNX/Decompose.cpp
+++ src/Transform/ONNX/Decompose.cpp
@@ -126,11 +126,9 @@ Value createSequenceConstructOp(
 
 } // namespace onnx_mlir
 
+namespace {
 /// Include the patterns defined in the Declarative Rewrite framework.
 #include "src/Transform/ONNX/ONNXDecompose.inc"
-
-namespace {
-
 RankedTensorType createResultType(
     Type outputType, int64_t axisValue, bool keepDims) {
   RankedTensorType outputShapeType = outputType.dyn_cast<RankedTensorType>();
@@ -257,7 +255,7 @@ namespace onnx_mlir {
 
 void populateDecomposingONNXBeforeMhloPatterns(
     RewritePatternSet &patterns, MLIRContext *ctx) {
-  populateWithGenerated(patterns);
+  ::populateWithGenerated(patterns);
   patterns.add<SoftmaxPattern>(ctx);
 }
 /*!
