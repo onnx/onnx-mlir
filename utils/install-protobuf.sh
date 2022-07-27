a4952a40699d8c8b5a4ba3ad50303743f3023b16
Gong Su <gong_su@hotmail.com>
2022-07-26 22:20:55 -0400

    - change Windows install-protobuf.cmd to use git tag - update prereq in README.md and docs/Prerequisite.md
    Signed-off-by: Gong Su <gong_su@hotmail.com>

--- utils/install-protobuf.sh
+++ utils/install-protobuf.sh
@@ -1,3 +1,4 @@
+# Check out a specific tag v3.16.0 which is the recommended version of onnx 1.11.0
 PROTOBUF_VERSION=3.16.0
 git clone -b v${PROTOBUF_VERSION} --recursive https://github.com/google/protobuf.git
 
