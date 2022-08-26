ee70027ea2c5280243a94d421cc739efac06db76
Alexandre Eichenberger <alexe@us.ibm.com>
2022-08-26 18:23:34 +0000

    enable broadcast in matmul when both A & B have the same static dimensions in the broadcast dims
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- test/numerical/TestMatMulBroadcast.cpp
+++ test/numerical/TestMatMulBroadcast.cpp
@@ -31,25 +31,28 @@ namespace test {
 // parameters/configuration. Matmul: A[IxK] * B[KxJ] = C[IxJ].
 // Include broadcasting in either A or B.
 static bool isOMMatmulTheSameAsNaiveImplFor(bool broadcastingB,
-    std::vector<int64_t> &broadcastDims, const int I, const int J,
-    const int K) {
+    bool sameStaticBroadcast, std::vector<int64_t> &broadcastDims, const int I,
+    const int J, const int K) {
   static int testNum = 0;
-  printf("attempt %d with i %d, j %d, k %d and broadcasting %c with rank %i "
+  printf("attempt %d with i %d, j %d, k %d and broadcasting %s with rank %i "
          "and sizes (",
-      ++testNum, I, J, K, broadcastingB ? 'B' : 'A', (int)broadcastDims.size());
+      ++testNum, I, J, K,
+      sameStaticBroadcast ? "both" : (broadcastingB ? "B" : "A"),
+      (int)broadcastDims.size());
   for (unsigned int i = 0; i < broadcastDims.size(); ++i)
     printf("%s%lld", i > 0 ? ", " : "", (long long int)broadcastDims[i]);
   printf(")\n");
-  MatMulSingleBroadcastLibBuilder matmul(
-      SHARED_LIB_BASE.str(), broadcastingB, broadcastDims, I, J, K);
+  MatMulSingleBroadcastLibBuilder matmul(SHARED_LIB_BASE.str(), broadcastingB,
+      sameStaticBroadcast, broadcastDims, I, J, K);
   return matmul.build() && matmul.compileAndLoad() && matmul.prepareInputs() &&
          matmul.run() && matmul.verifyOutputs();
 }
 } // namespace test
 } // namespace onnx_mlir
 
-bool broadcastB; // indicates if we broadcast on B (true) or A (false).
-
+bool broadcastB;          // Indicates if we broadcast on B (true) or A (false).
+bool sameStaticBroadcast; // Indicates if both A and B have the same static
+                          // broadcast dims.
 int main(int argc, char *argv[]) {
   using namespace onnx_mlir;
   using namespace onnx_mlir::test;
@@ -65,25 +68,34 @@ int main(int argc, char *argv[]) {
             << getCompilerOption(OptionKind::TargetAccel) << "\"\n";
 
   bool success;
-  for (int broadcastDim = 0; broadcastDim < 2; ++broadcastDim) {
-    broadcastB = (broadcastDim == 0);
-    printf("RapidCheck Matrix-vector/Matrix with broadcast %c test case generation.\n",
-        broadcastB ? 'B' : 'A');
-    success =
-        rc::check("Matrix-vector/Matrix Matmul implementation correctness", []() {
+  for (int i = 0; i < 3; ++i) {
+    // Define same static, broadcast A, or broadcast B.
+    broadcastB = sameStaticBroadcast = false;
+    if (i == 0)
+      sameStaticBroadcast = true; // Same static broadcast.
+    else if (i == 1)              // Broadcast A.
+      broadcastB = false;
+    else if (i == 2) // Broadcast B.
+      broadcastB = true;
+    printf("RapidCheck Matrix-vector/Matrix with broadcast %s test case "
+           "generation.\n",
+        sameStaticBroadcast ? "same static" : (broadcastB ? "B" : "A"));
+    success = rc::check(
+        "Matrix-vector/Matrix Matmul implementation correctness", []() {
           // There is some tiling up to 64, so pick up a large UB number .
           const int I = *rc::gen::inRange(2, 80);
-          const int J = *rc::gen::inRange(1, 80);
+          const int J = *rc::gen::inRange(1, 80); // From 1 for mat-vect.
           const int K = *rc::gen::inRange(2, 80);
           const int broadcastRank =
               *rc::gen::inRange(1, 5); // Additional rank for broadcast.
           std::vector<int64_t> broadcastDims;
           for (int i = 0; i < broadcastRank; ++i)
-            // No need for large dims, as broadcast of 2 is enough to uncover bugs.
+            // No need for large dims, as broadcast of 2 is enough to uncover
+            // bugs.
             broadcastDims.emplace_back((int64_t)*rc::gen::inRange(2, 4));
 
           RC_ASSERT(isOMMatmulTheSameAsNaiveImplFor(
-              broadcastB, broadcastDims, I, J, K));
+              broadcastB, sameStaticBroadcast, broadcastDims, I, J, K));
         });
     if (!success)
       return 1;
