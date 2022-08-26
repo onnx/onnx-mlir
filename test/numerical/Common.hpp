d70c2c4035e0c10837f3784e153dbed25872ae44
Soren Lassen <sorenlassen@gmail.com>
2022-08-25 23:56:23 -0700

    silence the rapidcheck compiler warnings for gcc, clang
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>
    
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

--- test/numerical/Common.hpp
+++ test/numerical/Common.hpp
@@ -2,8 +2,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated"   // gcc, clang
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS // MSVC
 #include <rapidcheck.h>
 #undef _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#pragma GCC diagnostic pop
 
 #include "test/modellib/ModelLib.hpp"
