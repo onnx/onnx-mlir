d96746f11aff129aafcf7bde85551b31876f31c0
Ashay Rane <ashay@users.noreply.github.com>
2022-09-05 19:54:37 +0000

    avoid re-definition of macro
    This patch adds a guard statement to only define the
    `_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS` macro if it has not previously
    been defined.  Similarly, it checks to make sure that the macro has been
    defined before un-defining it, but only if it was defined in this file.
    
    This patch is necessary if there is a dependency on ORT or another third
    party library that sets the macro.  If there is no such dependency, then
    the macro is correctly set and unset in the root CMakeLists.txt, making
    this patch a NOP.
    
    Signed-off-by: Ashay Rane <ashay@users.noreply.github.com>

--- test/numerical/Common.hpp
+++ test/numerical/Common.hpp
@@ -3,14 +3,23 @@
  */
 
 #if defined(_MSC_VER)
+#ifndef _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
 #define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#else
+#define _SILENCE_WAS_PREDEFINED
+#endif
 #else // gcc, clang
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated"
 #endif
+
 #include <rapidcheck.h>
+
 #if defined(_MSC_VER)
+#ifdef _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS &&                              \
+    !defined _SILENCE_WAS_PREDEFINED
 #undef _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#endif
 #else
 #pragma GCC diagnostic pop
 #endif
