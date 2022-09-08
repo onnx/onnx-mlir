1056af54cc6b60291a1cfc789de7a72b61b05c10
Soren Lassen <sorenlassen@gmail.com>
2022-09-07 19:32:30 -0700

    added onnx2json script
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

---
+++ utils/onnx2json.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0
+
+##################### RunONNXModel.py #########################################
+#
+# Converts a binary onnx model to json.
+# Useful for constructing test/mlir/onnx/parse lit tests.
+#
+################################################################################
+
+import argparse
+from onnx import onnx_ml_pb2
+from google.protobuf.json_format import MessageToJson
+
+parser = argparse.ArgumentParser()
+parser.add_argument('model_path', type=str, help="Path to the ONNX model")
+args = parser.parse_args()
+
+def main():
+    f = open(args.model_path, "rb")
+    model = onnx_ml_pb2.ModelProto()
+    model.ParseFromString(f.read())
+    json=MessageToJson(model)
+    print(json)
+
+if __name__ == '__main__':
+    main()
