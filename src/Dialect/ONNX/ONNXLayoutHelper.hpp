3663cd2f98ad0b4040214c6b71ef5bb18c4279e9
Alexandre Eichenberger <alexe@us.ibm.com>
2022-09-20 01:18:02 +0000

    version that works
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

---
+++ src/Dialect/ONNX/ONNXLayoutHelper.hpp
@@ -0,0 +1,24 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===---------- ONNXLayoutHelper.hpp - ONNX Layout Helper ---------------------===//
+//
+// Copyright 2022 The IBM Research Authors.
+//
+// =============================================================================
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/PatternMatch.h"
+
+namespace onnx_mlir {
+
+/// Note: Keep these strings in sync with the one in Dialect/ONNX/ONNX.td.
+const std::string LAYOUT_NCHW4C = "NCHW4C";
+const std::string LAYOUT_KCMN4C4K = "KCMN4C4K";
+
+} // namespace onnx_mlir
