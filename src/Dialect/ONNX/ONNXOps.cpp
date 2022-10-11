586e35de54153ff9e289b1617f7ad46c915a49e2
Philip Lassen <plassen@groq.com>
2022-10-10 23:49:15 -0700

    Add upsample
    Signed-off-by: Philip Lassen <plassen@groq.com>

--- src/Dialect/ONNX/ONNXOps.cpp
+++ src/Dialect/ONNX/ONNXOps.cpp
@@ -5476,7 +5476,58 @@ LogicalResult ONNXUniqueOp::inferShapes(
 
 LogicalResult ONNXUpsampleOp::inferShapes(
     std::function<void(mlir::Region &)> doShapeInference) {
-  return emitError(NOT_IMPLEMENTED_MESSAGE);
+  // Sanity checks on input data argument
+  if (!X().getType().isa<RankedTensorType>()) {
+    return success();
+  }
+  if (!scales().getType().isa<RankedTensorType>()) {
+    return success();
+  }
+
+  auto inputTy = X().getType().cast<RankedTensorType>();
+  int32_t inputRank = inputTy.getShape().size();
+
+  // Sanity checks on scale argument
+  auto scalesTy = scales().getType().cast<RankedTensorType>();
+  if (scalesTy.getShape().size() != 1) {
+    return emitError("Scales tensor must be rank-1");
+  }
+  if (scalesTy.getShape()[0] != inputRank) {
+    return emitError("Input tensor rank doesn't match scales tensor shape");
+  }
+
+  SmallVector<int64_t, 4> outputDims(inputRank, -1);
+
+  // Extract the scale values
+  auto scalesConstOp = getONNXConstantOp(scales());
+  if (!scalesConstOp) {
+    return success();
+  }
+  auto valueAttr = scalesConstOp.valueAttr().dyn_cast<DenseElementsAttr>();
+  if (!valueAttr) {
+    return emitError("Scales constant is not a DenseElementsAttr");
+  }
+  int scaleIdx = 0;
+  // Why are the scale values float's?
+  for (auto it = valueAttr.getValues<FloatAttr>().begin();
+       it != valueAttr.getValues<FloatAttr>().end(); ++it) {
+    if (scaleIdx >= inputRank) {
+      return emitError("Scales tensor shape doesn't match # of scale values");
+    }
+    outputDims[scaleIdx++] = (int)((*it).getValueAsDouble());
+  }
+  if (scaleIdx != inputRank) {
+    return emitError("Scales tensor shape doesn't match # of scale values");
+  }
+
+  // Compute and set the output shape
+  for (int i = 0; i < inputRank; ++i) {
+    outputDims[i] *= inputTy.getShape()[i];
+  }
+  getResult().setType(
+      RankedTensorType::get(outputDims, inputTy.getElementType()));
+
+  return success();
 }
 
 LogicalResult ONNXWhereOp::inferShapes(
