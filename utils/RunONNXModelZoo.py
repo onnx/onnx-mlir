65b2775e4af7a67135289afdd3bdee42d4347129
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 10:28:30 +0000

    Adding tarfile member sanitization to extractall()

--- utils/RunONNXModelZoo.py
+++ utils/RunONNXModelZoo.py
@@ -264,7 +264,26 @@ def check_model(model_path, model_name, compile_args, report_dir):
         # untar
         logger.debug('Extracting the .tar.gz to {}'.format(tmpdir))
         with tarfile.open(model_path, 'r:gz') as tgz:
-            tgz.extractall(tmpdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tgz, tmpdir)
         # ignore files starting with "." created by Mac OSX!
         _, onnx_files = execute_commands(
             ['find', tmpdir, '-type', 'f', '-name', '[^.]*.onnx'])
