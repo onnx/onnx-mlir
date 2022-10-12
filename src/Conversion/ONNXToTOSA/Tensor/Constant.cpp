4984a8653d37ee680d9c976dd22750b1b2b82fe7
Ferdinand Lemaire <ferdinan@xilinx.com>
2022-10-12 14:07:51 +0200

    Constant

--- src/Conversion/ONNXToTOSA/Tensor/Constant.cpp
+++ src/Conversion/ONNXToTOSA/Tensor/Constant.cpp
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-//===---------------- Elementwise.cpp - Elementwise Op --------------------===//
+//===---------------- Constant.cpp - Const Op --------------------===//
 //
 // Copyright (c) 2022 Advanced Micro Devices, Inc.
 //
