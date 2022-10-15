e647138e43fa7d7c23c51366f326e400c02a1341
Soren Lassen <sorenlassen@gmail.com>
2022-10-15 14:51:29 -0700

    template makeDenseIntOrFPElementsAttr
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

--- src/Dialect/Mlir/AttributesHelper.cpp
+++ src/Dialect/Mlir/AttributesHelper.cpp
@@ -11,6 +11,7 @@
 #include "src/Dialect/Mlir/AttributesHelper.hpp"
 
 #include "mlir/IR/AsmState.h"
+#include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinDialect.h"
 #include "mlir/IR/DialectResourceBlobManager.h"
@@ -22,15 +23,14 @@ using namespace mlir;
 namespace onnx_mlir {
 
 ElementsAttr makeDenseIntOrFPElementsAttr(
-    ShapedType type, char *data, size_t size) {
-  // TODO: test type.getElementType() is int or fp
-  ArrayRef<char> bytes = llvm::makeArrayRef(data, size);
+    ShapedType type, ArrayRef<char> bytes, size_t align) {
+  assert(type.getElementType().isIntOrFloat());
   if (ResourcePool *resourcePool = ResourcePool::get(type.getContext());
       resourcePool && resourcePool->isActive()) {
     // TODO: consider aligning everything to something large that works well for
     //       everything, e.g. 8 for double and i64, or 16 or 64 for SIMD ops
     mlir::DenseResourceElementsHandle r = resourcePool->createResource(
-        HeapAsmResourceBlob::allocateAndCopy(bytes, alignof(char)));
+        HeapAsmResourceBlob::allocateAndCopy(bytes, align));
     return DenseResourceElementsAttr::get(type, r);
   } else {
     return DenseElementsAttr::getFromRawBuffer(type, bytes);
--- src/Dialect/Mlir/AttributesHelper.hpp
+++ src/Dialect/Mlir/AttributesHelper.hpp
@@ -15,6 +15,18 @@
 namespace onnx_mlir {
 
 mlir::ElementsAttr makeDenseIntOrFPElementsAttr(
-    mlir::ShapedType type, char *data, size_t size);
+    mlir::ShapedType type, llvm::ArrayRef<char> bytes, size_t align = 1);
 
+template <typename NumericType>
+mlir::ElementsAttr makeDenseIntOrFPElementsAttr(
+    mlir::ShapedType type, llvm::ArrayRef<NumericType> numbers) {
+  constexpr size_t BYTE_BITWIDTH = 8;
+  assert(sizeof(NumericType) ==
+             type.getElementType().getIntOrFloatBitWidth() * BYTE_BITWIDTH &&
+         "static template type must match dynamic argument type");
+  llvm::ArrayRef<char> bytes(reinterpret_cast<const char *>(numbers.data()),
+      numbers.size() * sizeof(NumericType));
+  return makeDenseIntOrFPElementsAttr(type, bytes, alignof(NumericType));
 }
+
+} // namespace onnx_mlir
