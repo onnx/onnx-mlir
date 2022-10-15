fdf20dc13991a252e36b50eee62031fffae3b0da
Soren Lassen <sorenlassen@gmail.com>
2022-10-15 14:03:22 -0700

    remove redundant call
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

--- src/Dialect/Mlir/AttributesHelper.cpp
+++ src/Dialect/Mlir/AttributesHelper.cpp
@@ -25,14 +25,12 @@ ElementsAttr makeDenseIntOrFPElementsAttr(
     ShapedType type, char *data, size_t size) {
   // TODO: test type.getElementType() is int or fp
   ArrayRef<char> bytes = llvm::makeArrayRef(data, size);
-  ResourcePool *resourcePool = ResourcePool::get(type.getContext());
-  if (resourcePool && resourcePool->isActive()) {
+  if (ResourcePool *resourcePool = ResourcePool::get(type.getContext());
+      resourcePool && resourcePool->isActive()) {
     // TODO: consider aligning everything to something large that works well for
     //       everything, e.g. 8 for double and i64, or 16 or 64 for SIMD ops
-    mlir::DenseResourceElementsHandle r =
-        ResourcePool::get(type.getContext())
-            ->createResource(
-                HeapAsmResourceBlob::allocateAndCopy(bytes, alignof(char)));
+    mlir::DenseResourceElementsHandle r = resourcePool->createResource(
+        HeapAsmResourceBlob::allocateAndCopy(bytes, alignof(char)));
     return DenseResourceElementsAttr::get(type, r);
   } else {
     return DenseElementsAttr::getFromRawBuffer(type, bytes);
