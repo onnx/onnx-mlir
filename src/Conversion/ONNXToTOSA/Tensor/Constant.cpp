bf340e14e4cbb71df5ecc65da92f70e9e523266c
Ferdinand Lemaire <ferdinan@xilinx.com>
2022-10-17 14:52:37 +0200

    Cleanup tests and add handling for sparse_value attribute

--- src/Conversion/ONNXToTOSA/Tensor/Constant.cpp
+++ src/Conversion/ONNXToTOSA/Tensor/Constant.cpp
@@ -12,6 +12,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/BuiltinAttributeInterfaces.h"
 #include "mlir/IR/TypeUtilities.h"
 #include "src/Conversion/ONNXToTOSA/ONNXToTOSACommon.hpp"
 
@@ -27,10 +29,16 @@ public:
   using OpAdaptor = typename ONNXConstantOp::Adaptor;
   LogicalResult matchAndRewrite(ONNXConstantOp op, OpAdaptor adaptor,
       ConversionPatternRewriter &rewriter) const override {
-    ::mlir::Attribute valueAttr = *adaptor.value();
+    auto valueAttr = adaptor.value();
+    auto sparseAttr = adaptor.sparse_value();
+    // Only one of the attributes can be present and one must be present.
+    ::mlir::Attribute currentAttr = valueAttr.has_value() ? valueAttr.value() : sparseAttr.value();
     mlir::Type resultType = getTypeConverter()->convertType(op.getResult().getType());
-    rewriter.replaceOpWithNewOp<tosa::ConstOp>(op, resultType, valueAttr);
-    return success();
+    if (currentAttr.isa<ElementsAttr>()) {
+      rewriter.replaceOpWithNewOp<tosa::ConstOp>(op, resultType, currentAttr);
+      return success();
+    }
+    return failure();
   }
 };
 
