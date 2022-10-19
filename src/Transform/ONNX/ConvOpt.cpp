42cb9d0c51b6a0d1a863ba3e24d23df9cb9782d4
Alexandre Eichenberger <alexe@us.ibm.com>
2022-10-19 20:25:01 +0000

    update
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Transform/ONNX/ConvOpt.cpp
+++ src/Transform/ONNX/ConvOpt.cpp
@@ -199,13 +199,13 @@ struct Conv1x1ToMatmulPattern : public ConversionPattern {
     for (int i = 0; i < rank; ++i)
       outputDims.emplace_back(xShape[i]);
     outputDims[1] = Cout;
-    #if 0 // Test if that pattern below works better.
+#if 0 // Test if that pattern below works better.
     // Type outputType = RankedTensorType::get(outputDims, elementType);
     // Reshape results from matrix multiply MM.
     // Value res = create.onnx.reshape(outputType, MM, outputShapeVals);
-    #else
+#else
     Value res = create.onnx.reshape(convOp.Y().getType(), MM, outputShapeVals);
-    #endif
+#endif
     // Replace op and declare success.
     rewriter.replaceOp(convOp, res);
     fprintf(stderr, "  hi alex: success in converting conv to matmul\n");
