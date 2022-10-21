0dad8eff4cf298264091ebd9b539b856ddd70fe0
maxbartel <maximilian.bartel@amd.com>
2022-10-21 15:11:38 +0100

    style: changed filename comment

--- src/Conversion/ONNXToTOSA/Tensor/Reshape.cpp
+++ src/Conversion/ONNXToTOSA/Tensor/Reshape.cpp
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-//===---------------- Elementwise.cpp - Elementwise Op --------------------===//
+//===---------------- Reshape.cpp - Reshape Op --------------------===//
 //
 // Copyright (c) 2022 Advanced Micro Devices, Inc.
 //
