fdbd1f06bc3ff24683e823295b1fe6a0f580bcef
Jiaqing Chen <jchen501@asu.edu>
2022-10-24 08:31:26 -0700

    Add Python interface for compilation and one single interface for compilation and running process (#1690)
    * Add compiler interface to python
    
    Signed-off-by: Jiaqing Chen <jchen501@asu.edu>
    Co-authored-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Compiler/CompilerOptions.cpp
+++ src/Compiler/CompilerOptions.cpp
@@ -418,6 +418,13 @@ void setLLVMOption(const std::string &flag) { mllvm = flag; }
 void clearLLVMOption() { mllvm.clear(); }
 std::string getLLVMOption() { return (mllvm != "") ? mllvm : std::string(); }
 
+// Support for Verbose Option
+void setVerboseOption() { VerboseOutput = true; }
+void clearVerboseOption() { VerboseOutput = false; }
+std::string getVerboseOption() {
+  return VerboseOutput ? std::string("-v") : std::string();
+}
+
 // =============================================================================
 // Methods for OMCompilerOptions
 
@@ -451,6 +458,9 @@ int setCompilerOption(const OptionKind kind, const std::string &val) {
   case OptionKind::LLVMFlag:
     setLLVMOption(val);
     break;
+  case OptionKind::Verbose:
+    setVerboseOption();
+    break;
     // Ignore options that were added but are unknown.
   }
   return CompilerSuccess;
@@ -482,6 +492,9 @@ void clearCompilerOption(const OptionKind kind) {
   case OptionKind::LLVMFlag:
     clearLLVMOption();
     break;
+  case OptionKind::Verbose:
+    clearVerboseOption();
+    break;
     // Ignore options that were added but are unknown.
   }
 }
@@ -512,6 +525,8 @@ std::string getCompilerOption(const OptionKind kind) {
   }
   case OptionKind::LLVMFlag:
     return getLLVMOption();
+  case OptionKind::Verbose:
+    return getVerboseOption();
   }
   return std::string();
 }
