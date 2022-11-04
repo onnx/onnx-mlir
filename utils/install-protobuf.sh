34b13e7571519158caaab8f62989a840a0b0a8a4
Gong Su <gong_su@hotmail.com>
2022-11-04 10:45:51 -0400

    Install protobuf python binding using pip
    Signed-off-by: Gong Su <gong_su@hotmail.com>

--- utils/install-protobuf.sh
+++ utils/install-protobuf.sh
@@ -6,5 +6,5 @@ cd protobuf
 ./autogen.sh
 ./configure --enable-static=no
 make -j$(sysctl -n hw.logicalcpu) install
-cd python
-python3 setup.py install --cpp_implementation
+#cd python
+#python3 setup.py install --cpp_implementation
