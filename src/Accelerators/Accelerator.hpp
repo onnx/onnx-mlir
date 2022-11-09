7c5fedb2ab5ff2954ad1991df41b75540cf05724
Haruki Imai <imaihal@jp.ibm.com>
2022-11-09 01:10:27 +0000

    Remove unnecessary include file to fix an error.
    Signed-off-by: Haruki Imai <imaihal@jp.ibm.com>

--- src/Accelerators/Accelerator.hpp
+++ src/Accelerators/Accelerator.hpp
@@ -22,7 +22,6 @@
 
 #include "include/onnx-mlir/Compiler/OMCompilerTypes.h"
 #include "src/Accelerators/Accelerators.inc"
-#include "src/Accelerators/NNPA/Compiler/NNPACompilerUtils.hpp"
 
 // Define the macros used to generate various accelerators artifacts (via the
 // use of the APPLY_TO_ACCELERATORS macro, which is defined in the cmake
