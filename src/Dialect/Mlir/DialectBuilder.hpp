23fdd80a9fe5eaace8fbefee92f8f477f7348298
Alexandre Eichenberger <alexe@us.ibm.com>
2022-11-29 18:42:51 +0000

    format
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Dialect/Mlir/DialectBuilder.hpp
+++ src/Dialect/Mlir/DialectBuilder.hpp
@@ -10,8 +10,8 @@
 
 #pragma once
 
-#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/IR/Builders.h"
