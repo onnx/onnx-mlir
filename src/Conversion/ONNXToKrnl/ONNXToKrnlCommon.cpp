4ae30a4c89490c748d6982fe33838ace9ce4f61f
Soren Lassen <sorenlassen@gmail.com>
2023-01-05 21:08:31 -0800

    foldOrEmitONNXUnsqueezeV11Op fix
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

--- src/Conversion/ONNXToKrnl/ONNXToKrnlCommon.cpp
+++ src/Conversion/ONNXToKrnl/ONNXToKrnlCommon.cpp
@@ -354,27 +354,11 @@ DenseElementsAttr getDenseElementAttrFromValue(Value input) {
 /// and return a constant.
 Value foldOrEmitONNXSqueezeV11Op(ConversionPatternRewriter &rewriter,
     Location loc, Type resultType, Value input, int64_t axis) {
-<<<<<<< HEAD
   MultiDialectBuilder<OnnxBuilder> create(rewriter, loc);
   TensorType tensorType = create.onnx.toTensor(resultType);
   if (DenseElementsAttr inputElements = getDenseElementAttrFromValue(input)) {
     DenseElementsAttr squeezedElements = inputElements.reshape(tensorType);
     Value constVal = create.onnx.constant(squeezedElements);
-=======
-  MultiDialectBuilder<KrnlBuilder, MathBuilder, MemRefBuilder, OnnxBuilder>
-      create(rewriter, loc);
-  if (krnl::isKrnlGlobalConstant(input) || isDenseONNXConstant(input)) {
-    DenseElementsAttr inputElements =
-        getElementAttributeFromONNXValue(input).cast<DenseElementsAttr>();
-    ShapedType tensorType = create.onnx.toTensor(resultType).cast<ShapedType>();
-    DenseElementsAttr squeezedElements = inputElements.reshape(tensorType);
-    // TODO: Fix lit tests so we can change this to just:
-    // Value constVal = create.onnx.constant(squeezedElements);
-    // The lit tests want the constant to have memref type, so we do this:
-    Value constVal = rewriter.create<ONNXConstantOp>(loc, resultType,
-        Attribute(), squeezedElements, FloatAttr(), ArrayAttr(), IntegerAttr(),
-        ArrayAttr(), StringAttr(), ArrayAttr());
->>>>>>> f4929223 (use DenseElementsAttr::reshape to implement foldOrEmitONNXSqueezeV11Op, foldOrEmitONNXUnsqueezeV11Op)
     return create.onnx.toMemref(constVal);
   } else {
     return create.onnx.toMemref(
@@ -455,10 +439,6 @@ Value foldOrEmitONNXTransposeOp(ConversionPatternRewriter &rewriter,
     for (auto permVal : permAttr.getValue())
       perm.emplace_back(permVal.cast<IntegerAttr>().getInt());
 
-<<<<<<< HEAD
-=======
-    ElementsAttr inputElements = getElementAttributeFromONNXValue(input);
->>>>>>> c99a9543 (use simpler getElementAttributeFromONNXValue instead of getONNXConstantOp)
     ElementsAttrBuilder elementsBuilder(rewriter.getContext());
     ElementsAttr transposedElements =
         elementsBuilder.transpose(inputElements, perm);
