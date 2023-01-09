fe9ebe74008f24613a29fae531928715081ccd6d
Soren Lassen <sorenlassen@gmail.com>
2023-01-09 01:26:32 -0800

    implemented HeapReporter, added onnx-mlir-report-heap.sh script
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

--- src/Compiler/HeapReporter.hpp
+++ src/Compiler/HeapReporter.hpp
@@ -16,16 +16,21 @@
 namespace onnx_mlir {
 
 struct HeapReporter : public mlir::PassInstrumentation {
-  HeapReporter(std::string logFilename, std::string beforePasses, std::string afterPasses);
+  HeapReporter(std::string logFilename, llvm::StringRef beforePasses,
+      llvm::StringRef afterPasses);
   ~HeapReporter() override;
 
   void runBeforePass(mlir::Pass *pass, mlir::Operation *op) override;
   void runAfterPass(mlir::Pass *pass, mlir::Operation *op) override;
 
 private:
+  void reportBegin(const std::string &heading);
+  void reportHeap(const std::string &heading);
+
   std::string logFilename;
-  llvm::StringSet beforePasses;
-  llvm::StringSet afterPasses;
+  llvm::StringSet<> beforePassesSet;
+  llvm::StringSet<> afterPassesSet;
+  std::string command;
 };
 
 } // namespace onnx_mlir
