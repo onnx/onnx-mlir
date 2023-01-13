393b10cad2cf1ddf1e3856b6ca664b468cf8348b
Megan Hampton <hamptonm@us.ibm.com>
2023-01-13 14:40:14 -0500

    Update inference backend test

--- test/backend/inference_backend.py
+++ test/backend/inference_backend.py
@@ -430,7 +430,7 @@ def get_test_models():
         # IsInf
 
         # ==OP== IsNan
-        #"test_isnan_cpu": {STATIC_SHAPE:{}, DYNAMIC_SHAPE:{-1:{-1}}, CONSTANT_INPUT:{-1}},
+        "test_isnan_cpu": {STATIC_SHAPE:{}, DYNAMIC_SHAPE:{-1:{-1}}, CONSTANT_INPUT:{-1}},
 
         # ==OP== LeakyRelu
         "test_leakyrelu_cpu": {STATIC_SHAPE:{}, DYNAMIC_SHAPE:{-1:{-1}}, CONSTANT_INPUT:{-1}},
