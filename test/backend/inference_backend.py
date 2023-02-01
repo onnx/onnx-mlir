08e4a551375b9869446a403b447a21336da5987a
Megan Hampton <megan.hampton1@marist.edu>
2023-02-01 12:17:44 -0500

    Update SupportedONNXOps-cpu document

--- test/backend/inference_backend.py
+++ test/backend/inference_backend.py
@@ -432,7 +432,7 @@ def get_test_models():
         # "test_isinf_negative_cpu": {STATIC_SHAPE:{}, DYNAMIC_SHAPE:{-1:{-1}}, CONSTANT_INPUT:{-1}},
         # "test_isinf_positive_cpu": {STATIC_SHAPE:{}, DYNAMIC_SHAPE:{-1:{-1}}, CONSTANT_INPUT:{-1}},
 
-        # ==OP== IsNan
+        # ==OP== IsNaN
         "test_isnan_cpu": {STATIC_SHAPE:{}, DYNAMIC_SHAPE:{-1:{-1}}, CONSTANT_INPUT:{-1}},
 
         # ==OP== LeakyRelu
