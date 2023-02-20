04c9d6941a84af0b9e621665d8db59156a3707f4
Alexandre Eichenberger <alexe@us.ibm.com>
2023-02-20 17:54:04 +0000

    removed initial simd
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Conversion/ONNXToKrnl/Math/Elementwise.cpp
+++ src/Conversion/ONNXToKrnl/Math/Elementwise.cpp
@@ -749,73 +749,6 @@ struct ONNXElementwiseUnaryOpLowering : public ConversionPattern {
 
     // Only create krnl.iterate if one of the operands is not scalar tensor.
     if (!hasAllScalarValues(operands)) {
-
-      bool processedAsSIMD = false;
-      if (false && isa<mlir::ONNXCosOp>(op)) {
-        // hi alex
-        int64_t mVL = create.vec.getMachineVectorLength(elementType);
-        IndexExpr totSize = LiteralIndexExpr(1);
-        int64_t totConstSize = 1;
-        int64_t rank = memRefType.getRank();
-        for (int64_t i = 0; i < rank; ++i) {
-          IndexExpr currDim = shapeHelper.getOutputDims()[i];
-          totSize = totSize * currDim;
-          if (currDim.isLiteral())
-            totConstSize *= currDim.getLiteral();
-        }
-        bool isSIMD = true;
-        if (totConstSize % mVL != 0) {
-          fprintf(stderr, "hi alex: SIMD disabled as size %d mod %d != 0\n",
-              (int)totConstSize, (int)mVL);
-          isSIMD = false;
-        } else if (!memRefType.getLayout().isIdentity()) {
-          fprintf(stderr,
-              "hi alex: SIMD disabled as output memref layout is not "
-              "identity\n");
-          isSIMD = false;
-        } else {
-          assert(X.getType().isa<MemRefType>() && "expected memrefs");
-          if (!X.getType().cast<MemRefType>().getLayout().isIdentity()) {
-            fprintf(stderr,
-                "hi alex: SIMD disabled as output memref layout is not "
-                "identity\n");
-            isSIMD = false;
-          }
-        }
-        if (isSIMD) {
-          fprintf(stderr, "hi alex: SIMD possible\n");
-          // Create loop iteration (flattened to one dim) and blocked by mVL.
-          ValueRange loopDef = create.krnl.defineLoops(1);
-          ValueRange blockedLoopDef = create.krnl.block(loopDef[0], mVL);
-          SmallVector<IndexExpr, 1> lbs(1, LiteralIndexExpr(0));
-          SmallVector<IndexExpr, 1> ubs(1, totSize);
-          // Flatten the input and output: place all dims in one.
-          ReassociationIndices allInOne;
-          for (int64_t i = 0; i < rank; ++i)
-            allInOne.emplace_back(i);
-          SmallVector<ReassociationIndices> reassociation(1, allInOne);
-          Value flatX = create.mem.collapseShape(X, reassociation);
-          Value flatAlloc = create.mem.collapseShape(alloc, reassociation);
-          // Create the vector type to operate over.
-          VectorType vecElementType = VectorType::get({mVL}, elementType);
-          // Iterate only over the blocks.
-          create.krnl.iterateIE(loopDef, {blockedLoopDef[0]}, lbs, ubs,
-              [&](KrnlBuilder &ck, ValueRange loopInd) {
-                MultiDialectBuilder<KrnlBuilder, VectorBuilder> create(ck);
-                Value loadedVal =
-                    create.vec.load(vecElementType, flatX, loopInd);
-                auto loweredOpResult = emitScalarOpFor<ElementwiseUnaryOp>(
-                    rewriter, loc, op, vecElementType, {loadedVal});
-                // Store result in the resulting array.
-                create.vec.store(loweredOpResult, flatAlloc, loopInd);
-              });
-          processedAsSIMD = true;
-          rewriter.replaceOp(op, alloc);
-          return success();
-        }
-      }
-
-      if (!processedAsSIMD) {
         ValueRange loopDef = create.krnl.defineLoops(memRefType.getRank());
         SmallVector<IndexExpr, 4> lbs(
             memRefType.getRank(), LiteralIndexExpr(0));
@@ -829,7 +762,6 @@ struct ONNXElementwiseUnaryOpLowering : public ConversionPattern {
               // Store result in the resulting array.
               createKrnl.store(loweredOpResult, alloc, loopInd);
             });
-      }
     } else {
       Value loadedVal = create.krnl.load(X);
       auto loweredOpResult = emitScalarOpFor<ElementwiseUnaryOp>(
