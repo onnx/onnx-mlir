35fc21e77adc094151ff8e280e68c150a29fca94
Mike Essenmacher <essen@us.ibm.com>
2023-02-26 16:06:11 -0500

    Update LLVM to 5e111eb275eee3bec1123b4b85606328017e5ee5 and MHLO to a99159c45ee5c497f8dce01eff807a6d57629b61
    Signed-off-by: Mike Essenmacher <essen@us.ibm.com>

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
 git clone -n https://github.com/llvm/llvm-project.git
 # Check out a specific branch that is known to work with ONNX-MLIR.
-cd llvm-project && git checkout 114ce273d86a091f61446c8778dbf102942c96cf && cd ..
+cd llvm-project && git checkout 5e111eb275eee3bec1123b4b85606328017e5ee5 && cd ..
