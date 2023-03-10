2eac6fb1a0586897a8207f2eed87df1f453635f1
Doxygen Bot (GitHub Action) <onnxmlir@onnx.ai>
2023-03-10 12:08:50 +0000

    Push docs and doxygen updates into gh-pages branch

--- doxygen_html/OMTensorList_h/search/all_0.js
+++ doxygen_html/OMTensorList_h/search/all_0.js
@@ -3,8 +3,8 @@ var searchData=
   ['omtensorlist_0',['OMTensorList',['../_o_m_tensor_list_8h.html#a87970f36c1838a5c43f43ec7dd0aec3a',1,'OMTensorList.h']]],
   ['omtensorlist_2eh_1',['OMTensorList.h',['../_o_m_tensor_list_8h.html',1,'']]],
   ['omtensorlistcreate_2',['omTensorListCreate',['../_o_m_tensor_list_8h.html#aaa64f4eb669ba3d3fa6d8c6d3b947566',1,'OMTensorList.h']]],
-  ['omtensorlistcreatewithownership_3',['omTensorListCreateWithOwnership',['../_o_m_tensor_list_8h.html#abdbe902b539b09d65d1acfad0b97fda3',1,'OMTensorList.h']]],
-  ['omtensorlistdestroy_4',['omTensorListDestroy',['../_o_m_tensor_list_8h.html#a365180f0b342a2297c2ebe9b8d8f65c5',1,'OMTensorList.h']]],
+  ['omtensorlistdestroy_3',['omTensorListDestroy',['../_o_m_tensor_list_8h.html#a365180f0b342a2297c2ebe9b8d8f65c5',1,'OMTensorList.h']]],
+  ['omtensorlistdestroyshallow_4',['omTensorListDestroyShallow',['../_o_m_tensor_list_8h.html#af373888f6dc2295088d1727205f8b6a2',1,'OMTensorList.h']]],
   ['omtensorlistgetomtarray_5',['omTensorListGetOmtArray',['../_o_m_tensor_list_8h.html#a573a21a52a0b7ff91355d6b046e7bc6c',1,'OMTensorList.h']]],
   ['omtensorlistgetomtbyindex_6',['omTensorListGetOmtByIndex',['../_o_m_tensor_list_8h.html#a64480f00a2e07ddb0349d13628d76656',1,'OMTensorList.h']]],
   ['omtensorlistgetsize_7',['omTensorListGetSize',['../_o_m_tensor_list_8h.html#a38fbd71490b772ab9c7a42af3d0c9981',1,'OMTensorList.h']]]
--- doxygen_html/OMTensorList_h/search/functions_0.js
+++ doxygen_html/OMTensorList_h/search/functions_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['omtensorlistcreate_9',['omTensorListCreate',['../_o_m_tensor_list_8h.html#aaa64f4eb669ba3d3fa6d8c6d3b947566',1,'OMTensorList.h']]],
-  ['omtensorlistcreatewithownership_10',['omTensorListCreateWithOwnership',['../_o_m_tensor_list_8h.html#abdbe902b539b09d65d1acfad0b97fda3',1,'OMTensorList.h']]],
-  ['omtensorlistdestroy_11',['omTensorListDestroy',['../_o_m_tensor_list_8h.html#a365180f0b342a2297c2ebe9b8d8f65c5',1,'OMTensorList.h']]],
+  ['omtensorlistdestroy_10',['omTensorListDestroy',['../_o_m_tensor_list_8h.html#a365180f0b342a2297c2ebe9b8d8f65c5',1,'OMTensorList.h']]],
+  ['omtensorlistdestroyshallow_11',['omTensorListDestroyShallow',['../_o_m_tensor_list_8h.html#af373888f6dc2295088d1727205f8b6a2',1,'OMTensorList.h']]],
   ['omtensorlistgetomtarray_12',['omTensorListGetOmtArray',['../_o_m_tensor_list_8h.html#a573a21a52a0b7ff91355d6b046e7bc6c',1,'OMTensorList.h']]],
   ['omtensorlistgetomtbyindex_13',['omTensorListGetOmtByIndex',['../_o_m_tensor_list_8h.html#a64480f00a2e07ddb0349d13628d76656',1,'OMTensorList.h']]],
   ['omtensorlistgetsize_14',['omTensorListGetSize',['../_o_m_tensor_list_8h.html#a38fbd71490b772ab9c7a42af3d0c9981',1,'OMTensorList.h']]]
