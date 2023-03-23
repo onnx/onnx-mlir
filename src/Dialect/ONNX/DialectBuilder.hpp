93f08e845493934bd54e85771eb454db38387c15
philass <plassen@groq.com>
2023-03-22 23:41:24 -0700

    Clang format fixes
    Signed-off-by: philass <plassen@groq.com>

--- src/Dialect/ONNX/DialectBuilder.hpp
+++ src/Dialect/ONNX/DialectBuilder.hpp
@@ -34,11 +34,11 @@ struct OnnxBuilder : DialectBuilder {
 
   // Create operation and infer shape.
   template <typename OnnxOpType, typename... Args>
-  OnnxOpType createOpAndInferShapes(Args &&... args) const;
+  OnnxOpType createOpAndInferShapes(Args &&...args) const;
 
   template <typename OnnxOpType, typename... Args>
   OnnxOpType createTypedOpAndInferShapes(
-      mlir::Type result_ty, Args &&... args) const;
+      mlir::Type result_ty, Args &&...args) const;
 
   // ONNXAddOp
   mlir::Value add(mlir::Value A, mlir::Value B) const;
