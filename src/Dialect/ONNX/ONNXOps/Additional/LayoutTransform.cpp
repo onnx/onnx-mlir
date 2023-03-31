8e20096e0ffdbeb44adf5b9ea61c7a34e1842eaa
Alexandre Eichenberger <alexe@us.ibm.com>
2023-03-31 13:13:37 -0400

    Elementwise simd: added high level machine description of simd support in x86 (SSE 4.2, AVX2) and Z16 (#2127)
    * Added support to analyze a binary/asm file for the presence of SIMD instructions
    * Added cost model for deciding if/how to simonize
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps/Additional/LayoutTransform.cpp
+++ src/Dialect/ONNX/ONNXOps/Additional/LayoutTransform.cpp
@@ -38,8 +38,10 @@ LogicalResult ONNXLayoutTransformOp::inferShapes(
 // Verifier
 //===----------------------------------------------------------------------===//
 LogicalResult ONNXLayoutTransformOp::verify() {
-  if (auto dataType = getData().getType().dyn_cast<RankedTensorType>())
-    if (auto outputType = getOutput().getType().dyn_cast<RankedTensorType>())
+  if (auto dataType = getData().getType().dyn_cast<RankedTensorType>()) {
+    RankedTensorType outputType =
+        getOutput().getType().dyn_cast<RankedTensorType>();
+    if (outputType)
       for (int64_t i = 0; i < dataType.getRank(); ++i)
         // Check if there is an unknown dimension in the dataShape and
         // outputShape. If there is an unknown dimension, we will return true.
@@ -51,6 +53,7 @@ LogicalResult ONNXLayoutTransformOp::verify() {
         else if (dataType.getShape()[i] != outputType.getShape()[i])
           return emitOpError(
               "Input and output tensors must have the same shape");
+  }
   return success();
 }
 
