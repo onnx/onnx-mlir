51bbe3b5a19bd0c51d0ae52998b967fc6ebed8cc
Ferdinand Lemaire <ferdinan@xilinx.com>
2023-04-25 09:22:44 +0100

    Add constant of the size of the input in order for the add/sub to be valid

--- src/Conversion/ONNXToTOSA/NN/DequantizeLinear.cpp
+++ src/Conversion/ONNXToTOSA/NN/DequantizeLinear.cpp
@@ -36,6 +36,7 @@ public:
       ConversionPatternRewriter &rewriter) const override {
     Location loc = op->getLoc();
     Value x = op.x();
+    ArrayRef<int64_t> inputShape =  cast<TensorType>(x.getType()).getShape();
     Value x_scale = op.x_scale();
     Value x_zero_point = op.x_zero_point();
     Type resultType = op.getResult().getType();
@@ -48,10 +49,33 @@ public:
           op, "Only per-tensor quantization is handled.");
     }
 
+    // Since tosa.sub doesn't allow different shapes, get the value from the zero point
+    // constant, and create a constant of the same shape as the input out of it in order
+    // to have a correct sub.
+    mlir::ElementsAttr zeroPoint;
+    if (auto source = x_zero_point.getDefiningOp<ONNXConstantOp>()) {
+      zeroPoint = source.value().value(); 
+    }
+    else if (x_zero_point.getDefiningOp<mlir::tosa::ConstOp>()) {
+      zeroPoint = tosa::getValueFromTosaConst<ElementsAttr>(x_zero_point);
+    }
+    auto zpValue = zeroPoint.getValues<APInt>()[0];
+    
+    uint64_t numElements = 1;
+    for (int64_t a : inputShape) {
+      numElements *= a;
+    }
+    std::vector zpVec = std::vector<APInt>(numElements, zpValue);
+    auto constType =
+        RankedTensorType::get(inputShape, rewriter.getIntegerType(sizeof(int8_t) * 8));
+    auto constAttr = DenseElementsAttr::get(constType, zpVec);
+    auto zpConst = tosa::CreateOpAndInfer<mlir::tosa::ConstOp>(rewriter, loc, constType, constAttr);
+    
+    // Quantization formula is ((x / y_scale) + y_zero_point)
     // Dequantization formula is (x - zero_point) * scale
     // Cast into the destination type first
     Value castOp = tosa::CreateOpAndInfer<mlir::tosa::CastOp>(rewriter, loc, resultType, x).getResult();
-    Value subOp = tosa::CreateOpAndInfer<mlir::tosa::SubOp>(rewriter, loc, resultType, castOp, x_zero_point).getResult();
+    Value subOp = tosa::CreateOpAndInfer<mlir::tosa::SubOp>(rewriter, loc, resultType, castOp, zpConst).getResult();
     Value mulOp = tosa::CreateOpAndInfer<mlir::tosa::MulOp>(rewriter, loc, resultType, subOp, x_scale, 0).getResult();
 
     rewriter.replaceOp(op, mulOp);
--- src/Conversion/ONNXToTOSA/NN/QuantizeLinear.cpp
+++ src/Conversion/ONNXToTOSA/NN/QuantizeLinear.cpp
@@ -14,6 +14,8 @@
 
 #include "mlir/Dialect/Tosa/IR/TosaOps.h"
 #include "mlir/IR/Attributes.h"
+#include "mlir/IR/BuiltinAttributeInterfaces.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "src/Conversion/ONNXToTOSA/DialectBuilder.hpp"
 #include "src/Conversion/ONNXToTOSA/ONNXToTOSACommon.hpp"
@@ -38,6 +40,7 @@ public:
     Location loc = op->getLoc();
     Value x = op.x();
     Type xType = x.getType();
+    ArrayRef<int64_t> inputShape =  cast<TensorType>(xType).getShape();
     Value y_scale = op.y_scale();
     Value y_zero_point = op.y_zero_point();
     // Axis attribute is ignored for per-tensor quantization, which is the only one handled
@@ -48,12 +51,34 @@ public:
       return rewriter.notifyMatchFailure(
           op, "Only per-tensor quantization is handled.");
     }
-
+    
+    // Since tosa.add doesn't allow different shapes, get the value from the zero point
+    // constant, and create a constant of the same shape as the input out of it in order
+    // to have a correct add.
+    mlir::ElementsAttr zeroPoint;
+    if (auto source = y_zero_point.getDefiningOp<ONNXConstantOp>()) {
+      zeroPoint = source.value().value(); 
+    }
+    else if (y_zero_point.getDefiningOp<mlir::tosa::ConstOp>()) {
+      zeroPoint = tosa::getValueFromTosaConst<ElementsAttr>(y_zero_point);
+    }
+    auto zpValue = zeroPoint.getValues<APInt>()[0];
+    
+    uint64_t numElements = 1;
+    for (int64_t a : inputShape) {
+      numElements *= a;
+    }
+    std::vector zpVec = std::vector<APInt>(numElements, zpValue);
+    auto constType =
+        RankedTensorType::get(inputShape, rewriter.getIntegerType(sizeof(int8_t) * 8));
+    auto constAttr = DenseElementsAttr::get(constType, zpVec);
+    auto zpConst = tosa::CreateOpAndInfer<mlir::tosa::ConstOp>(rewriter, loc, constType, constAttr);
+    
     // Quantization formula is ((x / y_scale) + y_zero_point)
     // Replace the division by a reciprocal followed by a mul
     Value recOp = tosa::CreateOpAndInfer<mlir::tosa::ReciprocalOp>(rewriter, loc, xType, x).getResult();
     Value mulOp = tosa::CreateOpAndInfer<mlir::tosa::MulOp>(rewriter, loc, xType, recOp, y_scale, 0).getResult();
-    Value addOp = tosa::CreateOpAndInfer<mlir::tosa::AddOp>(rewriter, loc, xType, mulOp, y_zero_point).getResult();
+    Value addOp = tosa::CreateOpAndInfer<mlir::tosa::AddOp>(rewriter, loc, xType, mulOp, zpConst).getResult();
     // Cast into the result type
     Value castOp = tosa::CreateOpAndInfer<mlir::tosa::CastOp>(rewriter, loc, op.getResult().getType(), addOp).getResult();
 
