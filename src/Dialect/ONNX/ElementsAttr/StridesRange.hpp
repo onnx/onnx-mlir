b418eed16bb812b6724ad0cd4c56d85528bb5094
Soren Lassen <sorenlassen@gmail.com>
2023-05-07 07:45:06 -0700

    fix StridesIterator offsets to properly support negative values
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

--- src/Dialect/ONNX/ElementsAttr/StridesRange.hpp
+++ src/Dialect/ONNX/ElementsAttr/StridesRange.hpp
@@ -21,22 +21,24 @@ namespace onnx_mlir {
 
 // Used as value_type in StridesRange and StridesIterator in the context of a
 // shape of the given rank (the length of the 'index' vector) and N strides
-// which are not represented in StridesIndexPos itself.
+// which are not represented in StridesIndexOffsets itself.
 //
 // 'flattenedIndex' and 'index' are two representations of an index into a
 // tensor of the given rank.
-// 'pos' is an array of flattened indexes into strided arrays.
+// 'offsets' is an array of flattened offsets into strided arrays. Can be
+// negative if strides are negative, e.g. to represent negative slice steps.
 template <size_t N>
-struct StridesIndexPos {
+struct StridesIndexOffsets {
   // Non-zero flattenedIndex is only used to construct end iterators
-  // with meaningless index and pos.
-  StridesIndexPos(unsigned rank, uint64_t flattenedIndex = 0)
-      : flattenedIndex(flattenedIndex), index(rank, 0), pos{} {}
+  // with meaningless index and offsets.
+  StridesIndexOffsets(unsigned rank, uint64_t flattenedIndex = 0)
+      : flattenedIndex(flattenedIndex), index(rank, 0), offsets{} {}
   uint64_t flattenedIndex;
   llvm::SmallVector<uint64_t, 6> index;
-  std::array<size_t, N> pos;
-  // idxpos[i] is convenient shorthand for idxpos.pos[i]
-  size_t operator[](int i) const { return pos[i]; }
+  std::array<int64_t, N> offsets;
+  // idxoffs[i] is convenient shorthand for idxoffs.offsets[i]
+  int64_t operator[](int i) const { return offsets[i]; }
+  int64_t at(int i) const { return offsets[i]; }
 };
 
 // Suppose (array0,strides0),...,(arrayN,stridesN) are all strided tensors with
@@ -44,9 +46,9 @@ struct StridesIndexPos {
 // it = StridesIterator<N+1>(shape, {strides0,...,stridesN}) iterates over the
 // shape's elements in row-major order, with it->index describing the shape's
 // corresponding multidimensional index, it->flattenedIndex the row-major order
-// flattened linear index, and it->pos is an array of positions pos0,...,posN
-// in array0,...,arrayN that represent the index. i.e.,
-// it->pos[0] == getStridesPosition(it->index, strides0) etc.
+// flattened linear index, and it->offsets is an array of flat linear indexes
+// offset0,...,offsetN in array0,...,arrayN that represent the index, i.e.,
+// it->at(0) == it->offsets[0] == getStridesPosition(it->index, strides0) etc.
 //
 // For example, this can be used to compare the contents of two strided tensors
 // (a0,strides0) and (a1,strides1) with the same shape:
@@ -55,7 +57,7 @@ struct StridesIndexPos {
 //   bool equal(S shape, A a0, S strides0, A a1, S strides1) {
 //     StridesIterator<2> begin(shape, {strides0, strides1}), end(shape);
 //     return std::all_of(begin, end,
-//       [](const auto &idxpos) { return a0[idxpos[0]] == a1[idxpos[1]]; });
+//       [](const auto &idxoffs) { return a0[idxoffs[0]] == a1[idxoffs[1]]; });
 //   }
 //
 // Note: The iteration example above works when shape is empty because
@@ -67,7 +69,7 @@ struct StridesIndexPos {
 template <size_t N>
 class StridesIterator {
 public:
-  using value_type = StridesIndexPos<N>;
+  using value_type = StridesIndexOffsets<N>;
   using difference_type = int64_t;
   using pointer = const value_type *;
   using reference = const value_type &;
@@ -131,13 +133,13 @@ public:
 //   template <typename T, typename A = ArrayRef<T>, S = ArrayRef<int64_t>>
 //   bool equal(S shape, A a0, S strides0, A a1, S strides1) {
 //     return llvm::all_of(StridesRange<2>(shape, {strides0, strides1}),
-//         [](const auto &idxpos) { return a0[idxpos[0]] == a1[idxpos[1]]; });
+//       [](const auto &idxoffs) { return a0[idxoffs[0]] == a1[idxoffs[1]]; });
 //   }
 template <size_t N>
 class StridesRange {
 public:
   using iterator = StridesIterator<N>;
-  using value_type = StridesIndexPos<N>;
+  using value_type = StridesIndexOffsets<N>;
 
 private:
   const llvm::ArrayRef<int64_t> shape;
