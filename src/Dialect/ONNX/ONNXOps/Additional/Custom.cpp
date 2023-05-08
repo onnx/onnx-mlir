489045f8e7c2248cdb2fe2cfcd788d7715f87e52
chentong319 <chentong@us.ibm.com>
2023-05-08 13:19:00 -0400

    SameAs
    Signed-off-by: chentong319 <chentong@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps/Additional/Custom.cpp
+++ src/Dialect/ONNX/ONNXOps/Additional/Custom.cpp
@@ -28,6 +28,28 @@ using namespace onnx_mlir;
 
 LogicalResult ONNXCustomOp::inferShapes(
     std::function<void(Region &)> doShapeInference) {
+  if (!hasShapeAndRank(getOperation()))
+    return success();
+
+  std::optional<ArrayAttr> inputIndexAttrs = getInputsForInfer();
+  // Use the first input because their element type should be same
+  //int64_t inputIdx = (inputIndexAttrs.getValue()[0]).cast<IntegerAttr>().getInt();
+  int64_t inputIdx = 0;
+  Type elementType = getOutputElementType().value_or(getElementType(getInputs()[inputIdx].getType()));
+
+  ValueRange operands = getInputs(); // All the inputs, not other parameter
+  if (inputIndexAttrs.has_value()) {
+    SmallVector<Value, 4> specifiedInputs;
+    for(auto indexAttr : inputIndexAttrs.value()) {
+      specifiedInputs.emplace_back(getInputs()[indexAttr.cast<IntegerAttr>().getInt()]);
+    }
+    operands = specifiedInputs;
+  }
+  if (getShapeInferPattern() == "SameAs") {
+    ONNXUnaryOpShapeHelper shapeHelper(getOperation(), operands);
+    return shapeHelper.computeShapeAndUpdateType(elementType);
+  }
+    
   // TODO: This does not appear to be implemented, set to return an error.
   // getResult().setType(getOperand().getType());
   // return success();
