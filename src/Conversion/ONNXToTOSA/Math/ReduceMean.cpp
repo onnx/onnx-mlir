9b73e9519e41037d8b61b1819f282b3aec3e23d1
Ferdinand Lemaire <ferdinan@xilinx.com>
2023-05-09 16:53:05 +0100

    Add lowering of ReduceMeanV13

--- src/Conversion/ONNXToTOSA/Math/ReduceMean.cpp
+++ src/Conversion/ONNXToTOSA/Math/ReduceMean.cpp
@@ -104,12 +104,58 @@ public:
   }
 };
 
+class ONNXReduceMeanV13LoweringToTOSA
+    : public OpConversionPattern<ONNXReduceMeanV13Op> {
+public:
+  using OpConversionPattern::OpConversionPattern;
+  using OpAdaptor = typename ONNXReduceMeanV13Op::Adaptor;
+  LogicalResult matchAndRewrite(ONNXReduceMeanV13Op op, OpAdaptor adaptor,
+      ConversionPatternRewriter &rewriter) const override {
+
+    auto loc = op->getLoc();
+    TosaBuilder tosaBuilder(rewriter, loc);
+    Value input = adaptor.getData();
+    auto axes = adaptor.getAxes();
+    auto keepDims = adaptor.getKeepdims();
+
+    auto resultType = getTypeConverter()
+                          ->convertType(op.getResult().getType())
+                          .cast<RankedTensorType>();
+
+    // axes is mandatory for tosa
+    if (!axes) {
+      // if not present all axes are reduced
+      const int64_t numberOfAxes = input.getType().cast<ShapedType>().getRank();
+      std::vector<int64_t> allDims(numberOfAxes);
+      std::iota(std::begin(allDims), std::end(allDims), 0);
+      axes = rewriter.getI64ArrayAttr(allDims);
+    }
+    // Tosa needs a DenseElementsAttr
+    auto vecValues = extractFromI64ArrayAttr(axes.value());
+    const int64_t vecValuesSize = vecValues.size();
+    DenseElementsAttr newAxesAttr = DenseIntElementsAttr::get(
+        RankedTensorType::get({vecValuesSize}, rewriter.getI64Type()),
+        vecValues);
+
+    auto output = tosa::convertReduceMeanOp(
+        rewriter, op, tosaBuilder, resultType, input, newAxesAttr, keepDims);
+
+    if (!output) {
+      return rewriter.notifyMatchFailure(op, "Could not be converted");
+    }
+    // Shape inference is handled by the helper functions
+    rewriter.replaceOp(op, {output.value()});
+    return success();
+  }
+};
+
 } // namespace
 
 void populateLoweringONNXReduceMeanOpToTOSAPattern(ConversionTarget &target,
     RewritePatternSet &patterns, TypeConverter &typeConverter,
     MLIRContext *ctx) {
   patterns.insert<ONNXReduceMeanLoweringToTOSA>(typeConverter, ctx);
+  patterns.insert<ONNXReduceMeanV13LoweringToTOSA>(typeConverter, ctx);
 }
 
 } // namespace onnx_mlir
