3ede01623f1b33e43ae8cb907cadca30a202362f
chentong319 <chentong@us.ibm.com>
2023-05-10 14:47:45 -0400

    small fix
    Signed-off-by: chentong319 <chentong@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps/Additional/Custom.cpp
+++ src/Dialect/ONNX/ONNXOps/Additional/Custom.cpp
@@ -24,13 +24,18 @@ using namespace onnx_mlir;
 
 LogicalResult ONNXCustomOp::inferShapes(
     std::function<void(Region &)> doShapeInference) {
+  // ToDo: this check could be refined to the shape related input,
+  // if inputs_for_infer is specified
   if (!hasShapeAndRank(getOperation()))
     return success();
 
+  if (!getShapeInferPattern().has_value()) {
+    // No shape inference pattern provided. Do not know how to infer shape
+    return success();
+  }
+
   std::optional<ArrayAttr> inputIndexAttrs = getInputsForInfer();
   // Use the first input because their element type should be same
-  // int64_t inputIdx =
-  // (inputIndexAttrs.getValue()[0]).cast<IntegerAttr>().getInt();
   int64_t inputIdx = 0;
   if (inputIndexAttrs.has_value())
     (inputIndexAttrs->getValue()[0]).cast<IntegerAttr>().getInt();
@@ -47,10 +52,9 @@ LogicalResult ONNXCustomOp::inferShapes(
     }
     operands = specifiedInputs;
   }
-  if (!getShapeInferPattern().has_value()) {
-    // No shape inference pattern provided. Simply return.
-    return success();
-  } else if (getShapeInferPattern() == "SameAs") {
+
+  // getShapeInferPattern should have value due to previous check
+  if (getShapeInferPattern() == "SameAs") {
     ONNXUnaryOpShapeHelper shapeHelper(getOperation(), operands);
     return shapeHelper.computeShapeAndUpdateType(elementType);
   } else if (getShapeInferPattern() == "MDBroadcast") {
