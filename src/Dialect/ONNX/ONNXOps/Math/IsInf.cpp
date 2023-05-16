6cd490a194110ca4011634d6d19f2e1342e58e31
Megan Hampton <megan.hampton1@marist.edu>
2023-05-16 10:51:17 -0400

    Fix formatting

--- src/Dialect/ONNX/ONNXOps/Math/IsInf.cpp
+++ src/Dialect/ONNX/ONNXOps/Math/IsInf.cpp
@@ -31,4 +31,3 @@ LogicalResult ONNXIsInfOp::inferShapes(
   IntegerType i1Type = IntegerType::get(getContext(), 1, IntegerType::Signless);
   return inferShapeForUnaryOps(getOperation(), i1Type);
 }
-
