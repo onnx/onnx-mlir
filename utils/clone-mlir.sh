d846a2b6a2007d850de452781f60dce046675dda
hamptonm1 <79232909+hamptonm1@users.noreply.github.com>
2023-05-25 09:22:24 -0400

    Update clone-mlir.sh

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
 git clone -n https://github.com/llvm/llvm-project.git
 # Check out a specific branch that is known to work with ONNX-MLIR.
-cd llvm-project && git checkout 095e6ac9fd92d03dcb1e19b60cb06a8140aae69 && cd ..
+cd llvm-project && git checkout 095e6ac9fd92d03dcb1e19b60cb06a8140aae69d && cd ..
