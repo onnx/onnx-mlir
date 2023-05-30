b5a9324d38e41711be414a2ec62d4c1d481c8b34
Soren Lassen <sorenlassen@gmail.com>
2023-05-29 21:37:09 -0700

    call updateRootInPlace because op is updated in place
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

--- src/Dialect/ONNX/Rewrite.cpp
+++ src/Dialect/ONNX/Rewrite.cpp
@@ -222,20 +222,22 @@ public:
              << rhsType;
     }
 
-    OnnxBuilder createONNX(rewriter, op->getLoc());
-    SmallVector<int64_t> axesArray;
-    SmallVector<int64_t> unsqueezedShape(rhsType.getShape());
-    for (int64_t axis = rhsRank; axis < lhsRank - axis + 1; ++axis) {
-      axesArray.push_back(axis);
-      unsqueezedShape.push_back(1);
-    }
-    Value axes = createONNX.constantInt64(axesArray);
-    auto unsqueezedType =
-        RankedTensorType::get(unsqueezedShape, rhsType.getElementType());
-    Value unsqueezed = createONNX.unsqueeze(unsqueezedType, rhs, axes);
-    op->setOperand(1, unsqueezed);
-    Attribute removed = op->removeAttr("axis");
-    assert(removed && "axis was removed");
+    rewriter.updateRootInPlace(op, [&]{
+      OnnxBuilder createONNX(rewriter, op->getLoc());
+      SmallVector<int64_t> axesArray;
+      SmallVector<int64_t> unsqueezedShape(rhsType.getShape());
+      for (int64_t axis = rhsRank; axis < lhsRank - axis + 1; ++axis) {
+        axesArray.push_back(axis);
+        unsqueezedShape.push_back(1);
+      }
+      Value axes = createONNX.constantInt64(axesArray);
+      auto unsqueezedType =
+          RankedTensorType::get(unsqueezedShape, rhsType.getElementType());
+      Value unsqueezed = createONNX.unsqueeze(unsqueezedType, rhs, axes);
+      op->setOperand(1, unsqueezed);
+      Attribute removed = op->removeAttr("axis");
+      assert(removed && "axis was removed");
+    });
     return success();
   }
 };
