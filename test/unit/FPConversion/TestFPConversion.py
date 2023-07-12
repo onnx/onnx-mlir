45ce23b2332f37b7c7597bb2ee5224dd9477ce10
Soren Lassen <slassen@groq.com>
2023-07-12 11:02:11 -0500

    test_zero
    Signed-off-by: Soren Lassen <slassen@groq.com>

--- test/unit/FPConversion/TestFPConversion.py
+++ test/unit/FPConversion/TestFPConversion.py
@@ -1,12 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
-import onnx
+from onnx import helper
 import unittest
 
 class TestCase(unittest.TestCase):
 
-    def test_simple(self):
-        self.assertTrue(True)
+    def test_zero(self):
+        zero = helper.float32_to_float8e5m2(0.0)
+        self.assertEqual(zero, 0)
+        negZero = helper.float32_to_float8e5m2(-0.0)
+        self.assertEqual(negZero, 0x80)
 
 if __name__ == '__main__':
     unittest.main()
