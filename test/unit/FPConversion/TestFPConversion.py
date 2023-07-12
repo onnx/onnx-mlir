9e7fa13be798426bda0a8c11f311242b799b6825
Soren Lassen <slassen@groq.com>
2023-07-12 11:07:41 -0500

    test_inf
    Signed-off-by: Soren Lassen <slassen@groq.com>

--- test/unit/FPConversion/TestFPConversion.py
+++ test/unit/FPConversion/TestFPConversion.py
@@ -11,5 +11,11 @@ class TestCase(unittest.TestCase):
         negZero = helper.float32_to_float8e5m2(-0.0)
         self.assertEqual(negZero, 0x80)
 
+    def test_inf(self):
+        inf = helper.float32_to_float8e5m2(float('inf'))
+        self.assertEqual(inf, 0x7B)
+        negInf = helper.float32_to_float8e5m2(-float('inf'))
+        self.assertEqual(negInf, 0xFB)
+
 if __name__ == '__main__':
     unittest.main()
