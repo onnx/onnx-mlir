b859bb45f1032d1f6ca18ddbc70ec2ed1fc29d3f
Soren Lassen <slassen@groq.com>
2023-07-12 10:37:11 -0500

    python test scaffolding
    Signed-off-by: Soren Lassen <slassen@groq.com>

---
+++ test/unit/FPConversion/TestFPConversion.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import onnx
+import unittest
+
+class TestCase(unittest.TestCase):
+
+    def test_simple(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
