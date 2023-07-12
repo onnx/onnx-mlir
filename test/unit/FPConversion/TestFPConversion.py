f2b259f21be3e8d3e246454ad4364627dca04c27
Soren Lassen <slassen@groq.com>
2023-07-12 11:37:42 -0500

    TestF8E4M3FNUZ
    Signed-off-by: Soren Lassen <slassen@groq.com>

--- test/unit/FPConversion/TestFPConversion.py
+++ test/unit/FPConversion/TestFPConversion.py
@@ -30,6 +30,33 @@ class TestF8E4M3FN(unittest.TestCase):
     def test_nan(self):
         self.assertF8Equal(float('nan'), 0x7F)
 
+class TestF8E4M3FNUZ(unittest.TestCase):
+
+    @staticmethod
+    def toF8(f, saturate=True):
+        return helper.float32_to_float8e4m3(f, uz=True, saturate=saturate)
+
+    def assertF8Equal(self, f, i, j=None):
+        if j is None:
+            j = i
+        self.assertEqual(self.toF8(f), i)
+        self.assertEqual(self.toF8(f, saturate=False), j)
+
+    def test_zero(self):
+        self.assertF8Equal(0.0, 0)
+        self.assertF8Equal(-0.0, 0)
+
+    def test_max(self):
+        self.assertF8Equal(57344.0, 0x7F, 0x80)
+        self.assertF8Equal(-57344.0, 0xFF, 0x80)
+
+    def test_inf(self):
+        self.assertF8Equal(float('inf'), 0x7F, 0x80)
+        self.assertF8Equal(-float('inf'), 0XFF, 0x80)
+
+    def test_nan(self):
+        self.assertF8Equal(float('nan'), 0x80)
+
 class TestF8E5M2(unittest.TestCase):
 
     @staticmethod
