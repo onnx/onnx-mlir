289f0bac67f955a4078507a0c5ca1fb2867c3f01
Soren Lassen <slassen@groq.com>
2023-07-12 11:18:49 -0500

    TestF8E5M2FNUZ
    Signed-off-by: Soren Lassen <slassen@groq.com>

--- test/unit/FPConversion/TestFPConversion.py
+++ test/unit/FPConversion/TestFPConversion.py
@@ -3,19 +3,39 @@
 from onnx import helper
 import unittest
 
-class TestCase(unittest.TestCase):
+class TestF8E5M2(unittest.TestCase):
+
+    @staticmethod
+    def toF8(f):
+        return helper.float32_to_float8e5m2(f)
+
+    def assertF8Equal(self, f, i):
+        self.assertEqual(self.toF8(f), i)
 
     def test_zero(self):
-        zero = helper.float32_to_float8e5m2(0.0)
-        self.assertEqual(zero, 0)
-        negZero = helper.float32_to_float8e5m2(-0.0)
-        self.assertEqual(negZero, 0x80)
+        self.assertF8Equal(0.0, 0)
+        self.assertF8Equal(-0.0, 0x80)
 
     def test_inf(self):
-        inf = helper.float32_to_float8e5m2(float('inf'))
-        self.assertEqual(inf, 0x7B)
-        negInf = helper.float32_to_float8e5m2(-float('inf'))
-        self.assertEqual(negInf, 0xFB)
+        self.assertF8Equal(float('inf'), 0x7B)
+        self.assertF8Equal(-float('inf'), 0xFB)
+
+class TestF8E5M2FNUZ(unittest.TestCase):
+
+    @staticmethod
+    def toF8(f):
+        return helper.float32_to_float8e5m2(f, fn=True, uz=True)
+
+    def assertF8Equal(self, f, i):
+        self.assertEqual(self.toF8(f), i)
+
+    def test_zero(self):
+        self.assertF8Equal(0.0, 0)
+        self.assertF8Equal(-0.0, 0)
+
+    def test_inf(self):
+        self.assertF8Equal(float('inf'), 0x7F)
+        self.assertF8Equal(-float('inf'), 0xFF)
 
 if __name__ == '__main__':
     unittest.main()
