c380f387ee3b1bee7702dbc3fe2ce9f77604ee52
Charles Volzka <cjvolzka@us.ibm.com>
2023-07-21 11:53:26 -0500

    Revert "Bump to llvm commit 8b76aea8d8b1b71f6220bc2845abc749f18a19b7 (#2366)"
    This reverts commit ab4fa45752caf317e69eed08f29ea9b8e3c44287.

--- src/Dialect/ONNX/ONNXOps/Additional/ShapeTransform.cpp
+++ src/Dialect/ONNX/ONNXOps/Additional/ShapeTransform.cpp
@@ -47,7 +47,8 @@ LogicalResult ONNXShapeTransformOpShapeHelper::computeShape() {
   // TODO: support dynamic shape.
   MemRefType affineMemRefType =
       MemRefType::get(inputDims, elementType, AffineMapAttr::get(indexMap));
-  MemRefType flatMemRefType = affine::normalizeMemRefType(affineMemRefType);
+  MemRefType flatMemRefType =
+      affine::normalizeMemRefType(affineMemRefType, /*numSymbols=*/0);
   assert((flatMemRefType.getRank() == outputRank) && "Normalization failed");
 
   DimsExpr outputIEs(outputRank);
