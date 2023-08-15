baa8bacdfc7cb510a1220f635328234bd49a5f1f
Alexandre Eichenberger <alexe@us.ibm.com>
2023-08-15 08:58:15 -0400

    fix name
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- utils/make-report.py
+++ utils/make-report.py
@@ -2,7 +2,7 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-##################### fixLitTest.py ########################################
+##################### make-report.py ########################################
 #
 # Copyright 2023 The IBM Research Authors.
 #
