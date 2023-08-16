7b620eab808847257699f4ef5ba054aa75c0f3c3
Tung D. Le <tung@jp.ibm.com>
2023-08-16 13:48:19 +0900

    Call HybridPass one more to update shape if possible
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- src/Compiler/CompilerPasses.cpp
+++ src/Compiler/CompilerPasses.cpp
@@ -103,6 +103,18 @@ void addONNXToMLIRPasses(mlir::PassManager &pm, bool targetCPU) {
   // Simplify shape-related ops.
   pm.addPass(onnx_mlir::createSimplifyShapeRelatedOpsPass());
 
+  // One more call to ONNX shape inference/canonicalization/... to update shape
+  // if possible.
+  if (enableONNXHybridPass) {
+    // For starters only illustrating the new hybrid pass by replacing 3 passes
+    // here. The plan is to replace most of the passes in addONNXToMLIRPasses.
+    pm.addNestedPass<func::FuncOp>(onnx_mlir::createONNXHybridTransformPass());
+  } else {
+    pm.addNestedPass<func::FuncOp>(onnx_mlir::createShapeInferencePass());
+    pm.addPass(mlir::createCanonicalizerPass());
+    pm.addNestedPass<func::FuncOp>(onnx_mlir::createShapeInferencePass());
+  }
+
   // Replace ONNXReturnOp with func::ReturnOp.
   pm.addPass(onnx_mlir::createStandardFuncReturnPass());
 
