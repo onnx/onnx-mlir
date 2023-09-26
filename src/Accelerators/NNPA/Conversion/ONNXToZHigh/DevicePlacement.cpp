8026ae06e56122008fcb4843ba7284dc8a04cf43
Tung D. Le <tung@jp.ibm.com>
2023-09-26 16:50:13 +0900

    Save configuration to a JSON file
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- src/Accelerators/NNPA/Conversion/ONNXToZHigh/DevicePlacement.cpp
+++ src/Accelerators/NNPA/Conversion/ONNXToZHigh/DevicePlacement.cpp
@@ -20,6 +20,7 @@
 #include "llvm/ADT/SetOperations.h"
 #include "llvm/ADT/SmallSet.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/JSON.h"
 
 #include "src/Accelerators/NNPA/Conversion/ONNXToZHigh/ONNXToZHigh.hpp"
 #include "src/Accelerators/NNPA/Conversion/ONNXToZHigh/ONNXToZHighCommon.hpp"
@@ -44,7 +45,30 @@ struct DevicePlacementPass
     return "Device placement for NNPA";
   }
 
+  // Make sure that we have a valid default constructor and copy
+  // constructor to make sure that the options are initialized properly.
+  DevicePlacementPass() = default;
+  DevicePlacementPass(const DevicePlacementPass &pass)
+      : PassWrapper<DevicePlacementPass, OperationPass<ModuleOp>>() {}
+  DevicePlacementPass(std::string configFile) {
+    this->saveConfigFile = saveConfigFile;
+  }
+
+  Option<std::string> saveConfigFile{*this, "save-config-file",
+      llvm::cl::desc("Path to save a device configuration file in JSON format"),
+      llvm::cl::init("")};
+
+  bool isExcludedOp(Operation *op) {
+    if (op->getDialect()->getNamespace() != ONNXDialect::getDialectNamespace())
+      return true;
+    // No annotation for these ops.
+    if (isa<ONNXEntryPointOp, ONNXReturnOp, ONNXConstantOp>(op))
+      return true;
+    return false;
+  }
+
   void runOnOperation() final;
+  void saveConfigToJSONFile(ModuleOp &module);
 };
 
 void DevicePlacementPass::runOnOperation() {
@@ -99,20 +123,61 @@ void DevicePlacementPass::runOnOperation() {
 
   // Now annotate accelerator operations in the IR with `device` attribute.
   module.walk([&](Operation *op) -> WalkResult {
-    if (op->getDialect()->getNamespace() != ONNXDialect::getDialectNamespace())
+    if (isExcludedOp(op))
       return WalkResult::advance();
-    // No annotation for these ops.
-    if (isa<ONNXEntryPointOp, ONNXReturnOp, ONNXConstantOp>(op))
-      return WalkResult::advance();
-    // If `device` is already set, respect it.
+    // Set device if it is empty or unavailable.
     StringAttr device = op->getAttrOfType<mlir::StringAttr>(DEVICE_ATTRIBUTE);
-    if (device && !device.getValue().empty())
-      return WalkResult::advance();
-    // Otherwise, set device.
-    if (!cpuOps.contains(op))
-      op->setAttr(DEVICE_ATTRIBUTE, StringAttr::get(context, NNPA_DEVICE));
+    if (!device || device.getValue().empty()) {
+      if (!cpuOps.contains(op))
+        op->setAttr(DEVICE_ATTRIBUTE, StringAttr::get(context, NNPA_DEVICE));
+    }
     return WalkResult::advance();
   });
+
+  // Create a JSON configuration file if required.
+  if (!saveConfigFile.empty())
+    saveConfigToJSONFile(module);
+}
+
+void DevicePlacementPass::saveConfigToJSONFile(ModuleOp &module) {
+  llvm::json::Array jsonArr;
+  module.walk([&](Operation *op) -> WalkResult {
+    if (isExcludedOp(op))
+      return WalkResult::advance();
+    // Create a JSON object for this operation.
+    std::string deviceStr =
+        op->getAttrOfType<mlir::StringAttr>("device")
+            ? op->getAttrOfType<mlir::StringAttr>("device").getValue().str()
+            : "";
+    std::string nodeTypeStr = op->getName().getStringRef().str();
+    std::string onnxNodeNameStr =
+        op->getAttrOfType<mlir::StringAttr>("onnx_node_name")
+            ? op->getAttrOfType<mlir::StringAttr>("onnx_node_name")
+                  .getValue()
+                  .str()
+            : "";
+    llvm::json::Value jsonObj = llvm::json::Object{
+        {"device", deviceStr},
+        {"node_type", nodeTypeStr},
+        {"onnx_node_name", onnxNodeNameStr},
+    };
+    jsonArr.emplace_back(jsonObj);
+
+    return WalkResult::advance();
+  });
+
+  // Create a JSON configuration file.
+  if (!saveConfigFile.empty()) {
+    llvm::json::Object jsonContent{
+        {"device_placement", llvm::json::Value(std::move(jsonArr))}};
+    std::error_code EC;
+    llvm::raw_fd_ostream JsonOS(saveConfigFile, EC);
+    if (EC)
+      report_fatal_error("Error saving device placement json file : " +
+                         StringRef(EC.message()));
+    JsonOS << llvm::json::Value(std::move(jsonContent)) << "\n";
+    JsonOS.close();
+  }
 }
 
 } // namespace
@@ -124,6 +189,11 @@ namespace onnx_mlir {
  */
 std::unique_ptr<mlir::Pass> createDevicePlacementPass() {
   return std::make_unique<DevicePlacementPass>();
+}
+
+std::unique_ptr<mlir::Pass> createDevicePlacementPass(
+    std::string saveConfigFile) {
+  return std::make_unique<DevicePlacementPass>(saveConfigFile);
 }
 
 } // namespace onnx_mlir
