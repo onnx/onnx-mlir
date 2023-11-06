17ed559d3ea36509fe288b1b74e8273fd36b5359
Alexandre Eichenberger <alexe@us.ibm.com>
2023-11-06 12:46:16 -0500

    format
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Conversion/ONNXToKrnl/NN/Normalization.cpp
+++ src/Conversion/ONNXToKrnl/NN/Normalization.cpp
@@ -713,8 +713,7 @@ struct ONNXLayerNormalizationOpLowering
           MDBuilder create(ck);
           IndexExprScope innerScope(ck);
           IndexExpr blockedCurrIndex = DimIndexExpr(blockedLoopIndices[0]);
-          IndexExpr blockedUB =
-              SymbolIndexExpr(XFlatDims[0].getValue());
+          IndexExpr blockedUB = SymbolIndexExpr(XFlatDims[0].getValue());
           IndexExpr isFull = create.krnlIE.isTileFull(
               blockedCurrIndex, LiteralIndexExpr(B), blockedUB);
           Value zero = create.math.constantIndex(0);
