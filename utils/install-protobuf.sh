343bf91757b5730793984c0a4e20912e8716a208
Megan Hampton <hamptonm@us.ibm.com>
2023-11-09 17:50:28 -0500

    Ugh

--- utils/install-protobuf.sh
+++ utils/install-protobuf.sh
@@ -1,4 +1,4 @@
-# Check out protobuf
+# Check out protobuf (mac x86 seems to need an older version)
 PROTOBUF_VERSION=3.18.3
 git clone -b v${PROTOBUF_VERSION} --depth 1 --recursive https://github.com/protocolbuffers/protobuf.git
 
