5dffe4eef11f4efad4c3850c591d319fec2b8682
Tung D. Le <tung@jp.ibm.com>
2023-11-10 22:27:21 +0900

    format
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- utils/testing/dim_param.py
+++ utils/testing/dim_param.py
@@ -21,19 +21,23 @@ from google.protobuf.json_format import MessageToJson
 
 
 # Create one input (ValueInfoProto)
-X = helper.make_tensor_value_info("X", TensorProto.FLOAT, ["batch_size", "sequence_len"])
+X = helper.make_tensor_value_info(
+    "X", TensorProto.FLOAT, ["batch_size", "sequence_len"]
+)
 
 # Create second input (ValueInfoProto)
 Y = helper.make_tensor_value_info("Y", TensorProto.INT64, [1, "sequence_len"])
 
 # Create one output (ValueInfoProto)
-Z = helper.make_tensor_value_info("Z", TensorProto.FLOAT, ["batch_size", "sequence_len"])
+Z = helper.make_tensor_value_info(
+    "Z", TensorProto.FLOAT, ["batch_size", "sequence_len"]
+)
 
 # Create a node (NodeProto)
 node_def = helper.make_node(
-    "Add", # node name
-    ["X", "Y"], # inputs
-    ["Z"], # outputs
+    "Add",  # node name
+    ["X", "Y"],  # inputs
+    ["Z"],  # outputs
 )
 
 # Create the graph (GraphProto)
