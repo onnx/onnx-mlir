a145ceb41bfe54da5323cff3d79a90a427829c73
Ferdinand Lemaire <ferdinan@xilinx.com>
2024-01-03 14:19:04 +0000

    Align with PR for logical not

--- src/Conversion/ONNXToTOSA/ONNXToTOSACommon.hpp
+++ src/Conversion/ONNXToTOSA/ONNXToTOSACommon.hpp
@@ -311,7 +311,7 @@ inline bool isTOSABool(mlir::Type type) {
 
 inline bool isTOSASignedInt(mlir::Type type) {
   mlir::IntegerType intType = type.dyn_cast<mlir::IntegerType>();
-  std::set<unsigned> intWidth{1, 8, 16, 32, 48, 64};
+  std::set<unsigned> intWidth{8, 16, 32, 48, 64};
   return intType && intType.isSignless() &&
          (intWidth.find(intType.getWidth()) != intWidth.end());
 }
