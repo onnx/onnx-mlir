596d2bd9b806a9a979ba1583150eb21bf1bc92f2
Tung D. Le <tung@jp.ibm.com>
2024-01-18 03:02:38 -0500

    Try pthread
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- src/Accelerators/NNPA/Runtime/zDNNExtension/MatMul.c
+++ src/Accelerators/NNPA/Runtime/zDNNExtension/MatMul.c
@@ -18,7 +18,9 @@
 #endif
 #include <pthread.h>
 
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "zdnn.h"
 
@@ -27,28 +29,63 @@ extern "C" {
 #endif
 
 #define DEBUG 1
+#define USE_PTHREAD 1
+
+struct mmStruct {
+  zdnn_ztensor *input_a;
+  zdnn_ztensor *input_b;
+  zdnn_ztensor *input_c;
+  zdnn_matmul_ops op_type;
+  zdnn_ztensor *output;
+};
+
+void *call_zdnn_matmul_op(void *args) {
+  struct mmStruct *p = (struct mmStruct *)args;
+  zdnn_status status =
+      zdnn_matmul_op(p->input_a, p->input_b, p->input_c, p->op_type, p->output);
+  return (void *)status;
+}
 
 zdnn_status zdnn_matmul_op_ext(const zdnn_ztensor *input_a,
     const zdnn_ztensor *input_b, const zdnn_ztensor *input_c,
     zdnn_matmul_ops op_type, zdnn_ztensor *output) {
   if (DEBUG) {
     printf("I am in zdnn_matmul_op_ext\n");
-    // Examine input_a.
     const zdnn_tensor_desc *desc = input_a->transformed_desc;
-    printf("A: [%d, %d, %d, %d], %d\n", desc->dim4, desc->dim3, desc->dim2,
+    printf("A: [%d, %d, %d, %d], %d, ", desc->dim4, desc->dim3, desc->dim2,
         desc->dim1, desc->layout);
     desc = input_b->transformed_desc;
-    printf("B: [%d, %d, %d, %d], %d\n", desc->dim4, desc->dim3, desc->dim2,
+    printf("B: [%d, %d, %d, %d], %d, ", desc->dim4, desc->dim3, desc->dim2,
         desc->dim1, desc->layout);
     desc = input_c->transformed_desc;
-    printf("C: [%d, %d, %d, %d], %d\n", desc->dim4, desc->dim3, desc->dim2,
+    printf("C: [%d, %d, %d, %d], %d.", desc->dim4, desc->dim3, desc->dim2,
         desc->dim1, desc->layout);
     desc = output->transformed_desc;
     printf("Output: [%d, %d, %d, %d], %d\n", desc->dim4, desc->dim3, desc->dim2,
         desc->dim1, desc->layout);
   }
-  zdnn_status status =
-      zdnn_matmul_op(input_a, input_b, input_c, op_type, output);
+
+  struct mmStruct *args = malloc(sizeof(struct mmStruct));
+  args->input_a = input_a;
+  args->input_b = input_b;
+  args->input_c = input_c;
+  args->op_type = op_type;
+  args->output = output;
+
+  zdnn_status status;
+  if (USE_PTHREAD) {
+    printf("Using pthread\n");
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, call_zdnn_matmul_op, (void *)args);
+    pthread_join(thread_id, &status);
+  } else {
+    printf("No pthread\n");
+    status = (zdnn_status)call_zdnn_matmul_op(args);
+  }
+
+  free(args);
+  assert(status == ZDNN_OK && "Failed to call zdnn_matmul_op");
+
   return status;
 }
 
