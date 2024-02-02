251cff859d0eb090b335808e70b68a697fe892af
Megan Hampton <hamptonm@us.ibm.com>
2024-02-02 12:54:27 -0500

    Still testing

--- src/Compiler/CompilerPasses.cpp
+++ src/Compiler/CompilerPasses.cpp
@@ -70,10 +70,10 @@ void addONNXToMLIRPasses(mlir::PassManager &pm, bool targetCPU) {
       std::make_unique<DisposableGarbageCollector>(pm.getContext()));
   // Decompose first. Eliminates some unsupported ops without shape inference.
   pm.addNestedPass<func::FuncOp>(onnx_mlir::createDecomposeONNXToONNXPass());
-  if (!disableRecomposeOption)
-    pm.addNestedPass<func::FuncOp>(onnx_mlir::createRecomposeONNXToONNXPass());
   // Add canonicalizer pass to access ConstantOpNormalizationPattern
   pm.addPass(mlir::createCanonicalizerPass());
+  if (!disableRecomposeOption)
+    pm.addNestedPass<func::FuncOp>(onnx_mlir::createRecomposeONNXToONNXPass());
   if (enableONNXHybridPass) {
     pm.addNestedPass<func::FuncOp>(
         onnx_mlir::createONNXHybridTransformPass(!disableRecomposeOption));
