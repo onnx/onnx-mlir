97bb25b043efbfa616c7c5b2e1927f4c955b0c22
Robert Esclapez-Garcia <Robert.Garcia@xilinx.com>
2024-02-09 17:41:24 +0000

    feat: Lower ONNXShrink to TOSA

---
+++ src/Conversion/ONNXToTOSA/Tensor/Shrink.cpp
@@ -0,0 +1,91 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===---------------- Shrink.cpp - Shrink Op-------------------------------===//
+//
+// Copyright (c) 2022 Advanced Micro Devices, Inc.
+//
+// =============================================================================
+//
+// This file lowers the ONNX Shrink operator to TOSA dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/Tosa/IR/TosaOps.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "src/Conversion/ONNXToTOSA/DialectBuilder.hpp"
+#include "src/Conversion/ONNXToTOSA/ONNXToTOSACommon.hpp"
+#include "src/Conversion/ONNXToTOSA/ONNXToTOSALegalizeUtils.hpp"
+
+using namespace mlir;
+
+namespace onnx_mlir {
+
+namespace {
+
+class ONNXShrinkOpLoweringToTOSA : public OpConversionPattern<ONNXShrinkOp> {
+public:
+  using OpConversionPattern<ONNXShrinkOp>::OpConversionPattern;
+
+  LogicalResult matchAndRewrite(ONNXShrinkOp shrinkOp, OpAdaptor adaptor,
+      ConversionPatternRewriter &rewriter) const override {
+    Location loc = shrinkOp->getLoc();
+
+    auto lambd = adaptor.getLambdAttr();
+    auto bias = adaptor.getBiasAttr();
+    auto input = adaptor.getInput();
+
+    TosaBuilder tosaBuilder(rewriter, loc);
+
+    auto inputRankedTensorTy = dyn_cast<RankedTensorType>(input.getType());
+    if (!inputRankedTensorTy) {
+      return rewriter.notifyMatchFailure(
+          loc, "Expected RankedTensorType for input data of ShrinkOp");
+    }
+
+    // lambd and bias have float type so it is safe to conver it to float
+    const float lambdAsFloat = lambd.getValue().convertToFloat();
+    const float biasAsFloat = bias.getValue().convertToFloat();
+    auto lambdConstOp = tosaBuilder.getSplattedConst(lambdAsFloat,
+        inputRankedTensorTy.getShape(), inputRankedTensorTy.getElementType());
+    auto negatedLambdConstOp = tosaBuilder.getSplattedConst(-lambdAsFloat,
+        inputRankedTensorTy.getShape(), inputRankedTensorTy.getElementType());
+    auto biasConstOp = tosaBuilder.getSplattedConst(biasAsFloat,
+        inputRankedTensorTy.getShape(), inputRankedTensorTy.getElementType());
+    auto zeroConstOp = tosaBuilder.getSplattedConst(0,
+        inputRankedTensorTy.getShape(), inputRankedTensorTy.getElementType());
+
+    // Formula to be implemented:
+    // { x < -lambd, then y = x + bias
+    // { x > lambd,  then y = x - bias
+    // { otherwise,  then y = 0
+
+    auto firstCmp = tosaBuilder.compareOp<mlir::tosa::GreaterOp>(
+        rewriter, loc, negatedLambdConstOp, input);
+    auto firstFormula =
+        tosaBuilder.binaryOp<mlir::tosa::AddOp>(input, biasConstOp);
+    auto firstSelect = tosaBuilder.select(firstCmp, firstFormula, zeroConstOp);
+
+    auto secondCmp = tosaBuilder.compareOp<mlir::tosa::GreaterOp>(
+        rewriter, loc, input, lambdConstOp);
+    auto secondFormula =
+        tosaBuilder.binaryOp<mlir::tosa::SubOp>(input, biasConstOp);
+    auto secondSelect =
+        tosaBuilder.select(secondCmp, secondFormula, firstSelect);
+
+    rewriter.replaceOp(shrinkOp, secondSelect);
+
+    return success();
+  }
+};
+
+} // namespace
+
+void populateLoweringONNXShrinkOpToTOSAPattern(ConversionTarget & /*target*/,
+    RewritePatternSet &patterns, TypeConverter & /*typeConverter*/,
+    MLIRContext *ctx) {
+  patterns.insert<ONNXShrinkOpLoweringToTOSA>(ctx);
+}
+
+} // namespace onnx_mlir
