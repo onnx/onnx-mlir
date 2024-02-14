bdd30135545f2bb323e02a9256012f0bf9747062
Tung D. Le <tung@jp.ibm.com>
2024-02-13 20:39:17 -0500

    Do not store bool constants to file
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- src/Conversion/KrnlToLLVM/ConvertKrnlToLLVM.cpp
+++ src/Conversion/KrnlToLLVM/ConvertKrnlToLLVM.cpp
@@ -479,6 +479,9 @@ bool extractConstantsToFile(ModuleOp &module, std::string filepath,
       return WalkResult::advance();
 
     // Ignore constants of bool.
+    // For an unknown reason, enabling constants of bool caused segfault in the
+    // IBM granite.20B model (The model with KV cache) at 1265 input tokens.
+    // See issue https://github.com/onnx/onnx-mlir/issues/2713.
     if (llvm::cast<MemRefType>(op->getResult(0).getType())
             .getElementType()
             .isInteger(1))
