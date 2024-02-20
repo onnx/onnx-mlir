b92e5a5cb165f849a567fb52807f213bffd24ba3
Megan Hampton <hamptonm@us.ibm.com>
2024-02-20 12:56:34 -0500

    Test if F16 attr will work

--- src/Dialect/Mlir/DialectBuilder.cpp
+++ src/Dialect/Mlir/DialectBuilder.cpp
@@ -479,6 +479,9 @@ Value MathBuilder::constantIndex(int64_t val) const {
 TypedAttr MathBuilder::negativeInfAttr(mlir::Type type) const {
   TypedAttr attr;
   TypeSwitch<Type>(type)
+      .Case<Float16Type>([&](Type) {
+        attr = b().getF16FloatAttr(-std::numeric_limits<float>::infinity());
+      })
       .Case<Float32Type>([&](Type) {
         attr = b().getF32FloatAttr(-std::numeric_limits<float>::infinity());
       })
