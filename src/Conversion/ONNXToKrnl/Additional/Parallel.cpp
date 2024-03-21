84f9f0d1ee3632f94973d20d76e212ef4ec4bd19
Haruki Imai <imaihal@jp.ibm.com>
2024-03-21 05:26:42 +0000

    Clean up by creating a function for moving alloc op
    Signed-off-by: Haruki Imai <imaihal@jp.ibm.com>

--- src/Conversion/ONNXToKrnl/Additional/Parallel.cpp
+++ src/Conversion/ONNXToKrnl/Additional/Parallel.cpp
@@ -22,6 +22,50 @@ using namespace mlir;
 
 namespace onnx_mlir {
 
+LogicalResult moveAllocOpBeforeAndReplaceAllUses(
+    ConversionPatternRewriter &rewriter, Operation *op, Operation *yieldOp) {
+
+  for (unsigned ii = 0; ii < yieldOp->getNumOperands(); ++ii) {
+    // Check the return value of the block to check if operations in the
+    // block is already lowered to memref-level IR such as KrnlIR. Assume
+    // the block is still not lowered if the return value is still Tensor
+    // type. Actual return value of ONNXYieldOp is conveted into tensor by
+    // unrealized_conversion_cast. So, check the operand of previous
+    // operation.
+    Value returnVal = yieldOp->getOperands()[ii];
+    if (isa<UnrealizedConversionCastOp>(returnVal.getDefiningOp()))
+      returnVal = returnVal.getDefiningOp()->getOperands()[0];
+    if (isa<TensorType>(returnVal.getType()))
+      return failure();
+
+    // Move allocOps for results before op
+    Operation *justMovedOp = nullptr;
+    // Move AllocOp's operands first.
+    // TODO: Stll not enough to suppor dynamic dim. Need update
+    Operation *allocOpForReturnVal = returnVal.getDefiningOp();
+    for (unsigned i = 0; i < allocOpForReturnVal->getNumOperands(); ++i) {
+      Value oprd = allocOpForReturnVal->getOperand(i);
+      if (isa<BlockArgument>(oprd))
+        continue;
+      Operation *opToMove = oprd.getDefiningOp();
+      if (justMovedOp)
+        opToMove->moveAfter(justMovedOp);
+      else
+        opToMove->moveBefore(op);
+      justMovedOp = opToMove;
+    }
+    // Move AllocOp.
+    if (justMovedOp)
+      allocOpForReturnVal->moveAfter(justMovedOp);
+    else
+      allocOpForReturnVal->moveBefore(op);
+
+    rewriter.replaceAllUsesWith(
+        op->getResults()[ii], allocOpForReturnVal->getResult(0));
+  }
+  return success();
+}
+
 struct ONNXParallelOpLowering : public OpConversionPattern<ONNXParallelOp> {
   explicit ONNXParallelOpLowering(
       TypeConverter &typeConverter, MLIRContext *ctx)
@@ -34,7 +78,6 @@ struct ONNXParallelOpLowering : public OpConversionPattern<ONNXParallelOp> {
     Location loc = ONNXLoc<ONNXParallelOp>(op);
     IndexExprScope ieScope(&rewriter, loc);
     ValueRange operands = adaptor.getOperands();
-    // llvm::dbgs() << "ONNXParallelOpLowering start\n";
     MultiDialectBuilder<KrnlBuilder, IndexExprBuilderForKrnl, MemRefBuilder,
         MathBuilder>
         create(rewriter, loc);
@@ -54,105 +97,23 @@ struct ONNXParallelOpLowering : public OpConversionPattern<ONNXParallelOp> {
     // ForkOps in ParallelOp region
     // Move alloc op for return values and replace uses with them.
     // Still not replace ForkOps yet.
-    //    llvm::dbgs() << "ONNXParallelOpLowering: ParallelOp before move: "
-    //		 << parallelOp << "\n";
     SmallVector<ONNXForkOp, 4> forkOps;
-    SmallVector<std::vector<Value>, 4> forkOuts;
-    for (Operation &op : bodyBlock.getOperations()) {
-      if (auto forkOp = dyn_cast<ONNXForkOp>(op)) {
+    for (Operation &bOp : bodyBlock.getOperations()) {
+      if (auto forkOp = dyn_cast<ONNXForkOp>(bOp)) {
         forkOps.push_back(forkOp);
-        auto onnxYieldOp = llvm::dyn_cast<ONNXYieldOp>(
-            forkOp.getRegion().back().getTerminator());
-        if (!onnxYieldOp)
+        Operation *forkYieldOp = forkOp.getBody().front().getTerminator();
+        if (!isa<ONNXYieldOp>(forkYieldOp))
           return failure();
-        // Check the return value of the block to check if operations in the
-        // block is already lowered to memref-level IR such as KrnlIR. Assume
-        // the block is still not lowered if the return value is still Tensor
-        // type. Actual return value of ONNXYieldOp is conveted into tensor by
-        // unrealized_conversion_cast. So, check the operand of previous
-        // operation.
-        // for (Value v : onnxYieldOp.getOperands()) {
-        std::vector<Value> forkOut;
-        for (unsigned ii = 0; ii < onnxYieldOp.getNumOperands(); ++ii) {
-          //	  Value v = onnxYieldOp.getOperands()[ii];
-          //          Value returnVal = v.getDefiningOp()->getOperands()[0];
-          Value returnVal = onnxYieldOp.getOperands()[ii];
-          if (isa<UnrealizedConversionCastOp>(returnVal.getDefiningOp()))
-            returnVal = returnVal.getDefiningOp()->getOperands()[0];
-          //          llvm::dbgs() << "ONNXParallelOpLowering: ForkOp
-          //          returnValue: "
-          //                       << returnVal << "\n";
-          if (isa<TensorType>(returnVal.getType()))
-            return failure();
 
-          // Move allocOps for results before ForkOp
-          Operation *justMovedOp = nullptr;
-          // Move AllocOp's operands first.
-          Operation *returnValAllocOp = returnVal.getDefiningOp();
-          for (unsigned i = 0; i < returnValAllocOp->getNumOperands(); ++i) {
-            Value oprd = returnValAllocOp->getOperand(i);
-            if (isa<BlockArgument>(oprd))
-              continue;
-            Operation *opToMove = oprd.getDefiningOp();
-            if (justMovedOp)
-              opToMove->moveAfter(justMovedOp);
-            else
-              opToMove->moveBefore(forkOp);
-            justMovedOp = opToMove;
-          }
-          // Move AllocOp.
-          if (justMovedOp)
-            returnValAllocOp->moveAfter(justMovedOp);
-          else
-            returnValAllocOp->moveBefore(forkOp);
-
-          rewriter.replaceAllUsesWith(
-              forkOp.getResults()[ii], returnValAllocOp->getResult(0));
-          forkOut.push_back(returnValAllocOp->getResult(0));
-        }
-        forkOuts.push_back(forkOut);
+        if (failed(moveAllocOpBeforeAndReplaceAllUses(
+                rewriter, &bOp, forkYieldOp)))
+          return failure();
       }
     }
-    //    llvm::dbgs() << "ONNXParallelOpLowering: ParallelOp after move: "
-    //                 << parallelOp << "\n";
+
     // Move allocOp in ParallelOp region
-    SmallVector<Value, 4> outs;
-    for (unsigned ii = 0; ii < yieldOp->getNumOperands(); ++ii) {
-      Value returnVal = yieldOp->getOperands()[ii];
-      if (isa<UnrealizedConversionCastOp>(returnVal.getDefiningOp()))
-        returnVal = returnVal.getDefiningOp()->getOperands()[0];
-      //      llvm::dbgs() << "ONNXParallelOpLowering: ParallelOp returnValue: "
-      //                   << returnVal << "\n";
-      if (isa<TensorType>(returnVal.getType()))
-        return failure();
-
-      // Move allocOps for results before ForkOp
-      Operation *justMovedOp = nullptr;
-      // Move AllocOp's operands first.
-      Operation *returnValAllocOp = returnVal.getDefiningOp();
-      for (unsigned i = 0; i < returnValAllocOp->getNumOperands(); ++i) {
-        Value oprd = returnValAllocOp->getOperand(i);
-        if (isa<BlockArgument>(oprd))
-          continue;
-        Operation *opToMove = oprd.getDefiningOp();
-        if (justMovedOp)
-          opToMove->moveAfter(justMovedOp);
-        else
-          opToMove->moveBefore(parallelOp);
-        justMovedOp = opToMove;
-      }
-      // Move AllocOp.
-      if (justMovedOp)
-        returnValAllocOp->moveAfter(justMovedOp);
-      else
-        returnValAllocOp->moveBefore(parallelOp);
-
-      rewriter.replaceAllUsesWith(
-          parallelOp.getResults()[ii], returnValAllocOp->getResult(0));
-      outs.push_back(returnValAllocOp->getResult(0));
-    }
-    //    llvm::dbgs() << "ONNXParallelOpLowering: ParallelOp after move 2: "
-    //                 << parallelOp << "\n";
+    if (failed(moveAllocOpBeforeAndReplaceAllUses(rewriter, op, yieldOp)))
+      return failure();
 
     // 2. Create KrnlIterateOp and replace Parallel Op with it.
     std::vector<Value> loop;
@@ -166,13 +127,8 @@ struct ONNXParallelOpLowering : public OpConversionPattern<ONNXParallelOp> {
     Block &iterationBlock = iterateOp.getBodyRegion().back();
     rewriter.setInsertionPointToStart(&iterationBlock);
     ValueRange indices = createKrnl.getInductionVarValue({loop[0]});
-    // rewriter.eraseOp(llvm::dyn_cast<ONNXYieldOp>(yieldOp));
     rewriter.eraseOp(yieldOp);
     rewriter.inlineBlockBefore(&bodyBlock, iterationBlock.getTerminator());
-    //    rewriter.eraseOp(op); // Need this kinds of line, but not
-    // here
-    // llvm::dbgs() << "ONNXParallelOpLowering: iterateOp: " << iterateOp <<
-    // "\n";
 
     // 3. Create SCFIfOp and replace ForkOp with it.
     int64_t id = 0;
@@ -187,14 +143,11 @@ struct ONNXParallelOpLowering : public OpConversionPattern<ONNXParallelOp> {
       rewriter.eraseOp(forkYieldOp);
       rewriter.inlineBlockBefore(
           &forkOp.getRegion().back(), ifBlock.getTerminator());
-      // rewriter.eraseOp(forkOp);
-      rewriter.replaceOp(forkOp, forkOuts[id]);
+      rewriter.eraseOp(forkOp);
       id++;
     }
-    // llvm::dbgs() << "ONNXParallelOpLowering: iterateOp: " << iterateOp <<
-    // "\n"; llvm::dbgs() << "ONNXParallelOpLowering end\n";
 
-    rewriter.replaceOp(op, outs);
+    rewriter.eraseOp(op);
     return success();
   }
 };
