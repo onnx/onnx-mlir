cdbb19ea02610d512fa0d6dc5fab8264c6c37da4
Matthias Gehre <matthias.gehre@amd.com>
2024-04-02 16:35:11 +0200

    Use github.com/xilinx/llvm-project

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
-git clone -n https://github.com/llvm/llvm-project.git
+git clone -n https://github.com/xilinx/llvm-project.git
 # Check out a specific branch that is known to work with ONNX-MLIR.
 cd llvm-project && git checkout b2cdf3cc4c08729d0ff582d55e40793a20bbcdcc && cd ..
