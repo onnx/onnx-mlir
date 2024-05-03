f88badc4fee7e8db48b10c0b6e4af3925852a9fe
Tiago Trevisan Jost <tiago.trevisanjost@amd.com>
2024-05-03 14:25:13 +0200

    chore: minor change to utils/install-protobuf.sh

--- utils/install-protobuf.sh
+++ utils/install-protobuf.sh
@@ -3,7 +3,7 @@ PROTOBUF_VERSION=21.12
 git clone -b v${PROTOBUF_VERSION} --depth 1 --recursive https://github.com/protocolbuffers/protobuf.git
 
 cd protobuf
-autoreconf -f -i -Wall,no-obsolete
+autoreconf -f -i -Wall
 ./autogen.sh
 ./configure --enable-static=no
 make -j$(sysctl -n hw.logicalcpu) install
