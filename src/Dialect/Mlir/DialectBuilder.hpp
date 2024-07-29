8b558c92bc498890d574f708a6c227ffb1bccfd8
Megan Hampton <hamptonm@us.ibm.com>
2024-07-28 22:42:41 -0400

    Remove duplicate header

--- src/Dialect/Mlir/DialectBuilder.hpp
+++ src/Dialect/Mlir/DialectBuilder.hpp
@@ -12,8 +12,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef ONNX_MLIR_DIALECT_BUILDER_KRNL_H
-#define ONNX_MLIR_DIALECT_BUILDER_KRNL_H
+#ifndef ONNX_MLIR_DIALECT_BUILDER_MLIR_H
+#define ONNX_MLIR_DIALECT_BUILDER_MLIR_H
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
