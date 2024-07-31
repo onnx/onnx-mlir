1e2159a1359ecf566bf74c1746f34eb86b70a749
Tong Chen <chentong@us.ibm.com>
2024-07-31 10:13:21 -0400

    Document for building llvm-project with omp (WIP) (#2891)
    * parallel for
    
    Signed-off-by: Chen Tong <chentong@us.ibm.com>
    
    * run through
    
    Signed-off-by: Tong Chen <chentong@linux0d.pok.ibm.com>
    
    * flag
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * format
    
    Signed-off-by: chentong319 <chentong@us.ibm.com>
    
    * Revert "format"
    
    This reverts commit ebd27e2b38841b89e8bc966820705d3dbc07b394.
    
    * Revert "flag"
    
    This reverts commit 432e4bec6b3869bed70287f2d6a533cac772ddcb.
    
    * fix merge error
    
    Signed-off-by: chentong319 <chentong@us.ibm.com>
    
    * add openmp
    
    Signed-off-by: chentong319 <chentong@us.ibm.com>
    
    * fix
    
    Signed-off-by: chentong319 <chentong@us.ibm.com>
    
    * try
    
    Signed-off-by: chentong319 <chentong@us.ibm.com>
    
    * windows
    
    Signed-off-by: chentong319 <chentong@us.ibm.com>
    
    * space
    
    Signed-off-by: chentong319 <chentong@us.ibm.com>
    
    ---------
    
    Signed-off-by: Chen Tong <chentong@us.ibm.com>
    Signed-off-by: Tong Chen <chentong@linux0d.pok.ibm.com>
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    Signed-off-by: chentong319 <chentong@us.ibm.com>

--- utils/build-mlir.cmd
+++ utils/build-mlir.cmd
@@ -3,13 +3,14 @@ md llvm-project\build
 cd llvm-project\build
 call cmake %root_dir%\llvm-project\llvm -G "Ninja" ^
    -DCMAKE_INSTALL_PREFIX="%root_dir%\llvm-project\build\install" ^
-   -DLLVM_ENABLE_PROJECTS=mlir ^
+   -DLLVM_ENABLE_PROJECTS="mlir;clang;openmp" ^
    -DLLVM_TARGETS_TO_BUILD="host" ^
    -DCMAKE_BUILD_TYPE=Release ^
    -DLLVM_ENABLE_ASSERTIONS=ON ^
    -DLLVM_ENABLE_RTTI=ON ^
    -DLLVM_ENABLE_ZLIB=OFF ^
    -DLLVM_INSTALL_UTILS=ON ^
+   -DENABLE_LIBOMPTARGET=OFF ^
    -DLLVM_ENABLE_LIBEDIT=OFF
 
 call cmake --build . --config Release
