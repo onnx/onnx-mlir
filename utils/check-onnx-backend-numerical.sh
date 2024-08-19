73e505fd1606f07f142dfa839a9d11224079b41d
Alexandre Eichenberger <alexe@us.ibm.com>
2024-08-19 14:35:09 -0400

    add dyn checks to mac ci
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- utils/check-onnx-backend-numerical.sh
+++ utils/check-onnx-backend-numerical.sh
@@ -1,4 +1,4 @@
 # Run backend and numerical tests in parallel
 cd onnx-mlir/build
 CTEST_PARALLEL_LEVEL=$(sysctl -n hw.logicalcpu) \
-cmake --build . --parallel --target check-onnx-backend check-onnx-backend-input-verification check-onnx-numerical
+cmake --build . --parallel --target check-onnx-backend check-onnx-backend-dynamic check-onnx-backend-input-verification check-onnx-numerical
