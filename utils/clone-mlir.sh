5a67800ab9958c1e1063ada6deec1f741ac59b6c
Boyana Norris <bnorris@tsavoritesi.net>
2024-08-23 20:21:00 -0700

    update llvm and stablehlo commits; fix build
    Signed-off-by: Boyana Norris <bnorris@tsavoritesi.net>

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
 git clone -n https://github.com/llvm/llvm-project.git
 # Check out a specific branch that is known to work with ONNX-MLIR.
-cd llvm-project && git checkout 60a7d33106d3cd645d3100a8a935a1e3837f885d && cd ..
+cd llvm-project && git checkout f9031f00f2c90bc0af274b45ec3e169b5250a688 && cd ..
