90c2267225281ed66a502b5c8019c17c363cfba2
Matthias Gehre <matthias.gehre@amd.com>
2024-08-27 16:07:05 +0200

    Extract common CI parts

---
+++ utils/install-dependencies.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+set -e
+
+# Keep in sync with utils/install-protobuf.sh
+PROTOBUF_VERSION=21.12
+git clone -b v${PROTOBUF_VERSION} --depth 1 --recursive https://github.com/protocolbuffers/protobuf.git
+cd protobuf
+./autogen.sh
+./configure --enable-static=no --prefix=/usr
+sudo make -j2 install
+
+python3 -m pip install --upgrade wheel
+python3 -m pip install -r requirements.txt
+pip install onnx==1.15.0
