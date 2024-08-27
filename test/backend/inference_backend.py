19051f2591517487a2ac58cdd9b70e352e6e93c0
Megan Hampton <hamptonm@us.ibm.com>
2024-08-27 12:30:05 -0400

    Small updates

--- test/backend/inference_backend.py
+++ test/backend/inference_backend.py
@@ -12,9 +12,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
-import shutil
 import sys
-import warnings
 import json
 import base64
 import numpy as np
