a9221fd48910e1365c16dd7bd6e4d63b42f4e2a8
Matthias Gehre <matthias.gehre@amd.com>
2024-08-27 09:21:44 +0200

    Bump LLVM

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
 git clone -n https://github.com/xilinx/llvm-project.git
 # Check out a specific branch that is known to work with ONNX-MLIR.
-cd llvm-project && git checkout 643434e85ec4 && cd ..
+cd llvm-project && git checkout d92bf119bfd7 && cd ..
