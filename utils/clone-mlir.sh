3784890c5ad7e60235b95bde69e8bbd19ccc2f27
Christopher Munoz <chrismunoz1019@gmail.com>
2025-01-29 11:09:33 -0500

    upgrading llvm and stablehlo hash. Fixing mlir tests
    Signed-off-by: Christopher Munoz <chrismunoz1019@gmail.com>
    
    formatting fixes

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
 git clone -n https://github.com/llvm/llvm-project.git
 # Check out a specific branch that is known to work with ONNX-MLIR.
-cd llvm-project && git checkout e86910337f98e57f5b9253f7d80d5b916eb1d97e && cd ..
+cd llvm-project && git checkout b270525f730be6e7196667925f5a9bfa153262e9 && cd ..
