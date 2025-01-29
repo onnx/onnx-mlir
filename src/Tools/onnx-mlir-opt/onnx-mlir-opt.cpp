e8bc8bfca56bf3f995ed301de17310ca29e326c3
Haruki Imai <imaihal@jp.ibm.com>
2025-01-29 02:26:42 -0500

    Set number of threads in onnx-mlir-opt.
    Signed-off-by: Haruki Imai <imaihal@jp.ibm.com>

--- src/Tools/onnx-mlir-opt/onnx-mlir-opt.cpp
+++ src/Tools/onnx-mlir-opt/onnx-mlir-opt.cpp
@@ -184,8 +184,18 @@ int main(int argc, char **argv) {
   for (auto *accel : accel::Accelerator::getAccelerators())
     accel->configurePasses();
 
+  std::unique_ptr<llvm::ThreadPoolInterface> threadPoolPtr = nullptr;
   auto passManagerSetupFn = [&](PassManager &pm) {
     MLIRContext *ctx = pm.getContext();
+    // Set number of threads in the MLIRContext
+    if (compilationNumThreads > 0)
+      ctx->disableMultithreading();
+    if (compilationNumThreads > 1) {
+      threadPoolPtr = std::make_unique<llvm::DefaultThreadPool>(
+          llvm::hardware_concurrency(compilationNumThreads));
+      ctx->setThreadPool(*threadPoolPtr);
+    }
+
     // MlirOptMain constructed ctx with our registry so we just load all our
     // already registered dialects.
     ctx->loadAllAvailableDialects();
