7b95bbb18cea05cbb358b295be50fff3912b0707
Tong Chen <chentong@us.ibm.com>
2025-04-10 11:14:03 -0400

    Add z17 for -march (#3113)
    * done
    
    Signed-off-by: Tong Chen <chentong@zaiu.pok.stglabs.ibm.com>
    
    * convert
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * fix
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * format
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    ---------
    
    Signed-off-by: Tong Chen <chentong@zaiu.pok.stglabs.ibm.com>
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    Signed-off-by: Sunny Anand <sunnyanand.979@gmail.com>

--- src/Compiler/CompilerOptions.cpp
+++ src/Compiler/CompilerOptions.cpp
@@ -879,7 +879,7 @@ static int64_t decodeZArchNum(std::string str) {
     return 13;
   if (str == "arch14" || str == "z16") // Z16 and equivalents.
     return 14;
-  if (str == "arch15")
+  if (str == "arch15" || str == "z17") // Z17 and equivalents.
     return 15;
   return -1;
 }
@@ -1386,6 +1386,9 @@ void initCompilerConfig() {
     // Fast math option is enabled (in general)
     setLLVMOption(getLLVMOption() + " --enable-unsafe-fp-math");
   }
+
+  if (march == "z17")
+    march = "arch15";
 }
 
 } // namespace onnx_mlir
