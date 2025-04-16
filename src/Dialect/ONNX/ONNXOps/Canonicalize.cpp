adc83edaa2e4338544815d6dc1964a08e002542e
Arkar-Hema <hema.bhaskar@multicorewareinc.com>
2025-04-16 17:15:20 +0530

    Merge nested concat Ops optimization pass in ONNX dialect (#3111)
    * Merging nested concat ops
    
    Signed-off-by: Arkar-Hema <hema.bhaskar@multicorewareinc.com>
    
    ---------
    
    Signed-off-by: Arkar-Hema <hema.bhaskar@multicorewareinc.com>
    Co-authored-by: Tung D. Le <tung@jp.ibm.com>

--- src/Dialect/ONNX/ONNXOps/Canonicalize.cpp
+++ src/Dialect/ONNX/ONNXOps/Canonicalize.cpp
@@ -1503,6 +1503,62 @@ private:
 };
 
 // =============================================================================
+// Rewrite pattern concat
+// =============================================================================
+
+struct RecomposeConcatPattern : public OpRewritePattern<ONNXConcatOp> {
+  using OpRewritePattern<ONNXConcatOp>::OpRewritePattern;
+
+  // Helper function to check if an input is a mergeable Concat.
+  static bool isMergeableConcat(Value input, int64_t axis) {
+    ONNXConcatOp concatOp = input.getDefiningOp<ONNXConcatOp>();
+    if (!concatOp)
+      return false;
+    return (concatOp.getAxis() == axis) && (concatOp.getResult().hasOneUse());
+  }
+
+  LogicalResult matchAndRewrite(
+      ONNXConcatOp concatOp, PatternRewriter &rewriter) const final {
+    Location loc = concatOp.getLoc();
+    ValueRange inputs = concatOp.getOperands();
+    int64_t axis = concatOp.getAxis();
+
+    // If there is only a single input, replace the concat with that input.
+    if (inputs.size() == 1) {
+      rewriter.replaceOp(concatOp, inputs[0]);
+      return success();
+    }
+
+    SmallVector<Value, 16> newInputs;
+    bool merged = false;
+
+    // Flatten nested concat nodes.
+    for (Value input : inputs) {
+      if (isMergeableConcat(input, axis)) {
+        // Remove the nested concat and append its inputs.
+        ONNXConcatOp innerConcat = cast<ONNXConcatOp>(input.getDefiningOp());
+        newInputs.append(
+            innerConcat.getOperands().begin(), innerConcat.getOperands().end());
+        merged = true;
+      } else {
+        // Push non-mergeable input.
+        newInputs.push_back(input);
+      }
+    }
+
+    if (merged) {
+      // Create a new ONNXConcat op with the flattened inputs.
+      auto newConcat = rewriter.create<ONNXConcatOp>(
+          loc, concatOp.getResult().getType(), newInputs, axis);
+      rewriter.replaceOp(concatOp, newConcat.getResult());
+      return success();
+    }
+
+    return failure();
+  }
+};
+
+// =============================================================================
 // Rewrite pattern LayerNormalization
 // =============================================================================
 
@@ -1720,6 +1776,12 @@ void ONNXCastOp::getCanonicalizationPatterns(
   result.insert<SwapCastSlicePattern>(context);
   // TODO: Reintroduce pattern for sound type combinations, see issue #2210.
   // result.insert<FuseCastCastPattern>(context);
+}
+
+/// on the ONNXConcatOp.
+void ONNXConcatOp::getCanonicalizationPatterns(
+    RewritePatternSet &results, MLIRContext *context) {
+  results.insert<RecomposeConcatPattern>(context);
 }
 
 /// on the ONNXConstantOp.
