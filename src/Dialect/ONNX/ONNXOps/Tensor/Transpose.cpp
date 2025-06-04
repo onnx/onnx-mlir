e3128e9939b14de6a40339540640d8fa056221d2
Alexandre Eichenberger <alexe@us.ibm.com>
2025-06-04 12:19:03 -0400

    Added support for negative permute values in transform (#3191)
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps/Tensor/Transpose.cpp
+++ src/Dialect/ONNX/ONNXOps/Tensor/Transpose.cpp
@@ -30,10 +30,9 @@ LogicalResult ONNXTransposeOpShapeHelper::computeShape() {
   ONNXTransposeOp transposeOp = llvm::cast<ONNXTransposeOp>(op);
 
   Value data = operandAdaptor.getData();
-  if (!hasShapeAndRank(data)) {
+  if (!hasShapeAndRank(data))
     return failure();
-  }
-  auto rank = createIE->getShapedTypeRank(data);
+  int64_t rank = createIE->getShapedTypeRank(data);
 
   // Transposition which handles the default case of
   // reversing the shape of the tensor (similar to numpy.transpose).
@@ -42,7 +41,7 @@ LogicalResult ONNXTransposeOpShapeHelper::computeShape() {
     // Generate reverse order for default transpose operation.
     SmallVector<int64_t, 4> defaultVals;
     auto builder = Builder(op->getContext());
-    for (int i = rank - 1; i >= 0; --i)
+    for (int64_t i = rank - 1; i >= 0; --i)
       defaultVals.emplace_back(i);
     // Set default attribute.
     ArrayRef<int64_t> defaultRefs(defaultVals);
@@ -52,8 +51,12 @@ LogicalResult ONNXTransposeOpShapeHelper::computeShape() {
 
   // Perform transposition according to perm attribute.
   DimsExpr transposedDims;
-  for (decltype(rank) i = 0; i < rank; ++i) {
+  for (int64_t i = 0; i < rank; ++i) {
     int64_t inputIndex = ArrayAttrIntVal(permAttr, i);
+    if (inputIndex < 0)
+      inputIndex += rank;
+    assert(inputIndex >= 0 && inputIndex < rank &&
+           "transpose permute attribute out of bound");
     transposedDims.emplace_back(createIE->getShapeAsDim(data, inputIndex));
   }
 
@@ -66,6 +69,33 @@ LogicalResult ONNXTransposeOpShapeHelper::computeShape() {
 //===----------------------------------------------------------------------===//
 // Verify
 //===----------------------------------------------------------------------===//
+
+LogicalResult ONNXTransposeOp::verify() {
+
+  Value data = getData();
+  if (!hasShapeAndRank(data))
+    return success();
+
+  int64_t rank = mlir::cast<ShapedType>(data.getType()).getRank();
+  auto permAttr = getPermAttr();
+
+  // No permute pattern, it becomes an inverse, its good.
+  if (!permAttr)
+    return success();
+  // Has a permute, make sure its good.
+  for (int64_t i = 0; i < rank; ++i) {
+    int64_t p = ArrayAttrIntVal(permAttr, i);
+    if (p < 0)
+      p += rank;
+    if (p < 0)
+      return emitOpError() << "Transpose op with too low a permute value (" << p
+                           << " at index " << i << ")";
+    if (p >= rank)
+      return emitOpError() << "Transpose op with too high a permute value ("
+                           << p << " at index " << i << ")";
+  }
+  return success();
+}
 
 //===----------------------------------------------------------------------===//
 // Shape Inference
