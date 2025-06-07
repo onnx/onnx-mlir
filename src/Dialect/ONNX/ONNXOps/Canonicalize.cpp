026363008ceaff1e2205d87b73d09e518ee951d5
Alexandre Eichenberger <alexe@us.ibm.com>
2025-06-06 20:55:45 -0400

    remove debug code
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- src/Dialect/ONNX/ONNXOps/Canonicalize.cpp
+++ src/Dialect/ONNX/ONNXOps/Canonicalize.cpp
@@ -1282,9 +1282,6 @@ public:
             (inputIsFloat && expIsInt)))
       return failure();
 
-    fprintf(stderr, "hi alex: iw %d, ew %i, input i/f %d/%d, exp i/f %d/%d\n",
-        (int)inputWidth, (int)expWidth, (int)inputIsInt, (int)inputIsFloat,
-        (int)expIsInt, (int)expIsFloat);
     // Rewrite pow with a cast of the exp type to the input type.
     MultiDialectBuilder<OnnxBuilder> create(rewriter, loc);
     Value newExp = create.onnx.cast(exp, inputElementType);
