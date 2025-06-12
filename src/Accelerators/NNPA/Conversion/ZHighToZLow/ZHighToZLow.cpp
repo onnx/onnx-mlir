5618455215c29d265464c1ec8b9095892cc8c410
Tung D. Le <tung@jp.ibm.com>
2025-06-12 02:02:37 -0400

    redundant changes
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- src/Accelerators/NNPA/Conversion/ZHighToZLow/ZHighToZLow.cpp
+++ src/Accelerators/NNPA/Conversion/ZHighToZLow/ZHighToZLow.cpp
@@ -86,8 +86,7 @@ Value insertAllocForZMemRef(ZMemRefType zType, ArrayRef<IndexExpr> dims,
 
   // Insert alloc.
   SmallVector<IndexExpr> dimList(dims.begin(), dims.end());
-  MultiDialectBuilder<MathBuilder, MemRefBuilder, KrnlBuilder> create(
-      rewriter, loc);
+  MultiDialectBuilder<MemRefBuilder> create(rewriter, loc);
   return create.mem.alignedAlloc(resType, dimList, alignment);
 }
 
