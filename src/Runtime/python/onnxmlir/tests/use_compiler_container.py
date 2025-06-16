1f9522c6248a172bf77a6a0ca81c61122452f87d
Chen Tong <chentong@us.ibm.com>
2025-06-16 14:55:45 -0400

    test
    Signed-off-by: Chen Tong <chentong@us.ibm.com>

--- src/Runtime/python/onnxmlir/tests/use_compiler_container.py
+++ src/Runtime/python/onnxmlir/tests/use_compiler_container.py
@@ -12,8 +12,8 @@ sess = onnxmlir.InferenceSession(
     compiler_image_name="ghcr.io/onnxmlir/onnx-mlir-dev",
     compiler_path="/workdir/onnx-mlir/build/Debug/bin/onnx-mlir",
 )
-# In the above example, all the compiler container related options are the default
-# value. So the command can be simplified to
+# In the example above, all the compiler container related options are the default
+# value. Therefore, the previous sentence can be simplified to the following one:
 """
  sess = onnxmlir.InferenceSession("test_add.onnx", compile_args="-O3 --parallel")
 """
