c4a7ae1f4b14c115ab6bfbcc74c4c4a2aa1b07d4
Sunny Anand <sunnyanand.979@gmail.com>
2025-08-07 00:26:51 -0500

    updates
    Signed-off-by: Sunny Anand <sunnyanand.979@gmail.com>

--- utils/install-protobuf.sh
+++ utils/install-protobuf.sh
@@ -1,12 +1,14 @@
 # Check out protobuf source code and build and install it
-PROTOBUF_VERSION=4.25.1
+set -e 
+PROTOBUF_VERSION=25.1
+INSTALL_PROTOBUF_PATH=~/work
 git clone -b v${PROTOBUF_VERSION} --depth 1 --recursive https://github.com/protocolbuffers/protobuf.git
 cd protobuf
 git submodule update --init --recursive
 mkdir build_source && cd build_source
 cmake -G Ninja ../ \
     -Dprotobuf_BUILD_SHARED_LIBS=OFF \
-    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_PROTOBUF_PATH \
     -Dprotobuf_BUILD_TESTS=OFF \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
@@ -15,5 +17,6 @@ cmake -G Ninja ../ \
     ..
 cmake --build . --target install && ldconfig
 cd ../python && python3 setup.py install --cpp_implementation
+export PATH=$INSTALL_PROTOBUF_PATH/protobuf/include:$INSTALL_PROTOBUF_PATH/protobuf/lib:$INSTALL_PROTOBUF_PATH/protobuf/bin:$PATH
 protoc --version
 echo "protobuf installed"
