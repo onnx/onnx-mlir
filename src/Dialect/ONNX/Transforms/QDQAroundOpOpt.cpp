4ec19b87ba00aef7423a8d8ca7e9b583bc4a8e48
xiao han <xiao.han@amd.com>
2025-08-11 16:58:11 +0100

    check dq q op value

--- src/Dialect/ONNX/Transforms/QDQAroundOpOpt.cpp
+++ src/Dialect/ONNX/Transforms/QDQAroundOpOpt.cpp
@@ -4,23 +4,25 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/Operation.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
-#include "mlir/Transforms/DialectConversion.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "src/Dialect/ONNX/ONNXOps.hpp"
-#include "src/Dialect/ONNX/ONNXOps/OpHelper.hpp"
 #include "src/Pass/Passes.hpp"
+
+#include "mlir/Transforms/DialectConversion.h"
+#include "src/Pass/Passes.hpp"
+#include "llvm/ADT/STLExtras.h"
+#include <cmath>
 
 using namespace mlir;
 using namespace onnx_mlir;
-
-
-static std::tuple<Value /*input*/, Value /*output*/> getDataInputs(ONNXTransposeOp transposeOp) {
+std::tuple<Value /*input*/, Value /*output*/> getDataInputOutput(
+    ONNXTransposeOp transposeOp) {
   return {transposeOp.getData(), transposeOp.getTransposed()};
 }
 
-namespace {
 float getFloatFromConstant(Value val) {
   auto constOp = val.getDefiningOp<ONNXConstantOp>();
   if (!constOp)
@@ -59,31 +61,45 @@ bool quantizationParamsMatch(
   return zeroPointMatch && scaleClose;
 }
 
+namespace {
 template <typename T>
 class RemoveQDQAroundOpPattern : public OpRewritePattern<T> {
-    // if (isa<ONNXMatMulOp, ONNXMatMulIntegerOp>(op)) {
-
   using OpRewritePattern<T>::OpRewritePattern;
   LogicalResult matchAndRewrite(
       T op, PatternRewriter &rewriter) const override {
-    Value value = getDataInputs(op);
-    auto dqOp = value.getDefiningOp<ONNXDequantizeLinearOp>();
+    Value input, output;
+    std::tie(input, output) = getDataInputOutput(op);
+
+    auto dqOp = input.getDefiningOp<ONNXDequantizeLinearOp>();
     if (!dqOp) {
       return failure();
     }
-    if (!quantizationParamsMatch(dqOp.getXScale(), dqOp.getXZeroPoint(),
-            qOp.getYScale(), qOp.getYZeroPoint())) {
-      return failure();
-    }
-    rewriter.replaceOp(qOp, dqOp.getX());
-    return success();
+    if (output.hasOneUse()) {
+      Operation *firstOp = *(output.getUsers().begin());
+      if (mlir::isa<ONNXQuantizeLinearOp>(firstOp)) {
+        // auto *op2 = (*castedOp1.getODSOperands(1).begin()).getDefiningOp();
+        // if (!(op2)){
+        // }
+        auto qOp = ::llvm::dyn_cast<ONNXQuantizeLinearOp>(firstOp);
+        // if (!(castedOp2)){
+        if (!quantizationParamsMatch(dqOp.getXScale(), dqOp.getXZeroPoint(),
+                qOp.getYScale(), qOp.getYZeroPoint())) {
+          return failure();
+        }
+        rewriter.replaceOp(dqOp, dqOp.getX());
+        rewriter.replaceOp(qOp, qOp.getY());
+        return success();
+      }
+    };
   }
 };
-
 struct QDQAroundOpOptONNXToONNXPass
-    : public PassWrapper<QDQAroundOpOptONNXToONNXPass, OperationPass<func::FuncOp>> {
+    : public PassWrapper<QDQAroundOpOptONNXToONNXPass,
+          OperationPass<func::FuncOp>> {
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(QDQAroundOpOptONNXToONNXPass)
-  StringRef getArgument() const override { return "qdq-around-op-opt-onnx-to-onnx"; }
+  StringRef getArgument() const override {
+    return "qdq-around-op-opt-onnx-to-onnx";
+  }
   StringRef getDescription() const override {
     return "Remove QDQ around ops if safe.";
   }
@@ -91,19 +107,17 @@ struct QDQAroundOpOptONNXToONNXPass
   void runOnOperation() override {
     auto function = getOperation();
     RewritePatternSet patterns(&getContext());
-    patterns.add<RemoveQDQAroundOpPattern<ONNXTransposeOp>>(&getContext());
-    patterns.add<RemoveQDQAroundOpPattern<ONNXUnsqueezeOp>>(&getContext());
-    patterns.add<RemoveQDQAroundOpPattern<ONNXSqueezeOp>>(&getContext());
-    patterns.add<RemoveQDQAroundOpPattern<ONNXReshapeOp>>(&getContext());
-    patterns.add<RemoveQDQAroundOpPattern<ONNXGatherOp>>(&getContext());
-    patterns.add<RemoveQDQAroundOpPattern<ONNXReduceSumOp>>(&getContext());
-    patterns.add<RemoveQDQAroundOpPattern<ONNXSliceOp>>(&getContext());
-    patterns.add<RemoveQDQAroundOpPattern<ONNXResizeOp>>(&getContext());
-    patterns.add<RemoveQDQAroundOpPattern<ONNXFlattenOp>>(&getContext());
-
-
-    if (failed(applyPatternsGreedily(function, std::move(patterns))))
-      signalPassFailure();
+    patterns.add<RemoveQDQAroundOpPattern<ONNXTransposeOp>,
+        RemoveQDQAroundOpPattern<ONNXUnsqueezeOp>,
+        RemoveQDQAroundOpPattern<ONNXSqueezeOp>,
+        RemoveQDQAroundOpPattern<ONNXReshapeOp>,
+        RemoveQDQAroundOpPattern<ONNXGatherOp>,
+        RemoveQDQAroundOpPattern<ONNXReduceSumOp>,
+        RemoveQDQAroundOpPattern<ONNXSliceOp>,
+        RemoveQDQAroundOpPattern<ONNXResizeOp>,
+        RemoveQDQAroundOpPattern<ONNXFlattenOp>>(&getContext());
+    // if (failed(applyPatternsGreedily(function, std::move(patterns))))
+    //   signalPassFailure();
   }
 };
 } // namespace
