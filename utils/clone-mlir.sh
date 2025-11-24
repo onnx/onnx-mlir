503283d919e0e057aec58caf36062a060319c933
Jonas Rickert <jonas.rickert@amd.com>
2025-11-24 02:06:45 -0700

    Update llvm commit

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
 git clone -n https://github.com/xilinx/llvm-aie.git llvm-project
 # Check out a specific branch that is known to work with ONNX-MLIR.
-cd llvm-project && git checkout a0fc10d350b9 && cd ..
+cd llvm-project && git checkout 60a1abaabb5721b7ee6f1a085072f9ad7e7bc430 && cd ..
