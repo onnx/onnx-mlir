5a995bbe51fe3266a7b2b71266cbc2598aff24b0
Tung D. Le <tung@jp.ibm.com>
2025-12-04 01:25:06 -0500

    better code
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- src/Runtime/python/PyExecutionSessionBase.cpp
+++ src/Runtime/python/PyExecutionSessionBase.cpp
@@ -238,12 +238,7 @@ std::vector<py::array> PyExecutionSessionBase::pyRun(
 
   // 2. Call entry point.
   TIMING_INIT_START(inference);
-  OMTensorList *wrappedInput;
-  if (omts.size() == 0) {
-    wrappedInput = omTensorListCreate(nullptr, 0);
-  } else {
-    wrappedInput = omTensorListCreate(&omts[0], omts.size());
-  }
+  OMTensorList *wrappedInput = omTensorListCreate(omts.data(), omts.size());
   auto *wrappedOutput = _entryPointFunc(wrappedInput);
   if (!wrappedOutput)
     throw std::runtime_error(reportErrnoError());
