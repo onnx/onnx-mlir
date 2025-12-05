f98af8f9103c8180704a82b3d1cc1ab733390d5b
Tong Chen <chentong@us.ibm.com>
2025-12-05 20:13:55 +0000

    format
    Signed-off-by: Tong Chen <chentong@us.ibm.com>

--- src/Conversion/KrnlToLLVM/KrnlEntryPoint.cpp
+++ src/Conversion/KrnlToLLVM/KrnlEntryPoint.cpp
@@ -503,10 +503,10 @@ private:
                 MultiDialectBuilder<LLVMBuilder, KrnlBuilder> create(
                     createLLVM);
                 // Print an error message.
-                std::string msg =
-                    "Wrong size for the dimension " + std::to_string(d) +
-                    " of the input " + std::to_string(i) +
-                    ": expect a non-negative value\n";
+                std::string msg = "Wrong size for the dimension " +
+                                  std::to_string(d) + " of the input " +
+                                  std::to_string(i) +
+                                  ": expect a non-negative value\n";
                 StringRef errorMsg(msg);
                 create.krnl.printf(errorMsg);
                 // Set errno.
