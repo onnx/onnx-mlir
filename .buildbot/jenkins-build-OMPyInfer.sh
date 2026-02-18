b61aa8bf01f349031576b0cbc7d700e823773998
chentong319 <chentong@us.ibm.com>
2026-02-18 09:54:07 -0500

    debug
    Signed-off-by: chentong319 <chentong@us.ibm.com>

--- .buildbot/jenkins-build-OMPyInfer.sh
+++ .buildbot/jenkins-build-OMPyInfer.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+# Currently, no real check is done.
+# Next PR will perform real check
 echo "Start build OMPyInfer..."
-#if [ "$(uname -m)" = "s390x" ]; then 
-#  echo "this is s360 machine"
-#else
-#  echo "this is NOT s360 machine"
-#fi
+if [ "$(uname -m)" = "s390x" ]; then 
+  echo "this is s360 machine"
+else
+  echo "this is NOT s360 machine"
+fi
