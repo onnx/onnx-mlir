7e70f8fc62c1a9767fc014a4065fb28df995a2ff
Sunny Anand <sunnyanand.979@gmail.com>
2026-03-26 10:43:33 -0500

    fix correct header
    Signed-off-by: Sunny Anand <sunnyanand.979@gmail.com>

--- src/Conversion/ONNXToKrnl/NN/GridSample.cpp
+++ src/Conversion/ONNXToKrnl/NN/GridSample.cpp
@@ -14,7 +14,7 @@
 
 #include "src/Conversion/ONNXToKrnl/ONNXToKrnlCommon.hpp"
 #include "src/Dialect/ONNX/ONNXOps/ShapeHelper.hpp"
-#include "llvm/ADT/Triple.h"
+#include "llvm/TargetParser/Triple.h"
 
 using namespace mlir;
 
