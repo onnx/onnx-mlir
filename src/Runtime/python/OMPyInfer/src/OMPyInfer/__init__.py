504fcfef2d2856457d7edc404e67525b52a274da
Tong Chen <chentong@us.ibm.com>
2026-04-01 11:22:39 -0400

    Simplify PyRuntime (#3446)
    * implemented
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * format
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    * delete .onnx and .so
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>
    
    ---------
    
    Signed-off-by: Tong Chen <chentong@us.ibm.com>

--- src/Runtime/python/OMPyInfer/src/OMPyInfer/__init__.py
+++ src/Runtime/python/OMPyInfer/src/OMPyInfer/__init__.py
@@ -1,5 +1,16 @@
 # SPDX-License-Identifier: Apache-2.0
 
+import os
+import sys
+import importlib.util
+
+spec = importlib.util.find_spec(__package__)
+loader = spec.loader
+PyRuntimeC_module = os.path.join(
+    os.path.dirname(loader.get_filename(__package__)), "libs"
+)
+sys.path.append(PyRuntimeC_module)
+
 from .PyRuntime import OMExecutionSession as InferenceSession
 from .utils import (
     parse_args,
