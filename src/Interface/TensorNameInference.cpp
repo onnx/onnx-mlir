e2d015ddc3a9b9335a27210004da1462bb487001
Ilango Rajagopal <ilango.rajagopal@amd.com>
2026-04-06 04:48:33 -0600

    Validate static shapes and constant values for ops

--- src/Interface/TensorNameInference.cpp
+++ src/Interface/TensorNameInference.cpp
@@ -19,12 +19,14 @@ ReshapeOpTensorNameInference::inferTensorNameTransform(
     mlir::Operation *op) const {
   auto reshapeOp = cast<ONNXReshapeOp>(op);
 
-  auto inShape =
-      cast<RankedTensorType>(reshapeOp.getData().getType()).getShape();
-  auto outShape =
-      cast<RankedTensorType>(reshapeOp.getResult().getType()).getShape();
+  // Validate if shapes are static
+  auto inType = cast<RankedTensorType>(reshapeOp.getOperand(0).getType());
+  auto outType = cast<RankedTensorType>(reshapeOp.getResult().getType());
+  if (!inType.hasStaticShape() || !outType.hasStaticShape())
+    return nullptr;
 
-  return std::make_unique<ReshapeTransform>(inShape, outShape);
+  return std::make_unique<ReshapeTransform>(
+      inType.getShape(), outType.getShape());
 }
 
 std::unique_ptr<Transform>
@@ -32,18 +34,27 @@ TransposeOpTensorNameInference::inferTensorNameTransform(
     mlir::Operation *op) const {
   auto transposeOp = cast<ONNXTransposeOp>(op);
 
-  auto inShape =
-      cast<RankedTensorType>(transposeOp.getData().getType()).getShape();
-  auto perm = Transform::arrayToVector(transposeOp.getPermAttr());
-  auto outShape =
-      cast<RankedTensorType>(transposeOp.getResult().getType()).getShape();
+  // Validate if shapes are static
+  auto inType = cast<RankedTensorType>(transposeOp.getOperand().getType());
+  auto outType = cast<RankedTensorType>(transposeOp.getResult().getType());
+  if (!inType.hasStaticShape() || !outType.hasStaticShape())
+    return nullptr;
 
-  return std::make_unique<TransposeTransform>(inShape, perm, outShape);
+  auto perm = Transform::arrayToVector(transposeOp.getPermAttr());
+
+  return std::make_unique<TransposeTransform>(
+      inType.getShape(), perm, outType.getShape());
 }
 
 std::unique_ptr<Transform> PadOpTensorNameInference::inferTensorNameTransform(
     mlir::Operation *op) const {
   auto padOp = cast<ONNXPadOp>(op);
+
+  // Validate if shapes are static
+  auto inType = cast<RankedTensorType>(padOp.getOperand(0).getType());
+  auto outType = cast<RankedTensorType>(padOp.getResult().getType());
+  if (!inType.hasStaticShape() || !outType.hasStaticShape())
+    return nullptr;
 
   // Only mode = "constant" is supported
   if (padOp.getMode() != "constant")
@@ -54,44 +65,48 @@ std::unique_ptr<Transform> PadOpTensorNameInference::inferTensorNameTransform(
   Attribute constant = constOp.getValueAttr();
   if (!constant)
     return nullptr;
+
+  // Validate if pads is constant
   auto pads = Transform::valToVector(padOp.getPads());
   if (pads.size() == 0)
     return nullptr;
 
-  auto inShape = cast<RankedTensorType>(padOp.getData().getType()).getShape();
   auto splitAt = pads.size() / 2;
   auto starts = SmallVector<int64_t>(pads.begin(), pads.begin() + splitAt);
   auto ends = SmallVector<int64_t>(pads.begin() + splitAt, pads.end());
-  auto axes = Transform::axesToVector(padOp.getAxes(), inShape.size());
-  auto outShape =
-      cast<RankedTensorType>(padOp.getOutput().getType()).getShape();
+  auto axes = Transform::axesToVector(padOp.getAxes(), inType.getRank());
 
   return std::make_unique<PadTransform>(
-      inShape, starts, ends, axes, constant, outShape);
+      inType.getShape(), starts, ends, axes, constant, outType.getShape());
 }
 
 std::unique_ptr<Transform> SliceOpTensorNameInference::inferTensorNameTransform(
     mlir::Operation *op) const {
   auto sliceOp = cast<ONNXSliceOp>(op);
 
-  auto inShape = cast<RankedTensorType>(sliceOp.getData().getType()).getShape();
+  // Validate if shapes are static
+  auto inType = cast<RankedTensorType>(sliceOp.getOperand(0).getType());
+  auto outType = cast<RankedTensorType>(sliceOp.getResult().getType());
+  if (!inType.hasStaticShape() || !outType.hasStaticShape())
+    return nullptr;
+
+  // Validate if starts & ends are constant and steps is always 1
   auto starts = Transform::valToVector(sliceOp.getStarts());
   auto ends = Transform::valToVector(sliceOp.getEnds());
-  auto axes = Transform::axesToVector(sliceOp.getAxes(), inShape.size());
-  auto outShape =
-      cast<RankedTensorType>(sliceOp.getResult().getType()).getShape();
-
+  auto steps = Transform::valToVector(sliceOp.getSteps());
   if (starts.size() == 0 || ends.size() == 0 ||
-      llvm::any_of(Transform::valToVector(sliceOp.getSteps()),
-          [](int64_t s) { return s != 1; }))
+      llvm::any_of(steps, [](int64_t s) { return s != 1; }))
     return nullptr;
+
+  auto inShape = inType.getShape();
+  auto axes = Transform::axesToVector(sliceOp.getAxes(), inShape.size());
 
   // Clip end values
   for (const auto &[ax, en] : llvm::zip_equal(axes, ends))
     en = std::min(en, inShape[ax]);
 
   return std::make_unique<SliceTransform>(
-      inShape, starts, ends, axes, outShape);
+      inShape, starts, ends, axes, outType.getShape());
 }
 
 void registerTensorNameInferenceExternalModels(
