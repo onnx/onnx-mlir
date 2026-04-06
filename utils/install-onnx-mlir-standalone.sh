15940771d509a28190c45cab34c21af021612564
Alexandre Eichenberger <alexe@us.ibm.com>
2026-04-06 18:23:56 -0400

    updates
    Signed-off-by: Alexandre Eichenberger <alexe@us.ibm.com>

--- utils/install-onnx-mlir-standalone.sh
+++ utils/install-onnx-mlir-standalone.sh
@@ -2,7 +2,7 @@
 MLIR_DIR=$(pwd)/llvm-project/build/lib/cmake/mlir
 mkdir onnx-mlir/build && cd onnx-mlir/build
 if [[ -z "$pythonLocation" ]]; then
-  cmake -G Ninja \
+  cmake -G "Unix Makefiles" \
         -DCMAKE_CXX_COMPILER=/usr/bin/c++ \
         -DCMAKE_BUILD_TYPE=Release \
         -DLLVM_ENABLE_ASSERTIONS=ON \
@@ -11,7 +11,7 @@ if [[ -z "$pythonLocation" ]]; then
         -DCMAKE_IGNORE_PATH="/usr/local;/opt" \
         ..
 else
-  cmake -G Ninja \
+  cmake -G "Unix Makefiles" \
         -DCMAKE_CXX_COMPILER=/usr/bin/c++ \
         -DCMAKE_BUILD_TYPE=Release \
         -DLLVM_ENABLE_ASSERTIONS=ON \
