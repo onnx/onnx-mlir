cc99330b47d39c22df45a72ef1eb31b913b8693d
pragyam <pragya.musal@amd.com>
2026-05-20 11:11:13 -0600

    Clang fix

--- src/Dialect/ONNX/Transforms/xmc/DQBinaryQOptPass.cpp
+++ src/Dialect/ONNX/Transforms/xmc/DQBinaryQOptPass.cpp
@@ -378,7 +378,7 @@ private:
       if (auto intermediateOp = dq1.getX().getDefiningOp()) {
         if (isValuePreservingOp(intermediateOp)) {
           if (auto constOp = intermediateOp->getOperand(0)
-                                 .getDefiningOp<ONNXConstantOp>()) {
+                  .getDefiningOp<ONNXConstantOp>()) {
             constantDqOp = dq1;
             state.dequantActivationOfBinOp = dq2;
             constantSourceOp = constOp;
@@ -389,7 +389,7 @@ private:
       if (auto intermediateOp = dq2.getX().getDefiningOp()) {
         if (isValuePreservingOp(intermediateOp)) {
           if (auto constOp = intermediateOp->getOperand(0)
-                                 .getDefiningOp<ONNXConstantOp>()) {
+                  .getDefiningOp<ONNXConstantOp>()) {
             constantDqOp = dq2;
             state.dequantActivationOfBinOp = dq1;
             constantSourceOp = constOp;
