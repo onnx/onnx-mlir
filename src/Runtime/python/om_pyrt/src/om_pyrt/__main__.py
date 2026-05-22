dfdcef27b1d1189f0eeab62fce8459ef2f019b84
Tong Chen <chentong@us.ibm.com>
2026-05-22 18:13:03 +0000

    format
    Signed-off-by: Tong Chen <chentong@us.ibm.com>

--- src/Runtime/python/om_pyrt/src/om_pyrt/__main__.py
+++
@@ -1,8 +0,0 @@
-# ToFix: modify RunONNXModel.py for this purpose if desired
-# from .RunONNXModel import main
-
-def main():
-    print("not implemented yet")
-
-if __name__ == "__main__":
-    raise SystemExit(main())
