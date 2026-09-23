cbd874c1b80655c6f43304779b3e9ae367260791
Sunny Anand <sunnyanand.979@gmail.com>
2026-09-22 22:22:53 -0500

    fix endianness issue for s390x backend tests
    Signed-off-by: Sunny Anand <sunnyanand.979@gmail.com>

--- src/Conversion/KrnlToLLVM/ConvertKrnlToLLVM.cpp
+++ src/Conversion/KrnlToLLVM/ConvertKrnlToLLVM.cpp
@@ -572,7 +572,17 @@ bool extractConstantsToFile(ModuleOp &module, std::string filepath,
 
     op.setOffsetAttr(b.getI64IntegerAttr(totalConstSize));
     op.removeValueAttr();
-    outfile.write(rawData.data(), rawData.size());
+
+    if (llvm::endianness::native != llvm::endianness::little) {
+      auto memRefTy = mlir::cast<MemRefType>(op.getOperation()->getResult(0).getType());
+      SmallVector<char> swappedData(rawData.size());
+      DenseIntOrFPElementsAttr::convertEndianOfArrayRefForBEmachine(
+          rawData, swappedData,
+          RankedTensorType::get(memRefTy.getShape(), memRefTy.getElementType()));
+      outfile.write(swappedData.data(), swappedData.size());
+    } else {
+      outfile.write(rawData.data(), rawData.size());
+    }
     totalConstSize += rawData.size();
   }
 
