4c49f91af90ae5e27c44b4bb384f8d6cd8b9f820
Tung D. Le <tung@jp.ibm.com>
2026-09-23 11:29:33 +0900

    [NNPA] Add ExpandAttentionMask Pass for NNPA Broadcasting Support (#3647)
    * Expand mask to avoid broadcasting addition
    
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>
    
    * copyright
    
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>
    
    * clang-format
    
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>
    
    * Edit lit tests
    
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>
    
    * clang-format
    
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>
    
    ---------
    
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>
    Co-authored-by: Tong Chen <chentong@us.ibm.com>

--- src/Tools/onnx-mlir-opt/RegisterPasses.cpp
+++ src/Tools/onnx-mlir-opt/RegisterPasses.cpp
@@ -132,6 +132,10 @@ void registerOMPasses(int optLevel) {
   });
 
   mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
+    return createExpandAttentionMaskPass();
+  });
+
+  mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
     return krnl::createConvertKrnlToAffinePass();
   });
 
